549904b76543eec41c7b96f2b600d1b82d378421
Yann Bouret <yann.bouret@gmail.com>
2023-07-02 18:56:44 +0200

    wip

--- src/core/tests/text/human-readable.cpp
+++ src/core/tests/text/human-readable.cpp
@@ -15,5 +15,16 @@ namespace
 Y_UTEST(text_human_readable)
 {
     
+    for(size_t i=0;i<HumanReadable::Number;++i)
+    {
+        std::cerr << HumanReadable::Divide[i].suffix << " : " << HumanReadable::Divide[i].factor << std::endl;
+    }
+
+    HumanReadable hr1(20);
+    HumanReadable hr2(20000);
+
+    std::cerr << HumanReadable(10)        << std::endl;
+    std::cerr << HumanReadable(123456789) << std::endl;
+
 }
 Y_UDONE()
--- src/core/y/text/human-readable.hpp
+++ src/core/y/text/human-readable.hpp
@@ -4,15 +4,34 @@
 #define Y_Text_Plural_Included 1
 
 #include "y/config/starting.hpp"
+#include "y/ios/ostream-proto.hpp"
 
 namespace Yttrium
 {
-    struct HumanReadable
+    class HumanReadable
     {
-        static const char   Suffix[];
-        static const size_t Number;
-        static const size_t MaxExp;
+    public:
+        struct Divider {
+            const char     suffix;
+            const uint64_t factor;
+        };
+
+        static const Divider Divide[];
+        static const size_t  Number;
         
+
+
+        HumanReadable(const HumanReadable &) noexcept;
+        HumanReadable(const uint64_t u) noexcept;
+        ~HumanReadable() noexcept;
+        Y_OSTREAM_PROTO(HumanReadable);
+
+        const unsigned quot; //!< quotient
+        const unsigned rem;  //!< remainder
+        const unsigned sfx;  //!< value = quot * 10^(3*sfx) + rem
+
+    private:
+        Y_DISABLE_ASSIGN(HumanReadable);
     };
 }
 
