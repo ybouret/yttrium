ab124cd8bf1739feea172d214964127397439311
Yann Bouret <yann.bouret@gmail.com>
2023-07-05 12:13:17 +0200

    wip

--- src/core/tests/random/bits.cpp
+++ src/core/tests/random/bits.cpp
@@ -8,10 +8,53 @@
 using namespace Yttrium;
 
 
+template <typename T>
+static inline void find(const char *name)
+{
+    std::cerr << "-------- " << name << std::endl;
+    static const uint64_t one = 1;
+    unsigned     shift        = 32;
+    uint64_t     top          = one << shift;
+
+    while(true)
+    {
+        const T maxDen(top);
+        const T maxNum = T(top-1)+T(0.5);
+        if(maxNum/maxDen<T(1))
+        {
+            std::cerr << "maxOk@2^" << shift << std::endl;
+            break;
+        }
+        --shift;
+        top >>= 1;
+    }
+
+    while(true)
+    {
+        const T maxDen(top);
+        const T maxNum = T(0.5);
+        if(maxNum/maxDen>0)
+        {
+            std::cerr << "minOk@2^" << shift << std::endl;
+            break;
+        }
+        --shift;
+        top >>= 1;
+    }
+
+
+
+}
+
 
 Y_UTEST(random_bits)
 {
 
+    find<double>("double");
+    find<long double>("long double");
+    find<float>("float");
+
+    return 0;
     {
         const long double maxNum = 4294967295.5L;
         const long double maxDen = 4294967296.0L;
