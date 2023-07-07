36c14ec838c1635abd550afb8b97ba75b0499563
Yann Bouret <yann.bouret@unice.fr>
2023-07-07 11:46:38 +0200

    wip

--- src/core/tests/memory/straps.cpp
+++ src/core/tests/memory/straps.cpp
@@ -21,37 +21,41 @@ Y_UTEST(memory_straps)
     alea_seed();
 
     Memory::Album  album;
-    Memory::Straps straps(album);
-
     {
-        size_t  bs = 0;
-        void   *p  = straps.acquire(bs);
-        std::cerr << "got bs=" << bs << " @" << p << std::endl;
-        straps.release(p);
-    }
+        Memory::Straps straps(album);
 
-    if(false)
-    {
-        static const size_t NB = 1024;
-        block_t             blocks[NB];
-        Y_STATIC_ZARR(blocks);
-
-        for(size_t i=0;i<NB;++i)
         {
-            block_t &blk = blocks[i];
-            blk.size = alea_leq(100);
-            blk.addr = straps.acquire(blk.size);
+            size_t  bs = 0;
+            void   *p  = straps.acquire(bs);
+            std::cerr << "got bs=" << bs << " @" << p << std::endl;
+            straps.release(p);
         }
 
-
-        alea_shuffle(blocks,NB);
-
-        for(size_t i=0;i<NB;++i)
+        if(false)
         {
-            block_t &blk = blocks[i];
-            straps.release(blk.addr);
+            static const size_t NB = 1024;
+            block_t             blocks[NB];
+            Y_STATIC_ZARR(blocks);
+
+            for(size_t i=0;i<NB;++i)
+            {
+                block_t &blk = blocks[i];
+                blk.size = alea_leq(100);
+                blk.addr = straps.acquire(blk.size);
+            }
+
+
+            alea_shuffle(blocks,NB);
+
+            for(size_t i=0;i<NB;++i)
+            {
+                block_t &blk = blocks[i];
+                straps.release(blk.addr);
+            }
         }
     }
+
+    album.displayInfo(0);
 
 }
 Y_UDONE()
