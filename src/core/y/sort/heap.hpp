316160ec11e3d71ce73d8af8cfff4fd156612311
Yann Bouret <yann.bouret@unice.fr>
2023-07-10 14:04:41 +0200

    wip

--- src/core/y/sort/heap.hpp
+++ src/core/y/sort/heap.hpp
@@ -26,10 +26,10 @@ namespace Yttrium
             if(num<2) return;
 
             void *   _[ Y_WORDS_FOR(T) ];
-            size_t   il  = (num>>1)+1;
-            size_t   ir  = num;
-            T       &__  = *static_cast<T*>(Memory::OutOfReach::Addr(_));
-            T       *ar1 = &arr[1];
+            size_t          il  = (num>>1)+1;
+            size_t          ir  = num;
+            T       &       __  = *static_cast<T*>(Memory::OutOfReach::Addr(_));
+            T       * const a1  = &arr[1];
 
             while(true)
             {
@@ -39,26 +39,24 @@ namespace Yttrium
                 }
                 else
                 {
-                    Memory::OutOfReach::Move(_,&arr[ir],ar1,sizeof(T));
+                    Memory::OutOfReach::Move(_,&arr[ir],a1,sizeof(T));
                     if(--ir == 1)
                     {
-                        Memory::OutOfReach::Copy(ar1,_,sizeof(T));
+                        Memory::OutOfReach::Copy(a1,_,sizeof(T));
                         break;
                     }
                 }
 
                 size_t    i   = il;
                 size_t    j   = il<<1;
-
                 while (j <= ir)
                 {
                     if( (j<ir) && proc(arr[j],arr[j+1]) < 0 )
                             ++j;
 
-
                     if( proc(__,arr[j]) < 0 )
                     {
-                        arr[i]=arr[j];
+                        Memory::OutOfReach::Copy(&arr[i],&arr[j],sizeof(T));
                         i   = j;
                         j <<= 1;
                     }
