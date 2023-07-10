c9faa88673c9e4c912727c3c82e97a1ac89bb362
Yann Bouret <yann.bouret@unice.fr>
2023-07-10 15:56:42 +0200

    wip

---
+++ src/core/y/memory/allocator/global.cpp
@@ -0,0 +1,63 @@
+
+
+#include "y/memory/allocator/global.hpp"
+#include "y/memory/legacy.hpp"
+
+namespace Yttrium
+{
+
+    namespace Memory
+    {
+        const AtExit::Longevity Global:: LifeTime = AtExit::MaximumLongevity - 1;
+        const char * const      Global:: CallSign = "Memory::Global";
+
+
+
+        static void   *workspace[ Y_WORDS_FOR(Legacy) ];
+        static Legacy *legacy = 0;
+
+        Global:: ~Global() noexcept
+        {
+            assert(0!=legacy);
+            OutOfReach::Naught(legacy);
+            legacy = 0;
+        }
+
+        Global:: Global() :
+        SingletonType(),
+        Allocator()
+        {
+            legacy = new ( Y_STATIC_ZARR(workspace) ) Legacy();
+        }
+
+
+        void * Global:: acquire(size_t & count, const size_t blockSize)
+        {
+            assert(0!=legacy);
+            const size_t request = count * blockSize;
+            try {
+                void *p = legacy->acquire(request); // system locked
+                count = request;
+                assert( OutOfReach::Are0(p,count) );
+                return p;
+            }
+            catch(...)
+            {
+                count = 0;
+                throw;
+            }
+        }
+
+
+        void Global:: release(void *&entry, size_t &count) noexcept
+        {
+            assert(0!=legacy);
+            legacy->release(entry,count); // system locked
+        }
+
+        
+
+    }
+
+}
+
---
+++ src/core/y/memory/allocator/global.hpp
@@ -0,0 +1,38 @@
+//! \file
+
+#ifndef Y_Memory_Allocator_Global_Included
+#define Y_Memory_Allocator_Global_Included 1
+
+#include "y/singleton.hpp"
+#include "y/memory/allocator.hpp"
+
+namespace Yttrium
+{
+
+    namespace Memory
+    {
+
+        class Global : public Singleton<Global,NucleusSingleton>, public Allocator
+        {
+        public:
+            static const char * const      CallSign; //!< "Memory::Global"
+            static const AtExit::Longevity LifeTime; //!< AtExit::MaximumLongevity - 1;
+            
+            virtual void * acquire(size_t & count, const size_t blockSize);
+            virtual void   release(void * & entry, size_t &     count) noexcept;
+
+            
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Global);
+            explicit Global();
+            virtual ~Global() noexcept;
+            
+        };
+
+
+    }
+
+}
+
+#endif
+
