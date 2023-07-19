ee47fa6aa0573d6c80e7ea87634a6d61928a122a
Yann Bouret <yann.bouret@unice.fr>
2023-07-19 13:27:21 +0200

    wip

--- src/core/tests/fft/fft1d.cpp
+++ src/core/tests/fft/fft1d.cpp
@@ -1,6 +1,8 @@
 #include "y/fft/fft.hpp"
 #include "y/utest/run.hpp"
 #include "y/memory/legacy.hpp"
+#include "y/utest/timing.hpp"
+#include "y/type/utils.hpp"
 
 using namespace Yttrium;
 
@@ -8,10 +10,44 @@ using namespace Yttrium;
 namespace
 {
 
+    static inline size_t CountXBR(const size_t nn, size_t &imx) noexcept
+    {
+        size_t count = 0;
+        const size_t n = (nn<<1);
+        imx = 0;
+        size_t j=1;
+        for(size_t i=1;i<n;i+=2)
+        {
+            if(j>i)
+            {
+                //Swap(data[j],data[i]);
+                //Swap(data[j+1],data[i+1]);
+                //std::cerr << i << " <-> " << j << " AND " << (i+1) << " <-> " << (j+1) << std::endl;
+                imx = Max(imx,j+1);
+                count += 2;
+            }
+            size_t m=nn;
+            while (m >= 2 && j > m)
+            {
+                j -= m;
+                m >>= 1;
+            }
+            j += m;
+        }
+        std::cerr << "number = " << nn << ", #swaps=" << count << ", imx=" << imx << std::endl;
+        return count;
+    }
+
+
+    static double Duration = 1.0;
+
     template <typename T> static inline
-    void testFFT(const unsigned shift)
+    void testFFT(const unsigned shift, uint64_t &rate)
     {
         Memory::Legacy ram;
+        Timing         tmx;
+        rate = 0;
+
         std::cerr << "FFT: 2^" << shift << std::endl;
         const size_t n   = 1 << shift;
         const size_t nn  = n*2;
@@ -20,10 +56,18 @@ namespace
 
         for(size_t i=1;i<=nn;++i) data[i] = T(i);
         Core::Display(std::cerr,data+1,nn) << std::endl;
-        FFT::Run(data,n, 1);
-        FFT::Run(data,n,-1);
-        for(size_t i=2*n;i>0;--i) data[i] /= n;
+        do
+        {
+            const uint64_t mark = WallTime::Ticks();
+            FFT::Run(data,n, 1);
+            FFT::Run(data,n,-1);
+            tmx.ticks += WallTime::Ticks() - mark;
+            ++tmx.cycle;
+            for(size_t i=2*n;i>0;--i) data[i] /= n;
+        } while( tmx.probe() < Duration);
+
         Core::Display(std::cerr,data+1,nn) << std::endl;
+        std::cerr << "Speed: " << HumanReadable(tmx.speed()) << std::endl;
 
         ram.release(data+1,bs);
     }
@@ -32,25 +76,18 @@ namespace
 Y_UTEST(fft_1d)
 {
 
-    float arr[] = { 1,2, 3,4, 5,6, 7,8, 9,10, 11,12, 13,14, 15,16 };
-
-    Core::Display(std::cerr,arr,sizeof(arr)/sizeof(arr[0])) << std::endl;
-
-    FFT::Run(arr-1,8,1);
-    FFT::Run(arr-1,8,-1);
-
-    for(size_t i=0;i<16;++i)
+    for(size_t i=0;i<10;++i)
     {
-        arr[i] /= 8;
+        const size_t n   = (1<<i);
+        size_t       imx = 0;
+        CountXBR(n,imx);
     }
-    Core::Display(std::cerr,arr,sizeof(arr)/sizeof(arr[0])) << std::endl;
 
-    testFFT<float>(0);
-    testFFT<float>(1);
-    testFFT<float>(2);
-    testFFT<float>(3);
-    testFFT<float>(4);
-    testFFT<float>(5);
+    uint64_t rate[6];
+    for(unsigned i=0;i<6;++i)
+    {
+        testFFT<float>(i,rate[i]);
+    }
 
 
 }
--- src/core/y/fft/fft.hpp
+++ src/core/y/fft/fft.hpp
@@ -11,48 +11,59 @@ namespace Yttrium
 
     struct FFT
     {
-        template <typename T> static inline
-        void Run(T data[], const size_t nn, const int isign) noexcept
-        {
-            unsigned long n,mmax,m,j,istep,i;
-            double wtemp,wr,wpr,wpi,wi,theta;
-            T  tempr,tempi;
 
-            n=nn << 1;
-            j=1;
-            for (i=1;i<n;i+=2)
+        template <typename T>
+        static inline size_t XBR(T data[], const size_t nn) noexcept
+        {
+            const size_t n = (nn<<1);
+            size_t j=1;
+            for(size_t i=1;i<n;i+=2)
             {
-                if (j > i)
+                if(j>i)
                 {
                     Swap(data[j],data[i]);
                     Swap(data[j+1],data[i+1]);
                 }
-                m=nn;
-                while (m >= 2 && j > m) {
+                size_t m=nn;
+                while (m >= 2 && j > m)
+                {
                     j -= m;
-                    m >>= 1; }
+                    m >>= 1;
+                }
                 j += m;
             }
+            return n;
+        }
 
-            mmax=2;
-            while (n > mmax) {
-                istep=mmax << 1;
+        template <typename T> static inline
+        void Run(T data[], const size_t nn, const int isign) noexcept
+        {
+            typedef double LongT;
+            LongT wtemp,wr,wpr,wpi,wi,theta;
+            T     tempr,tempi;
+
+            const size_t n = XBR(data,nn);
+
+            size_t mmax=2;
+            while(n>mmax)
+            {
+                const size_t istep = (mmax << 1);
                 theta=isign*(6.28318530717959/mmax);
                 wtemp=sin(0.5*theta);
                 wpr = -2.0*wtemp*wtemp;
                 wpi=sin(theta);
                 wr=1.0;
                 wi=0.0;
-                for (m=1;m<mmax;m+=2)
+                for(size_t m=1;m<mmax;m+=2)
                 {
-                    for (i=m;i<=n;i+=istep)
+                    for(size_t i=m;i<=n;i+=istep)
                     {
-                        j=i+mmax;
+                        const size_t j=i+mmax;
                         tempr=wr*data[j]-wi*data[j+1];
                         tempi=wr*data[j+1]+wi*data[j];
                         data[j]=data[i]-tempr;
                         data[j+1]=data[i+1]-tempi;
-                        data[i] += tempr;
+                        data[i]   += tempr;
                         data[i+1] += tempi;
                     }
                     wr=(wtemp=wr)*wpr-wi*wpi+wr;
