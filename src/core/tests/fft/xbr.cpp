845e6afa27ebb98f22c32bc38eff011d5b9c3b6a
Yann Bouret <yann.bouret@unice.fr>
2023-07-26 13:44:02 +0200

    wip

--- src/core/tests/fft/xbr.cpp
+++ src/core/tests/fft/xbr.cpp
@@ -9,6 +9,7 @@
 #include "y/memory/out-of-reach.hpp"
 #include "y/memory/buffer/of.hpp"
 #include "y/text/hexadecimal.hpp"
+#include "y/random/fill.hpp"
 
 using namespace Yttrium;
 
@@ -54,26 +55,80 @@ namespace
     }
 
 
-    
+    static double Duration = 0.1;
+
+    template <typename T> static inline
+    void perfXBR(const unsigned shift, FFT &fft, Random::Bits &ran)
+    {
+        Timing tmx;
+        const size_t nc = 1<<shift;
+        std::cerr << "Timing XBR 2^" << std::setw(2) << shift << " = " << std::setw(8) << nc;
+        std::cerr.flush();
+
+        Memory::BufferOf< Complex<T> > cplx(nc);
+        Memory::BufferOf< Complex<T> > temp(nc);
+        Y_ASSERT(cplx.measure() == temp.measure() );
+
+        // fill with garbage
+        for(size_t i=0;i<nc;++i)
+        {
+            Complex<T> c;
+            Random::Fill::Block(&c, sizeof(T), ran, 1, 255);
+            cplx[i] = temp[i] = c;
+        }
+        Y_ASSERT(cplx.HasSameContentThan(temp));
+
+        // sanity check
+        FFT::MakeXBR(&cplx[0],nc);
+        fft.makeXBR(&temp[0],nc,shift);
+        Y_ASSERT(cplx.HasSameContentThan(temp));
+
+        tmx.reset();
+        Y_Timing(tmx,  FFT::MakeXBR(&cplx[0],nc), Duration);
+        const uint64_t Rate = tmx.speed();
+        std::cerr << " | Rate=" << HumanReadable(Rate) <<"Op/s";
+        std::cerr.flush();
+
+        tmx.reset();
+        Y_Timing(tmx,  fft.makeXBR(&cplx[0],nc,shift), Duration);
+        const uint64_t rate = tmx.speed();
+        std::cerr << " | rate=" << HumanReadable(rate) <<"Op/s";
+
+        const long double speedUp = static_cast<long double>(rate) / static_cast<long double>(Rate);
+
+        std::cerr << " | ratio : " << speedUp;
+
+        std::cerr << std::endl;
+
+    }
     
 }
 
 Y_UTEST(fft_xbr)
 {
+
+    if(argc>1) Duration = atof(argv[1]);
+
     FFT::Verbose = 1;
     FFT &fft = FFT::Instance();
     std::cerr << fft.callSign() << std::endl;
 
 
-    for(unsigned p=0;p<=20;++p)
+    for(unsigned p=0;p<=16;++p)
     {
         checkXBR<float>(p,fft);
         checkXBR<double>(p,fft);
         checkXBR<long double>(p,fft);
     }
 
-
-
+    Random::Rand ran;
+    std::cerr << std::endl;
+    std::cerr << "-- Checking Performances" << std::endl;
+    for(unsigned p=0;p<=20;++p)
+    {
+        perfXBR<float>(p,fft,ran);
+    }
+    std::cerr << "-- Done!" << std::endl;
 
 }
 Y_UDONE()
