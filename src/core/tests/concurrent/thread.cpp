3a4cdaa91d488ba2940f5e89331fd6c3e1a530ec
Yann Bouret <yann.bouret@gmail.com>
2023-08-01 12:22:10 +0200

    wip

---
+++ src/core/tests/concurrent/thread.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+#include "y/concurrent/mutex.hpp"
+#include "y/utest/run.hpp"
+
+#if defined(Y_BSD)
+#include <pthread.h>
+
+
+
+#endif
+
+using namespace Yttrium;
+
+
+
+
+Y_UTEST(concurrent_thread)
+{
+
+
+
+
+}
+Y_UDONE()
+
