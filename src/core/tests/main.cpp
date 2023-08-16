bb7f341bcb5361eb55390ae92aa353cd18a6bdac
Yann Bouret <yann.bouret@gmail.com>
2023-08-16 15:30:38 +0200

    wip

--- src/core/tests/main.cpp
+++ src/core/tests/main.cpp
@@ -151,6 +151,7 @@ Y_UTEST_DECL(256)
     Y_UTEST(mkl_vtx);
     Y_UTEST(mkl_xreal);
     Y_UTEST(mkl_api);
+    Y_UTEST(mkl_cameo);
 
     Y_UTEST(graphviz);
 }
---
+++ src/core/tests/mkl/cameo.cpp
@@ -0,0 +1,14 @@
+#include "../main.hpp"
+
+#include "y/mkl/api.hpp"
+#include "y/utest/run.hpp"
+
+using namespace Yttrium;
+
+
+Y_UTEST(mkl_cameo)
+{
+
+}
+Y_UDONE()
+
