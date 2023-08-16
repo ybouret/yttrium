1a64bbf2217fa797081cc0f000401adaf9932ee0
Yann Bouret <yann.bouret@gmail.com>
2023-08-16 21:59:51 +0200

    wip

--- src/core/tests/mkl/cameo.cpp
+++ src/core/tests/mkl/cameo.cpp
@@ -2,6 +2,7 @@
 
 #include "y/mkl/api.hpp"
 #include "y/utest/run.hpp"
+#include "y/comparison.hpp"
 
 using namespace Yttrium;
 
@@ -13,8 +14,51 @@ namespace Yttrium
         template <typename T>
         struct Proxy
         {
-            
+            class Unit
+            {
+            public:
+                const T usrValue;
+                const T absValue;
+                inline Unit(const T &args) :
+                usrValue(args),
+                absValue( MKL::Fabs<T>::Of(usrValue) )
+                {
+                }
+
+                inline ~Unit() noexcept {}
+
+                inline Unit(const Unit &other) :
+                usrValue(other.usrValue),
+                absValue(other.absValue)
+                {
+                }
+
+            private:
+                Y_DISABLE_ASSIGN(Unit);
+            };
+
+            class Comparator
+            {
+            public:
+                inline  Comparator() noexcept {}
+                inline ~Comparator() noexcept {}
+
+                inline SignType operator()(const Unit &lhs, const Unit &rhs)
+                {
+                    return Comparison::CxxIncreasing(lhs.absValue,rhs.absValue);
+                }
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Comparator);
+            };
         };
+
+        template <>
+        struct Proxy<apn> {
+            typedef apn Unit;
+        };
+
+
     }
 
 }
