50102b9705d3c30f60728f7147502f00d82be3d6
ybouret <yann.bouret@gmail.com>
2023-08-21 09:40:33 +0200

    wip

--- src/core/y/vfs/local-fs.cpp
+++ src/core/y/vfs/local-fs.cpp
@@ -1,35 +1,43 @@
 
 #include "y/vfs/local-fs.hpp"
 #include "y/system/exception.hpp"
-#include <unistd.h>
+
+#if defined(Y_WIN)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
 
 namespace Yttrium
 {
-    const char * const LocalFS:: CallSign = "LocalFS";
-    
-    LocalFS:: ~LocalFS() noexcept
-    {
-    }
+	const char * const LocalFS::CallSign = "LocalFS";
 
-    LocalFS:: LocalFS() noexcept :
-    Singleton<LocalFS>(),
-    VFS()
-    {
-    }
+	LocalFS:: ~LocalFS() noexcept
+	{
+	}
 
-    bool LocalFS:: TryRemove(const String &path)
-    {
-        Y_GIANT_LOCK();
+	LocalFS::LocalFS() noexcept :
+		Singleton<LocalFS>(),
+		VFS()
+	{
+	}
+
+	bool LocalFS::TryRemove(const String &path)
+	{
+		Y_GIANT_LOCK();
 #if defined(Y_BSD)
-        if( 0!=unlink( path() ) ) return false;
-        return true;
+		if (0 != unlink(path())) return false;
+		return true;
 #endif
 
-        throw Specific::Exception(CallSign,"TryRemove not implemented");
-    }
+#if defined(Y_WIN)
+		if (!::DeleteFile(path())) return false;
+		return true;
+#endif
+		throw Specific::Exception(CallSign, "TryRemove not implemented");
+	}
 
 
-    
+
 }
 
 #include "y/system/exception.hpp"
@@ -40,62 +48,126 @@ namespace Yttrium
 
 namespace Yttrium
 {
-    namespace
-    {
-        class LocalScanner : public VFS::Scanner
-        {
-        public:
-            explicit LocalScanner(const VFS &fsys, const String &dirName) :
-            VFS::Scanner(fsys), dir(Setup(dirName))
-            {
-            }
+	namespace
+	{
+		class LocalScanner : public VFS::Scanner
+		{
+		public:
+			explicit LocalScanner(const VFS &fsys, const String &dirName) :
+				VFS::Scanner(fsys), dir(Setup(dirName))
+			{
+			}
 
-            virtual ~LocalScanner() noexcept
-            {
-                assert(0!=dir);
-                closedir(dir);
-                dir=0;
-            }
+			virtual ~LocalScanner() noexcept
+			{
+				assert(0 != dir);
+				closedir(dir);
+				dir = 0;
+			}
 
-            DIR *dir;
+			DIR *dir;
 
-        private:
-            Y_DISABLE_COPY_AND_ASSIGN(LocalScanner);
+		private:
+			Y_DISABLE_COPY_AND_ASSIGN(LocalScanner);
 
-            static inline
-            DIR *Setup(const String &dirName)
-            {
-                Y_GIANT_LOCK();
-                DIR *d = opendir(dirName());
-                if(!d)
-                {
-                    throw Libc::Exception(errno,"opendir(%s)", dirName());
-                }
-                return d;
-            }
+			static inline
+				DIR *Setup(const String &dirName)
+			{
+				Y_GIANT_LOCK();
+				DIR *d = opendir(dirName());
+				if (!d)
+				{
+					throw Libc::Exception(errno, "opendir(%s)", dirName());
+				}
+				return d;
+			}
 
-            virtual VFS::Entry *get()
-            {
-                Y_GIANT_LOCK();
-                assert(0!=dir);
-                const dirent *dp = readdir(dir);
-                if(!dp) return 0;
-                return new VFS::Entry(fs,dp->d_name);
-            }
-        };
-    }
+			virtual VFS::Entry *get()
+			{
+				Y_GIANT_LOCK();
+				assert(0 != dir);
+				const dirent *dp = readdir(dir);
+				if (!dp) return 0;
+				return new VFS::Entry(fs, dp->d_name);
+			}
+		};
+	}
 }
 
 #endif
 
 
+#if defined(Y_WIN)
+#include "y/ptr/auto.hpp"
+
+namespace Yttrium
+{
+	String MakeWin32Path(const String &dirName)
+	{
+		String res = dirName;
+
+	}
+
+	class LocalScanner : public VFS::Scanner
+	{
+	public:
+		explicit LocalScanner(const VFS &fsys, const String &dirName) :
+			VFS::Scanner(fsys),
+			fData(),
+			hFind(INVALID_HANDLE_VALUE),
+			ready(TRUE)
+		{
+			Y_GIANT_LOCK();
+			std::cerr << "opening " << dirName << std::endl;
+			memset(&fData, 0, sizeof(fData));
+			hFind = ::FindFirstFile(dirName(), &fData);
+			if (INVALID_HANDLE_VALUE == hFind)
+			{
+				throw Win32::Exception(::GetLastError(), "FindFirstFile(%s)", dirName());
+			}
+
+		}
+
+		virtual ~LocalScanner() noexcept
+		{
+			::FindClose(hFind);
+			hFind = INVALID_HANDLE_VALUE;
+			memset(&fData, 0, sizeof(fData));
+		}
+
+		WIN32_FIND_DATA fData;
+		HANDLE          hFind;
+		int             ready;
+
+	private:
+		Y_DISABLE_COPY_AND_ASSIGN(LocalScanner);
+
+		virtual VFS::Entry *get()
+		{
+			Y_GIANT_LOCK();
+			if (ready)
+			{
+				std::cerr << "ready with " << fData.cFileName << std::endl;
+				AutoPtr<VFS::Entry> ep = new VFS::Entry(fs, fData.cFileName);
+				ready = ::FindNextFile(hFind, &fData);
+				if (ready) std::cerr << "still ready!" << std::endl;
+				return ep.yield();
+			}
+			else
+				return 0;
+		}
+	};
+
+}
+#endif
+
 namespace Yttrium
 {
 
 
-    VFS::Scanner *LocalFS:: OpenDirectory(const String &dirName)
-    {
-        return new LocalScanner(*this,dirName);
-    }
+	VFS::Scanner *LocalFS::OpenDirectory(const String &dirName)
+	{
+		return new LocalScanner(*this, dirName);
+	}
 
 }
