93ffefc5a6ae7cb5d1beac598d5f6db45f942581
Yann Bouret <yann.bouret@unice.fr>
2023-08-28 09:54:07 +0200

    wip

--- algo/nwsort.pl
+++ algo/nwsort.pl
@@ -20,11 +20,18 @@ for my $inputs (2..4)
     $nw->index_base([1 .. $inputs]);
     print $nw->title(), "\n";
      
-    $nw->formats([ "{%d,%d}," ]);
+    $nw->formats([ "%d,%d," ]);
     my $string = '{' . $nw->formatted();
     substr($string, -1, 1) = '}';    # Overwrite the trailing comma.
     print $string, ";\n";
    
     #print $nw->formatted(),"\n";
+
+    my $filename = "nw" . $inputs . ".hxx";
+    print $filename,  ";\n";
+    open(FH,'>',$filename);
+    print FH "const unsigned nwTable$inputs\[\]=", $string, ";\n";
+    print FH "const unsigned nwCount$inputs = sizeof(nwTable$inputs)/sizeof(nwTable$inputs\[0\])/2;\n";
+    close(FH)
 }
  
