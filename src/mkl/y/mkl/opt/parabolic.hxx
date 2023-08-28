d242d59082917f30984dc413f94566a1499bd168
Yann Bouret <yann.bouret@unice.fr>
2023-08-28 13:02:18 +0200

    wip

--- src/mkl/y/mkl/opt/parabolic.hxx
+++ src/mkl/y/mkl/opt/parabolic.hxx
@@ -1,10 +1,31 @@
 
 
+static inline real_t getBeta(const Triplet<real_t> &x)
+{
+    assert(x.isIncreasing());
+    if(x.b<=x.a)
+        return static_cast<real_t>(0);
+    else
+    {
+        if(x.c<=x.b)
+        {
+            return static_cast<real_t>(1);
+        }
+        else
+        {
+            assert(x.a<x.c);
+            return (x.b-x.a)/(x.c-x.a);
+        }
+    }
+}
+
 template <>
 void Parabolic<real_t>:: Step(Triplet<real_t> &x, Triplet<real_t> &f, FunctionType &F)
 {
-    assert(x.a<=x.b); assert(x.b<=x.c);
+    assert(x.isIncreasing());
     assert(f.isLocalMinimum());
+
+    const real_t beta = getBeta(x);
     
 
 }
