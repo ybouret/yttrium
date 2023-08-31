0de6e3b7630ab64032d806f5de5b4897423cdbf7
Yann Bouret <yann.bouret@unice.fr>
2023-08-31 08:56:15 +0200

    wip

--- src/core/tests/apex/mylar.cpp
+++ src/core/tests/apex/mylar.cpp
@@ -5,6 +5,7 @@
 #include "y/apex/mylar.hpp"
 #include "y/utest/run.hpp"
 #include "y/random/shuffle.hpp"
+#include "y/sequence/vector.hpp"
 
 #include <cstdio>
 
@@ -15,8 +16,19 @@ Y_UTEST(apex_mylar)
 {
     Random::Rand ran;
 
+    Vector<apq> Q;
+    std::cerr << "ComDen(" << Q << ")=" << Apex::Mylar::CommonDenominator(Q) << std::endl;
 
+    Q << apq(1,2);
+    std::cerr << "ComDen(" << Q << ")=" << Apex::Mylar::CommonDenominator(Q) << std::endl;
+    Q << apq(1,3);
+    std::cerr << "ComDen(" << Q << ")=" << Apex::Mylar::CommonDenominator(Q) << std::endl;
 
+    Q << apq(1,7);
+    std::cerr << "ComDen(" << Q << ")=" << Apex::Mylar::CommonDenominator(Q) << std::endl;
+
+    Apex::Mylar::Simplify(Q);
+    std::cerr << "Q=" << Q << std::endl;
 
 
 }
--- src/core/y/apex/mylar.hpp
+++ src/core/y/apex/mylar.hpp
@@ -7,7 +7,54 @@
 
 namespace Yttrium
 {
-    
+    namespace Apex
+    {
+
+        //! Mylar Yields Linear Algebra for Rationals
+        struct Mylar
+        {
+
+            template <typename ITERATOR> static inline
+            Natural CommonDenominator(ITERATOR curr, size_t n)
+            {
+                if(n<=0) return 1;
+                Natural den = (*curr).denom;
+                while(--n > 0)
+                {
+                    ++curr;
+                    den = Natural::LCM(den,(*curr).denom);
+                }
+                return den;
+            }
+
+            template <typename SEQUENCE> static inline
+            Natural CommonDenominator(SEQUENCE &seq)
+            {
+                return CommonDenominator(seq.begin(),seq.size());
+            }
+
+            template <typename ITERATOR> static inline
+            void Simplify(ITERATOR curr, size_t n)
+            {
+                const Natural den = CommonDenominator(curr,n);
+                while(n-- > 0)
+                {
+                    (*curr) *= den;
+                    ++curr;
+                }
+            }
+
+            template <typename SEQUENCE> static inline
+            void Simplify(SEQUENCE &seq)
+            {
+                Simplify(seq.begin(),seq.size());
+            }
+
+
+        };
+
+    }
+
 }
 
 #endif
