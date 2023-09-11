79a2d1767448a4e2d4926d7e47dc4fd4449d02e0
Yann Bouret <yann.bouret@unice.fr>
2023-09-11 09:02:18 +0200

    wip ode, need to debug calls

--- src/mkl/tests/ode/odeint.cpp
+++ src/mkl/tests/ode/odeint.cpp
@@ -21,7 +21,7 @@ namespace
 
         inline void compute(Writable<T> &dydt, double, const Readable<T> &y)
         {
-            dydt[1] = k * y[1];
+            dydt[1] = -Fabs<T>::Of(k) * y[1];
         }
 
 
@@ -35,12 +35,34 @@ namespace
 Y_UTEST(odeint)
 {
 
-    dExp<double> dd(-1.2);
+    dExp<double>                   dd(1.2);
     ODE::Named<double>::Equation   drvs( &dd, & dExp<double>::compute );
     ODE::RK45_Step<double>::Handle step = new ODE::CashKarp<double>();
     ODE::RK45<double>              rk45(step);
     ODE::Integrator<double>        odeint(1e-7);
-    
+
+    Vector<double> y(1,2.1);
+    double         h    = 1e-4;
+    const double   xini = 0;
+    const double   xend = 10;
+    const size_t   np   = 100;
+
+    Libc::OutputFile fp("exp.dat");
+    fp("%.15g %.15g\n",xini,y[1]);
+    for(size_t i=1;i<=np;++i)
+    {
+        const double x1 = xini + (i-1)*(xend-xini)/double(np);
+        const double x2 = xini + i*(xend-xini)/double(np);
+        odeint.run(y, x1, x2, h, drvs, 0, rk45);
+        fp("%.15g %.15g\n",x2,y[1]);
+    }
+
+
+
+
+
+
+
 
 }
 Y_UDONE()
