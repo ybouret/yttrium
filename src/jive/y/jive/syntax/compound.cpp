1eb317fcfd0ae88ed823b16a931c0624b73cd8ba
Yann Bouret <yann.bouret@unice.fr>
2023-10-05 13:57:21 +0200

    wip

---
+++ src/jive/y/jive/syntax/compound.cpp
@@ -0,0 +1,21 @@
+
+#include "y/jive/syntax/compound.hpp"
+
+namespace Yttrium
+{
+    namespace Jive
+    {
+
+        namespace Syntax
+        {
+
+            Compound:: ~Compound() noexcept
+            {
+            }
+
+            
+        }
+
+    }
+
+}
---
+++ src/jive/y/jive/syntax/compound.hpp
@@ -0,0 +1,52 @@
+
+//! \file
+
+#ifndef Y_Jive_Syntax_Compound_Included
+#define Y_Jive_Syntax_Compound_Included 1
+
+#include "y/jive/syntax/internal.hpp"
+#include "y/data/small/light/list/bare.hpp"
+
+namespace Yttrium
+{
+    namespace Jive
+    {
+
+        namespace Syntax
+        {
+            typedef Small::BareLightList<const Rule> Manifest;
+
+            //__________________________________________________________________
+            //
+            //
+            //
+            //! Base class for Compound Rule
+            //
+            //
+            //__________________________________________________________________
+            class Compound : public Internal, public Manifest
+            {
+            public:
+                virtual ~Compound() noexcept; //!< cleanup
+
+
+            protected:
+                //! forward name and uuid
+                template <typename ID>
+                explicit Compound(const ID &id, const uint32_t dw) : 
+                Internal(id,dw),
+                Manifest()
+                {
+                }
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Compound);
+            };
+
+        }
+
+    }
+
+}
+
+#endif
