eaaffa5f86f6c7c0bbeb60ea5a43474a1b4b3626
Yann Bouret <yann.bouret@gmail.com>
2023-10-15 20:42:29 +0200

    wip

--- src/chem/y/chem/plexus/cluster/connect-all.cpp
+++ src/chem/y/chem/plexus/cluster/connect-all.cpp
@@ -5,11 +5,28 @@ namespace Yttrium
 {
     namespace Chemical
     {
-        void Cluster:: connectAll(XMLog &xml, Equilibria &all)
+
+        static inline String MakeName(const EqArray &                 eqs,
+                                      const Algebraic::Coefficients & cof)
+        {
+
+            return String();
+        }
+
+        void Cluster:: connectAll(XMLog &      xml,
+                                  Equilibria & all)
         {
             Y_XML_SECTION(xml, "ConnectAll");
             Chemical::Algebraic::Weight::List W;
             Chemical::Algebraic::Compute(W,Nu,xml);
+
+            for(const Algebraic::Weight *w=W.head;w;w=w->next)
+            {
+                std::cerr << *w << std::endl;
+                const String eid = MakeName(*edb,*w);
+
+            }
+
         }
     }
 
