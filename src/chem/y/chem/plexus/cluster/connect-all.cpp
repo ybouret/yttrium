a69a9e3b9613df29298443d9185171a5a06fad5b
Yann Bouret <yann.bouret@unice.fr>
2023-10-16 08:07:15 +0200

    wip

--- src/chem/y/chem/plexus/cluster/connect-all.cpp
+++ src/chem/y/chem/plexus/cluster/connect-all.cpp
@@ -6,11 +6,55 @@ namespace Yttrium
     namespace Chemical
     {
 
+
+        static inline String FirstCof(const int nu)
+        {
+            switch(nu)
+            {
+                case -1: return String('-');
+                case  1: return String();
+            }
+            assert(abs(nu)>1);
+            return FormatString("%d*",nu);
+        }
+
+        static inline String ExtraCof(const int nu)
+        {
+            switch(nu)
+            {
+                case -1: return String('-');
+                case  1: return String('+');
+            }
+            assert(abs(nu)>1);
+            if(nu < -1)
+                return FormatString("%d*",nu);
+            assert(nu>1);
+            return FormatString("+%d*",nu);
+        }
+
         static inline String MakeName(const EqArray &                 eqs,
                                       const Algebraic::Coefficients & cof)
         {
+            assert(eqs.size()>=cof.size());
+            String name;
+            bool   first = true;
+            for(size_t i=1;i<=cof.size();++i)
+            {
+                const int nu = cof[i];
+                if(0==nu) continue;
+                if(first)
+                {
+                    name += FirstCof(nu);
+                    first = false;
+                }
+                else
+                {
+                    name += ExtraCof(nu);
+                }
+                name += eqs[i]->name;
+            }
 
-            return String();
+            return name;
         }
 
         void Cluster:: connectAll(XMLog &      xml,
@@ -22,9 +66,9 @@ namespace Yttrium
 
             for(const Algebraic::Weight *w=W.head;w;w=w->next)
             {
-                std::cerr << *w << std::endl;
-                const String eid = MakeName(*edb,*w);
 
+                const String eid = MakeName(*edb,*w);
+                std::cerr << *w << " #" << w->nEqs << " => " << eid << std::endl;
             }
 
         }
