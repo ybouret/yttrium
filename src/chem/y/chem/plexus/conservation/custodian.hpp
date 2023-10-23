1213864ca094a5c66659f4fe56cb46af163c7b6b
Yann Bouret <yann.bouret@unice.fr>
2023-10-23 15:15:07 +0200

    wip

---
+++ src/chem/y/chem/plexus/conservation/custodian.hpp
@@ -0,0 +1,19 @@
+
+//! \file
+
+#ifndef Y_Chemical_Conservation_Custodian_Included
+#define Y_Chemical_Conservation_Custodian_Included 1
+
+#include "y/chem/plexus.hpp"
+#include "y/stream/xmlog.hpp"
+
+namespace Yttrium
+{
+    namespace Chemical
+    {
+    }
+
+}
+
+#endif
+
