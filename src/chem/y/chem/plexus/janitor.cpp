5cf97c9889b2b1a700056f020f8e29b0520736c4
Yann Bouret <yann.bouret@unice.fr>
2023-10-23 08:54:59 +0200

    wip

--- src/chem/y/chem/plexus/janitor.cpp
+++ src/chem/y/chem/plexus/janitor.cpp
@@ -1,5 +1,6 @@
 
 #include "y/chem/plexus/janitor.hpp"
+#include "y/data/small/heavy/list/coop.hpp"
 
 namespace Yttrium
 {
@@ -20,45 +21,73 @@ namespace Yttrium
         public:
             xreal   xi; //!< xi value
 
-            Cursor(const SpProxy &_) : SpStrip(_), xi(0) {}
-            ~Cursor() noexcept {}
+            inline explicit Cursor(const SpProxy &_) : SpStrip(_), xi(0) {}
+            inline          Cursor(const Cursor  &cursor) : SpStrip(cursor), xi(cursor.xi) {}
+            inline virtual ~Cursor() noexcept {}
 
         private:
-            Y_DISABLE_COPY_AND_ASSIGN(Cursor);
+            Y_DISABLE_ASSIGN(Cursor);
+        };
+
+        typedef Small::CoopHeavyList<Cursor> Cursors;
+        typedef Cursors::ProxyType           CursorsProxy;
+
+        class Inquiry
+        {
+        public:
+            inline explicit Inquiry(const CursorsProxy &_) noexcept : limiting(_), equating(_) { }
+            inline virtual ~Inquiry()                      noexcept {}
+
+
+            Cursors limiting;
+            Cursors equating;
+
+
+            inline void probe(const Actors          &actors,
+                              const Readable<xreal> &Corg,
+                              const Booleans        &kept)
+            {
+                limiting.free();
+                equating.free();
+                for(const Actor *a=actors.head;a;a=a->next)
+                {
+                    const Species &sp = a->sp;
+                    if(!kept[sp.indx[SubLevel]]) continue;
+                    const double c = Corg[sp.indx[TopLevel]];
+                    if(c>=0)
+                    {
+                        // limit
+                    }
+                    else
+                    {
+                        // slide
+                    }
+                }
+            }
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Inquiry);
         };
 
 
-        static inline void Probe(const Actors          &actors,
-                                 const Readable<xreal> &Corg,
-                                 const Booleans        &kept)
-        {
-            for(const Actor *a=actors.head;a;a=a->next)
-            {
-                const Species &sp = a->sp;
-                if(!kept[sp.indx[SubLevel]]) continue;
-                const double c = Corg[sp.indx[TopLevel]];
-                if(c>=0)
-                {
-                    // limit
-                }
-                else
-                {
-                    // slide
-                }
-            }
-        }
 
         void Janitor:: process(const Cluster   &cluster,
                                Writable<xreal> &Corg,
                                XMLog           &xml)
         {
             Y_XML_SECTION(xml, "Janitor::Cluster");
+
+            CursorsProxy cproxy;
+            Inquiry      reac(cproxy);
+            Inquiry      prod(cproxy);
+
             const Booleans &kept = *cluster.kept;
             const EqRepo &repo = cluster.army->definite;
             for(const EqNode *en=repo.head;en;en=en->next)
             {
                 const Equilibrium &eq = **en;
-                Probe(eq.reac,Corg,kept);
+                reac.probe(eq.reac, Corg, kept);
+                prod.probe(eq.prod, Corg, kept);
             }
 
 
