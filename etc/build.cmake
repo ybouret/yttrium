59336a73df573dd1ef87576393f67644570a0b38
Yann Bouret <yann.bouret@unice.fr>
2023-10-26 11:45:32 +0200

    wip

--- etc/build.cmake
+++ etc/build.cmake
@@ -1,24 +1,37 @@
 file(GLOB buildDirs "build*")
 foreach(buildDir ${buildDirs})
+	#-------------------------------------------------------------------
+	#
+	# Loop over build directories
+	#
+	#-------------------------------------------------------------------
 	if(IS_DIRECTORY ${buildDir})
 		message(STATUS "processing ${buildDir}")
-		set(BUILD OFF)
-		set(TGT  "all")
+		set(BUILD OFF)	 
 		
+		# Ninja
 		if(EXISTS "${buildDir}/build.ninja")
 			set(BUILD ON)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all"
+							COMMAND_ECHO      STDERR)
 		endif()
 		
+		# Makefile
 		if(EXISTS "${buildDir}/Makefile")
 			set(BUILD ON)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all"
+							COMMAND_ECHO      STDERR)
+		endif()
+		
+		# Visual Studio
+		if(EXISTS "${buildDir}/yttrium.sln")
+			set(BUILD_ON)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "ALL_BUILD" "-C" "Release"
+							COMMAND_ECHO      STDERR)
 		endif()
 		
 		if(NOT BUILD)
 			message( STATUS "*** build system not recognized" )
-		else()
- 			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target ${TGT}"
-							#WORKING_DIRECTORY ${buildDir}
-							COMMAND_ECHO      STDERR)
 		endif()
 			
 		
