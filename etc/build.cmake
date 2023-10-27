fe1425863b37fa66390375770674dcc8e9212430
Yann Bouret <yann.bouret@unice.fr>
2023-10-27 15:21:35 +0200

    wip

--- etc/build.cmake
+++ etc/build.cmake
@@ -18,25 +18,25 @@ foreach(buildDir ${buildDirs})
 		message(STATUS "processing ${buildDir}")
 		set(BUILD OFF)	 
 		
+		# update build directory	
+		execute_process(COMMAND "${CMAKE_COMMAND}" "${buildDir}")
+		 
 		# Ninja
 		if(EXISTS "${buildDir}/build.ninja")
 			set(BUILD ON)
-			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all" "--parallel" ${N}
-							COMMAND_ECHO      STDERR)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all" "--parallel" ${N})
 		endif()
 		
 		# Makefile
 		if(EXISTS "${buildDir}/Makefile")
 			set(BUILD ON)
-			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all" "--parallel" ${N}
-							COMMAND_ECHO      STDERR)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "all" "--parallel" ${N})
 		endif()
 		
 		# Visual Studio
 		if(EXISTS "${buildDir}/yttrium.sln")
 			set(BUILD ON)
-			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "ALL_BUILD" "--config" "Release" #"--parallel" ${N}
-							COMMAND_ECHO      STDERR)
+			execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "${buildDir}" "--target" "ALL_BUILD" "--config" "Release" "--parallel" ${N})
 		endif()
 		
 		if(NOT BUILD)
