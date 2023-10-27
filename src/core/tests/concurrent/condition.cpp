b7c16d8469cd05340a8ffa392269ba9435d6c482
Yann Bouret <yann.bouret@unice.fr>
2023-10-27 15:24:59 +0200

    wip

---
+++ src/core/tests/concurrent/condition.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+#include "y/concurrent/condition.hpp"
+#include "y/utest/run.hpp"
+
+using namespace Yttrium;
+
+
+
+Y_UTEST(concurrent_condition)
+{
+
+    Concurrent::Condition cond;
+    
+}
+Y_UDONE()
+
