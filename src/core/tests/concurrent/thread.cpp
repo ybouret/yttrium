488001d192b5766e31524240f28ac76278643f59
Yann Bouret <yann.bouret@unice.fr>
2023-11-06 09:24:28 +0100

    wip

--- src/core/tests/concurrent/thread.cpp
+++ src/core/tests/concurrent/thread.cpp
@@ -1,103 +1,39 @@
-
-
-
-
 #include "y/concurrent/mutex.hpp"
 #include "y/concurrent/thread.hpp"
 #include "y/utest/run.hpp"
-#include "y/system/exception.hpp"
-#include "y/system/error.hpp"
-#include <cerrno>
 
-#if 0
-#if defined(Y_BSD)
-#include <pthread.h>
-using namespace Yttrium;
-
-
-
-class Thread : public Concurrent::NucleusMutex
-{
-public:
-    typedef void (*Proc)(void *);
-
-
-
-
-    Thread(Proc userProc, void *userArgs) :
-    Concurrent::NucleusMutex(),
-    handle(),
-    proc(userProc),
-    args(userArgs)
-    {
-        assert(0!=proc);
-        Y_LOCK(*this);
-        (std::cerr << "creating thread..." << std::endl).flush();
-        const int err = pthread_create(&handle,NULL,launch,this);
-        if(0!=err) throw Libc::Exception(errno,"pthread_create");
-    }
-
-    virtual ~Thread() noexcept
-    {
-        {
-            Y_LOCK(*this);
-            (std::cerr << "waiting for thread..." << std::endl).flush();
-        }
-        void     *ret = 0;
-        const int err = pthread_join(handle,&ret);
-        if(0!=err) Libc::CriticalError(err, "pthread_join");
-    }
-
-    pthread_t handle;
-
-
-    static void * launch(void *param) noexcept
-    {
-        assert(0!=param);
-        static_cast<Thread *>(param)->call();
-        return 0;
-    }
-
-    Proc               proc;
-    void              *args;
-
-    void call() noexcept
-    {
-        try
-        {
-
-            assert(0!=proc);
-            proc(args);
-        }
-        catch(...)
-        {
-        }
-    }
-};
-
-#endif
-
-#endif
 
 using namespace Yttrium;
 
 
-
-static inline void theProc(void *args)
+namespace
 {
-    assert(0!=args);
-    Lockable &access = *(Lockable *)args;
 
-    Y_LOCK(access);
-    std::cerr << "in thread..." << std::endl;
+    struct Param
+    {
+        Lockable &access;
+        size_t    value;
+    };
+
+    static inline void theProc(void *args)
+    {
+        assert(0!=args);
+        Param &param = *static_cast<Param *>(args);
+
+        Y_LOCK(param.access);
+        std::cerr << "in thread #" << param.value << std::endl;
+    }
+
 }
-
 
 Y_UTEST(concurrent_thread)
 {
 
     Lockable          &access = Lockable::Giant();
-    Concurrent::Thread thr(theProc,&access);
+    Param              p1 = { access, 1 };
+    Param              p2 = { access, 2 };
+    Concurrent::Thread t1(theProc,&p1);
+    Concurrent::Thread t2(theProc,&p2);
 
 }
 Y_UDONE()
--- src/core/y/concurrent/thread.hxx
+++ src/core/y/concurrent/thread.hxx
@@ -3,138 +3,134 @@
 
 namespace Yttrium
 {
-	namespace Concurrent
-	{
-		namespace Nucleus
-		{
+    namespace Concurrent
+    {
+        namespace Nucleus
+        {
 
-			//__________________________________________________________________
-			//
-			//
-			//! native thread wrapper
-			//
-			//__________________________________________________________________
-			class Thread
-			{
-
-			public:
-
-				//______________________________________________________________
-				//
-				//
-				//! start thread
-				//
-				//______________________________________________________________
-				explicit Thread(ThreadProc proc, void *args) : thr(0),
-#if defined(Y_WIN)
-					tid(0),
+#if defined(Y_BSD)
+#define Y_THREAD pthread_t
+#define Y_LAUNCH() void * launch(void *param)
 #endif
-					myProc(proc),
-					myArgs(args)
 
-				{
-					assert(0 != proc);
+#if defined(Y_WIN)
+#define Y_THREAD HANDLE
+#define Y_LAUNCH WORD WINAPI launch(LPVOID param)
+#endif
+
+            //__________________________________________________________________
+            //
+            //
+            //! native thread wrapper
+            //
+            //__________________________________________________________________
+            class Thread
+            {
+
+            public:
+
+                //______________________________________________________________
+                //
+                //
+                //! start thread
+                //
+                //______________________________________________________________
+                explicit Thread(ThreadProc proc, void *args) : thr(0),
+#if defined(Y_WIN)
+                tid(0),
+#endif
+                myProc(proc),
+                myArgs(args)
+
+                {
+                    assert(0 != proc);
                     Y_GIANT_LOCK();
 
 #if defined(Y_BSD)
-					const int err = pthread_create(&thr, NULL, launch, this);
-					if (0 != err) throw Libc::Exception(err, "pthread_create");
+                    const int err = pthread_create(&thr, NULL, launch, this);
+                    if (0 != err) throw Libc::Exception(err, "pthread_create");
 #endif
 
 #if defined(Y_WIN)
-					thr = CreateThread(
-						NULL,        // default security attributes
-						0,           // use default stack size  
-						launch,      // thread function name
-						this,        // argument to thread function 
-						0,           // use default creation flags 
-						&tid);       // returns the thread identifier 
-					if (0 == thr) throw Win32::Exception(::GetLastError(), "::CreateThread");
+                    thr = CreateThread(
+                                       NULL,        // default security attributes
+                                       0,           // use default stack size
+                                       launch,      // thread function name
+                                       this,        // argument to thread function
+                                       0,           // use default creation flags
+                                       &tid);       // returns the thread identifier
+                    if (0 == thr) throw Win32::Exception(::GetLastError(), "::CreateThread");
 #endif
-				}
+                }
 
-				//______________________________________________________________
-				//
-				//
-				//! quit thread
-				//
-				//______________________________________________________________
-				inline ~Thread() noexcept
-				{
+                //______________________________________________________________
+                //
+                //
+                //! quit thread
+                //
+                //______________________________________________________________
+                inline ~Thread() noexcept
+                {
 #if defined(Y_BSD)
-					void     *ret = 0;
-					const int err = pthread_join(thr, &ret);
-					if (0 != err) Libc::CriticalError(err, "pthread_join");
+                    void     *ret = 0;
+                    const int err = pthread_join(thr, &ret);
+                    if (0 != err)
+                        Libc::CriticalError(err, "pthread_join");
 #endif
 
 #if defined(Y_WIN)
-					if (WAIT_OBJECT_0 != WaitForSingleObject(thr, INFINITE))
-						Win32::CriticalError(::GetLastError(), "WaitForSingleObject");
-
+                    if (WAIT_OBJECT_0 != WaitForSingleObject(thr, INFINITE))
+                        Win32::CriticalError(::GetLastError(), "WaitForSingleObject");
 #endif
 
-				}
+                }
 
-				//______________________________________________________________
-				//
-				//
-				// Members
-				//
-				//______________________________________________________________
-#if defined(Y_BSD)
-				pthread_t thr;
-#endif
-
+                //______________________________________________________________
+                //
+                //
+                // Members
+                //
+                //______________________________________________________________
+                Y_THREAD  thr;
 #if defined(Y_WIN)
-				HANDLE    thr;
-				DWORD     tid;
+                DWORD     tid;
 #endif
 
-			private:
-				Y_DISABLE_COPY_AND_ASSIGN(Thread);
-				ThreadProc  const myProc;
-				void      * const myArgs;
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Thread);
+                ThreadProc  const myProc;
+                void      * const myArgs;
 
-				//______________________________________________________________
-				//
-				//
-				//! wrapper
-				//
-				//______________________________________________________________
-				inline void call() noexcept
-				{
-					try
-					{
-						assert(0 != myProc);
-						myProc(myArgs);
-					}
-					catch (...)
-					{
+                //______________________________________________________________
+                //
+                //
+                //! wrapper
+                //
+                //______________________________________________________________
+                inline void call() noexcept
+                {
+                    try
+                    {
+                        assert(0 != myProc);
+                        myProc(myArgs);
+                    }
+                    catch (...)
+                    {
 
-					}
+                    }
 
-				}
+                }
+                
+                static inline Y_LAUNCH() noexcept
+                {
+                    assert(0 != param);
+                    static_cast<Thread *>(param)->call();
+                    return 0;
+                }
 
-#if defined(Y_BSD)
-				static inline void * launch(void *param) noexcept
-				{
-					assert(0 != param);
-					static_cast<Thread *>(param)->call();
-					return 0;
-				}
-#endif
+            };
+        }
 
-#if defined(Y_WIN)
-				static inline DWORD WINAPI launch(LPVOID param) noexcept
-				{
-					assert(0 != param);
-					static_cast<Thread *>(param)->call();
-					return 0;
-				}
-#endif
-			};
-		}
-
-	}
+    }
 
 }
