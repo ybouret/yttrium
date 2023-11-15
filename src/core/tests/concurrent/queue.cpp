e2cc3f8d0fe5e37ce17d117a3db43efab81ed24f
Yann Bouret <yann.bouret@gmail.com>
2023-11-15 10:23:11 +0100

    wip

--- src/core/tests/concurrent/queue.cpp
+++ src/core/tests/concurrent/queue.cpp
@@ -9,6 +9,7 @@
 #include "y/data/list/cxx.hpp"
 #include "y/random/bits.hpp"
 #include "y/text/ascii/convert.hpp"
+#include "y/functor.hpp"
 
 using namespace Yttrium;
 
@@ -16,13 +17,82 @@ namespace Yttrium
 {
     namespace Concurrent
     {
-        typedef uint32_t JobID;
+
+
+        class Context
+        {
+        public:
+            explicit Context(Lockable &mx, const size_t sz, const size_t rk) noexcept;
+            explicit Context(Lockable &mx) noexcept; //!< sz=1, rk=0
+            virtual ~Context() noexcept;
+            Context(const Context &) noexcept;
+
+            Lockable    &sync; //!< shared persistent  mutex
+            const size_t size; //!< size>0
+            const size_t rank; //!< 0..size-1
+            const size_t indx; //!< 1..size
+
+        private:
+            Y_DISABLE_ASSIGN(Context);
+        };
+
+        Context:: Context(Lockable &mx, const size_t sz, const size_t rk) noexcept :
+        sync(mx),
+        size(sz),
+        rank(rk),
+        indx(rank+1)
+        {
+            assert(size>0); assert(rank<size);
+        }
+
+        Context:: Context(Lockable &mx) noexcept :
+        sync(mx),
+        size(1),
+        rank(0),
+        indx(1)
+        {
+
+        }
+
+        Context:: ~Context() noexcept {}
+
+        Context:: Context(const Context &ctx) noexcept :
+        sync(ctx.sync),
+        size(ctx.size),
+        rank(ctx.rank),
+        indx(ctx.indx)
+        {
+        }
+
+        typedef int64_t                            JobID;
+        typedef Functor<void,TL1(const Context &)> Job;
+
+        class Demo
+        {
+        public:
+            inline  Demo(int i) noexcept : code(i) {}
+            inline ~Demo() noexcept {}
+            Demo(const Demo &demo) noexcept : code(demo.code) {}
+
+            const int code;
+
+            inline void operator()(const Context &ctx)
+            {
+                {
+                    Y_LOCK(ctx.sync);
+                    std::cerr << "called by " << ctx.size << "." << ctx.rank << std::endl;
+                    std::cerr << "code=" << code << std::endl;
+                }
+            }
+
+        private:
+            Y_DISABLE_ASSIGN(Demo);
+        };
 
 
         class Worker : public Object
         {
         public:
-
             Worker *next;
             Worker *prev;
         private:
@@ -69,7 +139,11 @@ Y_UTEST(concurrent_queue)
     const Concurrent::ThreadHandle th = Concurrent::Thread::CurrentHandle();
     std::cerr << "Master Thread   @" << th << std::endl;
     Y_SIZEOF(Concurrent::ThreadHandle);
-    
+
+    Concurrent::Demo demo = 7;
+    Concurrent::Job j = demo;
+    const Concurrent::Context   solo( Lockable::Giant() );
+    j(solo);
 }
 Y_UDONE()
 
