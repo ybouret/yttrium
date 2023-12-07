4e9a495f44fc86b67005e68e3d8417d9ac3b9b98
Yann Bouret <yann.bouret@unice.fr>
2023-12-07 13:55:09 +0100

    wip

--- src/mkl/tests/test-tao.cpp
+++ src/mkl/tests/test-tao.cpp
@@ -1,5 +1,5 @@
-#include "y/mkl/tao/level1.hpp"
-#include "y/mkl/tao/carver.hpp"
+#include "y/mkl/tao/seq/level1.hpp"
+#include "y/mkl/tao/par/level1.hpp"
 
 #include "y/concurrent/loop/crew.hpp"
 #include "y/concurrent/loop/mono.hpp"
@@ -16,64 +16,7 @@ using namespace MKL;
 
 namespace
 {
-#if 0
-    class TaoRes1D : public Concurrent::Resource1D<size_t>
-    {
-    public:
-        explicit TaoRes1D() noexcept {}
-        virtual ~TaoRes1D() noexcept {}
-
-    private:
-        Y_DISABLE_COPY_AND_ASSIGN(TaoRes1D);
-
-        virtual void attach(const Concurrent::ThreadContext &)
-        {
-
-        }
-    };
-
-
-    class TaoRes2D : public Concurrent::Resource2D<size_t>
-    {
-    public:
-        explicit TaoRes2D() noexcept {}
-        virtual ~TaoRes2D() noexcept {}
-
-    private:
-        Y_DISABLE_COPY_AND_ASSIGN(TaoRes2D);
-
-        virtual void attach(const Concurrent::ThreadContext &)
-        {
-
-        }
-    };
-#endif
-
-
-#if 0
-    template <typename TARGET, typename SOURCE>   inline
-    void Load(TARGET &target, SOURCE &source, Concurrent::SIMD &simd)
-    {
-        assert(target.size()<=source.size());
-        struct Work
-        {
-            void operator()(const Concurrent::Range &range, TARGET &target, SOURCE &source)
-            {
-                for(size_t i=range.offset,k=range.length;k>0;--k,++i)
-                {
-                    target[i] = source[i];
-                }
-            }
-        };
-
-        Work todo = {};
-        simd(todo,target,source);
-
-
-        // for(size_t i=target.size();i>0;--i)             target[i] = source[i];
-    }
-#endif
-
+    
 }
 
 Y_UTEST(tao)
---
+++ src/mkl/y/mkl/tao/par/level1.hpp
@@ -0,0 +1,25 @@
+
+#ifndef Y_MKL_ParTao1_Included
+#define Y_MKL_ParTao1_Included 1
+
+
+#include "y/mkl/api.hpp"
+#include "y/mkl/antelope/add.hpp"
+#include "y/mkl/tao/carver.hpp"
+
+namespace Yttrium
+{
+    namespace MKL
+    {
+
+        namespace Tao
+        {
+
+        }
+
+    }
+
+}
+
+#endif
+
