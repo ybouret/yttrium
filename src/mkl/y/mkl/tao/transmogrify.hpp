461614312fd22b7c6644808552f3d31218042687
Yann Bouret <yann.bouret@unice.fr>
2023-12-14 13:53:15 +0100

    wip

--- src/mkl/y/mkl/tao/transmogrify.hpp
+++ src/mkl/y/mkl/tao/transmogrify.hpp
@@ -9,34 +9,56 @@ namespace Yttrium
 {
     namespace MKL
     {
+        //! \namespace Tao
         namespace Tao
         {
-
-
+            //__________________________________________________________________
+            //
+            //! Relationship between target and source
+            //__________________________________________________________________
             enum Relationship
             {
-                IsSubClassOf,
-                MustCastFrom
+                IsSubClassOf, //!<   Y_Is_SuperSubClass(SOURCE,TARGET)
+                MustCastFrom  //!< ! Y_Is_SuperSubClass(SOURCE,TARGET)
             };
 
+            //! \namespace Cog
             namespace Cog
             {
+                //______________________________________________________________
+                //
+                //
+                //! base class for info
+                //
+                //______________________________________________________________
                 class TransmogrifyInfo
                 {
                 public:
-                    static const char *ToText(const Relationship) noexcept;
-                    inline explicit TransmogrifyInfo() noexcept {}
-                    inline virtual ~TransmogrifyInfo() noexcept {}
+                    static const char *ToText(const Relationship) noexcept; //!< human readable
+
+                    inline explicit TransmogrifyInfo() noexcept {} //!< do nothing
+                    inline virtual ~TransmogrifyInfo() noexcept {} //!< do nothing
                 private:
                     Y_DISABLE_COPY_AND_ASSIGN(TransmogrifyInfo);
                 };
             }
 
+            //__________________________________________________________________
+            //
+            //
+            //! specific info
+            //
+            //__________________________________________________________________
             template <Relationship R> class TransmogrifyInfo : public Cog::TransmogrifyInfo
             {
             public:
-                static const Relationship Status = R;
-                static inline const char *Text() noexcept { return ToText(R); }
+                static const Relationship Status = R;                           //!< alias
+                static inline const char *Text() noexcept { return ToText(R); } //!< human readable
+
+                inline explicit TransmogrifyInfo() noexcept {} //!< do nothing
+                inline virtual ~TransmogrifyInfo() noexcept {} //!< do nothing
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(TransmogrifyInfo);
             };
 
 
@@ -45,8 +67,12 @@ namespace Yttrium
             {
                 template <typename TARGET,Relationship,typename SOURCE> class To;
 
-
+                //______________________________________________________________
+                //
+                //
                 //! TARGET is sub-class of SOURCE => untouched
+                //
+                //______________________________________________________________
                 template <typename TARGET,typename SOURCE>
                 class To<TARGET,IsSubClassOf,SOURCE> : public Tao::TransmogrifyInfo<IsSubClassOf>
                 {
@@ -54,14 +80,19 @@ namespace Yttrium
                     typedef TARGET & ReturnType;
                     static inline ReturnType  Get(const SOURCE &source) noexcept { return source; }
 
-                    inline explicit To() noexcept {}
-                    inline virtual ~To() noexcept {}
+                    inline explicit To() noexcept {} //!< do nothing
+                    inline virtual ~To() noexcept {} //!< do nothing
 
                 private:
                     Y_DISABLE_COPY_AND_ASSIGN(To);
                 };
 
+                //______________________________________________________________
+                //
+                //
                 //! TARGET does not derive from SOURCE => cast
+                //
+                //______________________________________________________________
                 template <typename TARGET, typename SOURCE>
                 class To<TARGET,MustCastFrom,SOURCE> : public Tao::TransmogrifyInfo<MustCastFrom>
                 {
@@ -69,8 +100,8 @@ namespace Yttrium
                     typedef TARGET ReturnType;
                     static inline  ReturnType Get(const SOURCE &source) noexcept { return TARGET(source); }
 
-                    inline explicit To() noexcept {}
-                    inline virtual ~To() noexcept {}
+                    inline explicit To() noexcept {} //!< do nothing
+                    inline virtual ~To() noexcept {} //!< do nothing
 
                 private:
                     Y_DISABLE_COPY_AND_ASSIGN(To);
@@ -78,28 +109,49 @@ namespace Yttrium
             }
 
 
+            //__________________________________________________________________
+            //
+            //
+            //! Relation TARGET/SOURCE
+            //
+            //__________________________________________________________________
             template <typename TARGET, typename SOURCE>
             struct Relation
             {
+                //! alias
                 static const Relationship Status = Y_Is_SuperSubClass(SOURCE,TARGET) ? IsSubClassOf : MustCastFrom;
             };
 
+
+            //__________________________________________________________________
+            //
+            //
+            //! Low-Level info and conversions
+            //
+            //__________________________________________________________________
             template <typename TARGET,typename SOURCE>
             class To : public Cog::To<TARGET, Relation<TARGET,SOURCE>::Status, SOURCE>
             {
             public:
-
-                inline explicit To() noexcept {}
-                inline virtual ~To() noexcept {}
+                inline explicit To() noexcept {} //!< do nothing
+                inline virtual ~To() noexcept {} //!< do nothing
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(To);
             };
 
+
+            //__________________________________________________________________
+            //
+            //
+            //! High-Level operations
+            //
+            //__________________________________________________________________
             template <typename RES, typename LHS, typename RHS>
             struct Transmogrify
             {
 
+                //! product
                 static inline RES Product(const LHS &lhs,
                                           const RHS &rhs)
                 {
