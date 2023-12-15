60f76e1dd2f07f02b6a6a7f92c5186510bdabe15
Yann Bouret <yann.bouret@unice.fr>
2023-12-15 09:37:45 +0100

    wip

--- src/mkl/y/mkl/tao/par/level2.hpp
+++ src/mkl/y/mkl/tao/par/level2.hpp
@@ -48,16 +48,74 @@ namespace Yttrium
             {
                 assert( target.size() == M.rows );
                 assert( source.size() == M.cols );
+              
                 typedef typename TARGET::Type TGT;
                 typedef void    (*PROC)(TGT &, const U &);
+                
                 static  PROC   proc = Ops<typename TARGET::Type,U>::Set;
                 const size_t   para = engine.in1D.size();
+                
                 engine.setup(M.rows);                      // process rows in parallel
                 engine.in1D.attach(xma.make(para,M.cols)); // with help
+                
                 volatile Engine::Clean1D willClean(engine.in1D);
                 engine.in1D(Parallel::Mul<TARGET,T,SOURCE,U,PROC>,target,M,source,proc);
-
             }
+
+            //__________________________________________________________________
+            //
+            //! target += M*source
+            //__________________________________________________________________
+            template <typename TARGET, typename T, typename SOURCE, typename U> inline
+            void MulAdd(TARGET &          target,
+                        const Matrix<T>  &M,
+                        SOURCE           &source,
+                        MultiAdd<U>      &xma,
+                        Engine           &engine)
+            {
+                assert( target.size() == M.rows );
+                assert( source.size() == M.cols );
+                
+                typedef typename TARGET::Type TGT;
+                typedef void    (*PROC)(TGT &, const U &);
+
+                static  PROC   proc = Ops<typename TARGET::Type,U>::Add;
+                const size_t   para = engine.in1D.size();
+                
+                engine.setup(M.rows);                      // process rows in parallel
+                engine.in1D.attach(xma.make(para,M.cols)); // with help
+                
+                volatile Engine::Clean1D willClean(engine.in1D);
+                engine.in1D(Parallel::Mul<TARGET,T,SOURCE,U,PROC>,target,M,source,proc);
+            }
+
+            //__________________________________________________________________
+            //
+            //! target += M*source
+            //__________________________________________________________________
+            template <typename TARGET, typename T, typename SOURCE, typename U> inline
+            void MulSub(TARGET &          target,
+                        const Matrix<T>  &M,
+                        SOURCE           &source,
+                        MultiAdd<U>      &xma,
+                        Engine           &engine)
+            {
+                assert( target.size() == M.rows );
+                assert( source.size() == M.cols );
+
+                typedef typename TARGET::Type TGT;
+                typedef void    (*PROC)(TGT &, const U &);
+
+                static  PROC   proc = Ops<typename TARGET::Type,U>::Sub;
+                const size_t   para = engine.in1D.size();
+
+                engine.setup(M.rows);                      // process rows in parallel
+                engine.in1D.attach(xma.make(para,M.cols)); // with help
+
+                volatile Engine::Clean1D willClean(engine.in1D);
+                engine.in1D(Parallel::Mul<TARGET,T,SOURCE,U,PROC>,target,M,source,proc);
+            }
+
         }
 
     }
