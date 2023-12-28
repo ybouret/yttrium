cbfa621b1e92d76c8fe3ecaa587469a7f711d901
Yann Bouret <yann.bouret@gmail.com>
2023-12-28 15:01:30 +0100

    wip

--- src/mkl/y/mkl/filter/smooth.hpp
+++ src/mkl/y/mkl/filter/smooth.hpp
@@ -13,18 +13,54 @@ namespace Yttrium
     namespace MKL
     {
 
+        //______________________________________________________________________
+        //
+        //
+        //
+        //! Smooth using low-degree polynomial on local excerpts
+        //
+        //
+        //______________________________________________________________________
         template <typename T>
         class Smooth : public Readable<T>
         {
         public:
+            //__________________________________________________________________
+            //
+            //
+            // C++
+            //
+            //__________________________________________________________________
             explicit Smooth();
             virtual ~Smooth() noexcept;
 
+            //__________________________________________________________________
+            //
+            //
+            // Methods
+            //
+            //__________________________________________________________________
+
+            //__________________________________________________________________
+            //
+            //! fit (t,z) at t0 with polynomial of max degree d
+            /**
+             on output: [1] = smoothed value
+                        [2] = smoothed first  derivative
+                        [3] = smoothed second derivative
+             */
+            //__________________________________________________________________
             void  run(const T           &t0,
                       const Readable<T> &t,
                       const Readable<T> &z,
                       const size_t       degree);
 
+            //__________________________________________________________________
+            //
+            //
+            // interface
+            //
+            //__________________________________________________________________
             virtual const char * callSign()               const noexcept; //!< "Smooth"
             virtual size_t       size()                   const noexcept; //!< 3
             virtual const T &    operator[](const size_t) const noexcept; //!< [1:3]
