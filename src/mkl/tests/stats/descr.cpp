dc2a76d5186e1cbb7496bd56f26f783c8af07f34
Yann Bouret <yann.bouret@gmail.com>
2023-12-28 10:33:27 +0100

    wip

--- src/mkl/tests/stats/descr.cpp
+++ src/mkl/tests/stats/descr.cpp
@@ -13,7 +13,7 @@ namespace
     template <typename T>
     static inline void doMetrics(Random::Bits &ran)
     {
-        const size_t    n = 1 + ran.leq(20);
+        const size_t    n = 2 + ran.leq(20);
         Vector<T>       v(n,AsCapacity);
         List<T>         l(n,AsCapacity);
 
@@ -38,21 +38,28 @@ namespace
         std::cerr << "Average: " << vAverage << " | " << lAverage << std::endl;
 
         const T vVariance = Statistics::Variance(v,xadd,vAverage);
-        const T lVariance = Statistics::Variance(l,xadd,vAverage);
+        const T lVariance = Statistics::Variance(l,xadd,lAverage);
         const T vStdDev0  = Sqrt<T>::Of(vVariance);
         const T lStdDev0  = Sqrt<T>::Of(lVariance);
         std::cerr << "StdDev0: " << vStdDev0 << " | " <<  lStdDev0 << std::endl;
 
-
+        
         const T vStdDev = Statistics::StandardDeviation(v,xadd,vAverage);
-        const T lStdDev = Statistics::StandardDeviation(l,xadd,vAverage);
+        const T lStdDev = Statistics::StandardDeviation(l,xadd,lAverage);
         std::cerr << "StdDev : " << vStdDev << " | " <<  lStdDev << std::endl;
 
         const T vAbsDev = Statistics::AbsoluteDeviation(v,xadd,vAverage);
-        const T lAbsDev = Statistics::AbsoluteDeviation(l,xadd,vAverage);
+        const T lAbsDev = Statistics::AbsoluteDeviation(l,xadd,lAverage);
         std::cerr << "AbsDev : " << vAbsDev << " | " <<  lAbsDev << std::endl;
 
+        const T vSkew = Statistics::Skewness(v, xadd, vAverage, vStdDev);
+        const T lSkew = Statistics::Skewness(l, xadd, lAverage, lStdDev);
+        std::cerr << "Skew   : " << vSkew << " | " <<  lSkew << std::endl;
 
+
+        const T vKurt = Statistics::Kurtosis(v, xadd, vAverage, vStdDev);
+        const T lKurt = Statistics::Kurtosis(l, xadd, lAverage, lStdDev);
+        std::cerr << "Kurt   : " << vKurt << " | " <<  lKurt << std::endl;
     }
 }
 
@@ -62,9 +69,11 @@ Y_UTEST(stats_descr)
 
     Random::Rand ran;
 
-    doMetrics<float>(ran);
     doMetrics< XReal<long double> >(ran);
+    doMetrics<float>(ran);
+    doMetrics<long double>(ran);
     if(false) doMetrics<apq>(ran);
+
 }
 Y_UDONE()
 
--- src/mkl/y/mkl/statistics/metrics.hpp
+++ src/mkl/y/mkl/statistics/metrics.hpp
@@ -11,6 +11,12 @@ namespace Yttrium
     {
         namespace Statistics
         {
+            //__________________________________________________________________
+            //
+            //
+            //! Average of a range
+            //
+            //__________________________________________________________________
             template <typename T,typename ITERATOR> inline
             T Average(ITERATOR it, size_t n, Antelope::Add<T> &xadd)
             {
@@ -28,6 +34,12 @@ namespace Yttrium
                 return xadd.sum()/den;
             }
 
+            //__________________________________________________________________
+            //
+            //
+            //! Average of a sequence
+            //
+            //__________________________________________________________________
             template <typename T,typename SEQUENCE> inline
             T Average(SEQUENCE &seq, Antelope::Add<T> &xadd)
             {
@@ -37,6 +49,12 @@ namespace Yttrium
 
         namespace Statistics
         {
+            //__________________________________________________________________
+            //
+            //
+            //! Variance of a range with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename ITERATOR> inline
             T Variance(ITERATOR it, size_t n, Antelope::Add<T> &xadd, const T &average)
             {
@@ -58,6 +76,12 @@ namespace Yttrium
                 return xadd.sum()/den;
             }
 
+            //__________________________________________________________________
+            //
+            //
+            //! Variance of a sequence with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename SEQUENCE> inline
             T Variance(SEQUENCE &seq, Antelope::Add<T> &xadd, const T &average)
             {
@@ -67,6 +91,12 @@ namespace Yttrium
 
         namespace Statistics
         {
+            //__________________________________________________________________
+            //
+            //
+            //! Standard Deviation of a range with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename ITERATOR> inline
             T StandardDeviation(ITERATOR it, size_t n, Antelope::Add<T> &xadd, const T &average)
             {
@@ -74,6 +104,12 @@ namespace Yttrium
                 return Sqrt<T>::Of(_);
             }
 
+            //__________________________________________________________________
+            //
+            //
+            //! Standard Deviation of a sequence with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename SEQUENCE> inline
             T StandardDeviation(SEQUENCE &seq, Antelope::Add<T> &xadd, const T &average)
             {
@@ -83,6 +119,12 @@ namespace Yttrium
 
         namespace Statistics
         {
+            //__________________________________________________________________
+            //
+            //
+            //! Absolute Deviation of a range with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename ITERATOR> inline
             T AbsoluteDeviation(ITERATOR it, size_t n, Antelope::Add<T> &xadd, const T &average)
             {
@@ -102,13 +144,117 @@ namespace Yttrium
                 }
                 return xadd.sum()/den;
             }
-            
+
+            //__________________________________________________________________
+            //
+            //
+            //! Absolute Deviation of a sequence with pre-computed average
+            //
+            //__________________________________________________________________
             template <typename T,typename SEQUENCE> inline
             T AbsoluteDeviation(SEQUENCE &seq, Antelope::Add<T> &xadd, const T &average)
             {
                 return AbsoluteDeviation( seq.begin(), seq.size(), xadd, average);
             }
+        }
 
+        namespace Statistics
+        {
+            //__________________________________________________________________
+            //
+            //
+            //! Skewness of a range with pre-computed average and stddev
+            //
+            //__________________________________________________________________
+            template <typename T,typename ITERATOR> inline
+            T Skewness(ITERATOR          it,
+                       size_t            n,
+                       Antelope::Add<T> &xadd,
+                       const T          &average,
+                       const T          &standardDeviation)
+            {
+                switch(n)
+                {
+                    case 0:
+                        return T(0);
+                    default:
+                        break;
+                }
+                const T den(n);
+                xadd.free();
+                while(n-- > 0)
+                {
+                    const T delta = (*(it++) - average)/standardDeviation;
+                    const T delta2 = delta*delta;
+                    xadd << delta*delta2;
+                }
+                return xadd.sum()/den;
+            }
+
+            //__________________________________________________________________
+            //
+            //
+            //! Skewness of a sequence with pre-computed average and stddev
+            //
+            //__________________________________________________________________
+            template <typename T,typename SEQUENCE> inline
+            T Skewness(SEQUENCE         &seq,
+                       Antelope::Add<T> &xadd,
+                       const T          &average,
+                       const T          &standardDeviation)
+            {
+                return Skewness(seq.begin(), seq.size(), xadd, average, standardDeviation);
+            }
+        }
+
+        namespace Statistics
+        {
+            //__________________________________________________________________
+            //
+            //
+            //! Kurtosis of a range with pre-computed average and stddev
+            //
+            //__________________________________________________________________
+            template <typename T,typename ITERATOR> inline
+            T Kurtosis(ITERATOR          it,
+                       size_t            n,
+                       Antelope::Add<T> &xadd,
+                       const T          &average,
+                       const T          &standardDeviation)
+            {
+                switch(n)
+                {
+                    case 0:
+                        return T(0);
+                    default:
+                        break;
+                }
+                const T three(3);
+                const T den(n);
+                xadd.free();
+                while(n-- > 0)
+                {
+                    const T delta = (*(it++) - average)/standardDeviation;
+                    const T delta2 = delta*delta;
+                    xadd << delta2*delta2;
+                }
+                return xadd.sum()/den-three;
+            }
+
+            //__________________________________________________________________
+            //
+            //
+            //! Kurtosis of a sequence with pre-computed average and stddev
+            //
+            //__________________________________________________________________
+            template <typename T,typename SEQUENCE> inline
+            T Kurtosis(SEQUENCE         &seq,
+                       Antelope::Add<T> &xadd,
+                       const T          &average,
+                       const T          &standardDeviation)
+            {
+                return Kurtosis(seq.begin(), seq.size(), xadd, average, standardDeviation);
+            }
         }
     }
 }
