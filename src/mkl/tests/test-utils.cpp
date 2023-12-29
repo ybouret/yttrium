0785c867ac47cf78fbd374bd504dc40248dab061
ybouret <yann.bouret@gmail.com>
2023-12-29 18:14:13 +0100

    win32 correction

--- src/mkl/tests/test-utils.cpp
+++ src/mkl/tests/test-utils.cpp
@@ -11,19 +11,19 @@ Y_UTEST(utils)
     {
         const float one = 1;
         const float eps = Numeric<float>::EPSILON / 2;
-        Y_CHECK(IsUnchanged(one,eps));
+        Y_CHECK(IsUnchanged(one,eps) || Die("float") );
     }
 
     {
         const double one = 1;
         const double eps = Numeric<double>::EPSILON / 2;
-        Y_CHECK(IsUnchanged(one,eps));
+        Y_CHECK(IsUnchanged(one,eps) || Die("double") );
     }
 
     {
         const long double one = 1;
         const long double eps = Numeric<long double>::EPSILON / 2;
-        Y_CHECK(IsUnchanged(one,eps));
+        Y_CHECK(IsUnchanged(one,eps) || Die("long double") );
     }
 }
 Y_UDONE()
