5dc0bb719af53ead4c485159734a816412b87ebe
Yann Bouret <yann.bouret@gmail.com>
2023-12-31 10:09:59 +0100

    wip

--- src/mkl/tests/ode/scheme45.cpp
+++ src/mkl/tests/ode/scheme45.cpp
@@ -2,17 +2,49 @@
 #include "y/mkl/ode/rk45/cash-karp.hpp"
 #include "y/utest/run.hpp"
 #include "y/random/bits.hpp"
+#include "y/system/rtti.hpp"
 
 using namespace Yttrium;
 using namespace MKL;
 
 namespace
 {
+    template <typename T>
+    struct Meca
+    {
+        T mass;
+        T k;
+        T lam;
+
+        inline void compute(Writable<T> &dydt,T,const Readable<T> &y)
+        {
+            const T x     = y[1];
+            const T v     = y[2];
+            const T force = -k*x - lam*v;
+
+            T &dxdt = dydt[1]; dxdt = v;
+            T &dvdt = dydt[1]; dvdt = force/mass;
+
+        }
+    };
+
     template <typename T> static inline
     void testScheme(Random::Bits & )
     {
+        static const String &tid = RTTI::Name<T>();
+
+        std::cerr << "Enter Scheme<" << tid << ">" << std::endl;
         ODE::RK45::Scheme<ODE::RK45::CashKarp,T> scheme;
+
+        Meca<T>                          meca = { 0.11f, 1.01, 0.0f };
+        typename ODE::Named<T>::Equation diffeq( &meca, & Meca<T>::compute );
+
         
+
+        std::cerr << "Leave Scheme<" << tid << ">" << std::endl << std::endl;
+
+
+
     }
 }
 
