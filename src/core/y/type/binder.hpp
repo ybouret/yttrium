0128bb97b6dfd8a010d3f993a5db184b7bbd792e
Yann Bouret <yann.bouret@unice.fr>
2024-01-12 10:48:04 +0100

    wip

---
+++ src/core/y/type/binder.hpp
@@ -0,0 +1,105 @@
+//! \file
+
+
+#ifndef Y_Type_Binder_Included
+#define Y_Type_Binder_Included 1
+
+#include "y/type/list.hpp"
+#include "y/type/traits.hpp"
+#include <iostream>
+
+namespace Yttrium
+{
+
+    //__________________________________________________________________________
+    //
+    //
+    //! helper to declare parameters in Binder
+    //
+    //__________________________________________________________________________
+#define Y_BINDER_DECL(I)                                               \
+typedef typename TL::SafeTypeAt<TLIST,I-1,EmptyType>::Result Type##I;  \
+typedef typename TypeTraits<Type##I>::ParamType              Param##I; \
+Type##I                                                      arg##I
+
+    //__________________________________________________________________________
+    //
+    //
+    //! Binding types
+    //
+    //__________________________________________________________________________
+    template <typename TLIST>
+    class Binder
+    {
+    public:
+        //______________________________________________________________________
+        //
+        //
+        // Declarations
+        //
+        //______________________________________________________________________
+        Y_BINDER_DECL(1); //!< arg1
+        Y_BINDER_DECL(2); //!< arg2
+        Y_BINDER_DECL(3); //!< arg3
+        Y_BINDER_DECL(4); //!< arg4
+
+        //______________________________________________________________________
+        //
+        //
+        // C++
+        //
+        //______________________________________________________________________
+
+        //! empty/default binder
+        inline explicit Binder() :
+        arg1(), arg2(), arg3(), arg4() {}
+
+        //! setup1
+        inline explicit Binder(Param1 p1) :
+        arg1(p1), arg2(), arg3(), arg4() {}
+
+        //! setup2
+        inline explicit Binder(Param1 p1,
+                               Param2 p2) :
+        arg1(p1), arg2(p2), arg3(), arg4() {}
+
+        //! setup3
+        inline explicit Binder(Param1 p1,
+                               Param2 p2,
+                               Param3 p3) :
+        arg1(p1), arg2(p2), arg3(p3), arg4() {}
+
+        //! setup4
+        inline explicit Binder(Param1 p1,
+                               Param2 p2,
+                               Param3 p3,
+                               Param4 p4) :
+        arg1(p1), arg2(p2), arg3(p3), arg4(p4) {}
+
+        //! cleanup
+        inline virtual ~Binder() noexcept {}
+
+        //! display
+        inline friend std::ostream & operator<<(std::ostream &os, const Binder &self)
+        {
+            os << '[';
+
+            os << ' ' <<        self.arg1;
+            os << ',' << ' ' << self.arg2;
+            os << ',' << ' ' << self.arg3;
+            os << ',' << ' ' << self.arg4;
+
+            os << ' ' << ']';
+            return os;
+        }
+
+
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(Binder);
+    };
+
+}
+
+#endif
+
