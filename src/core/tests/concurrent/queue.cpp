e86fa21a5316cae427bf822029b94c3e64eb3493
Yann Bouret <yann.bouret@unice.fr>
2024-01-15 09:31:42 +0100

    wip

--- src/core/tests/concurrent/queue.cpp
+++ src/core/tests/concurrent/queue.cpp
@@ -1,6 +1,7 @@
 
 #include "y/concurrent/pipeline/alone.hpp"
 #include "y/concurrent/pipeline/queue.hpp"
+#include "y/concurrent/pipeline/job.hpp"
 
 #include "y/concurrent/thread.hpp"
 #include "y/utest/run.hpp"
@@ -25,7 +26,6 @@ namespace
         inline  Demo(const Demo &dem) noexcept :
         param(dem.param)
         {
-            //std::cerr << "[#Demo] @" << param << std::endl;
         }
 
 
@@ -77,13 +77,37 @@ namespace
         Y_DISABLE_ASSIGN(Demo);
     };
 
-    static inline void Process(const Concurrent::ThreadContext &ctx, const int value)
+
+
+    static inline void Proc0(const Concurrent::ThreadContext &ctx)
     {
-        Y_LOCK(ctx.sync);
-        (std::cerr << "Process(" << value << ")" << " in " << ctx.name << std::endl).flush();
+        {
+            Y_LOCK(ctx.sync);
+            (std::cerr << "Proc0()" << " in " << ctx.name << std::endl).flush();
+        }
         WallTime tmx;
         tmx.wait(0.1);
     }
+
+    static inline void Proc1(const Concurrent::ThreadContext &ctx, const int value)
+    {
+        {
+            Y_LOCK(ctx.sync);
+            (std::cerr << "Proc1(" << value << ")" << " in " << ctx.name << std::endl).flush();
+        }
+        WallTime tmx;
+        tmx.wait(0.1);
+    }
+
+    static inline void Proc2(const Concurrent::ThreadContext &ctx, int &res, const int value)
+    {
+        {
+            Y_LOCK(ctx.sync);
+            (std::cerr << "Proc2(" << value << ")" << " in " << ctx.name << std::endl).flush();
+        }
+        res = value * 2;
+    }
+
 
 }
 
@@ -93,9 +117,100 @@ Y_UTEST(concurrent_queue)
     const Concurrent::Topology topology;
     std::cerr << topology << std::endl;
     
-    Demo         dem(3);
-    Random::Rand ran;
+    Demo               dem(3);
+    Random::Rand       ran;
+    Concurrent::Alone  alone;
+    Concurrent::Queue  queue(topology);
 
+
+    typedef Concurrent::Job<NullType> Job0;
+    void                    (*proc0)(const Concurrent::ThreadContext &) = Proc0;
+
+
+    std::cerr << "-- Testing TaskUUID" << std::endl;
+    {
+        const Job0 j1(Functionoid,dem);
+        const Job0 j2(CxxMethodOf,dem, & Demo::unfold);
+        const Job0 j3 = j1;
+        const Job0 j4 = j2;
+
+        {
+            Concurrent::TaskUUID tid = 0;
+            tid = alone.load(j1); std::cerr << "tid = " << tid << std::endl;
+            tid = alone.load(j2); std::cerr << "tid = " << tid << std::endl;
+            tid = alone.load(j3); std::cerr << "tid = " << tid << std::endl;
+            tid = alone.load(j4); std::cerr << "tid = " << tid << std::endl;
+        }
+    }
+
+    Job0::Launch(alone,dem);
+    Job0::Launch(alone,proc0);
+    Job0::Invoke(alone,dem, & Demo::unfold );
+
+    Job0::Launch(queue,dem);
+    Job0::Launch(queue,proc0);
+    Job0::Invoke(queue,dem, & Demo::unfold );
+    queue.flush();
+
+
+    {
+        Vector<Concurrent::Task>       tsk;
+        Vector<Concurrent::TaskUUID>   tid;
+        for(int i=1;i<=40;++i)
+        {
+            const Demo todo(i);
+            const Job0 task(Functionoid,todo);
+            tsk << task;
+        }
+
+        queue.push(tid,tsk);
+        queue.flush();
+    }
+
+    typedef Concurrent::Job<TL1(int)> Job1;
+    void                    (*proc1)(const Concurrent::ThreadContext &, const int) = Proc1;
+
+    const int value = 31;
+    Job1::Launch(alone,proc1,value);
+    Job1::Invoke(alone,dem, & Demo:: doMul, value);
+
+
+    Job1::Launch(queue,proc1,value);
+    Job1::Invoke(queue,dem, & Demo:: doMul, value);
+    queue.flush();
+
+
+    typedef Concurrent::Job<TL2(int &, const int)> Job2;
+    void                    (*proc2)(const Concurrent::ThreadContext &,
+                                     int &, const int) = Proc2;
+
+    {
+        int res = 0;
+        Job2::Launch(alone,proc2,res,value);
+        std::cerr << "res=" << res << std::endl;
+
+        res = 0;
+        Job2::Invoke(alone,dem, & Demo::getMul, res, value);
+        std::cerr << "res=" << res << std::endl;
+    }
+
+    {
+        Vector<int> res(8,0);
+        for(int i=1;i<=8;++i)
+        {
+            Job2::Launch(alone,proc2,res[i],i);
+        }
+        std::cerr << "res=" << res << std::endl;
+
+        res.ld(0);
+
+        for(int i=1;i<=8;++i)
+        {
+            Job2::Launch(queue,proc2,res[i],i);
+        }
+        queue.flush();
+        std::cerr << "res=" << res << std::endl;
+    }
 
 
 #if 0
--- src/core/y/concurrent/pipeline/job.hpp
+++ src/core/y/concurrent/pipeline/job.hpp
@@ -5,6 +5,7 @@
 #define Y_Concurrent_Job_Included 1
 
 #include "y/concurrent/pipeline/task/manager.hpp"
+#include "y/concurrent/pipeline/task.hpp"
 
 
 namespace Yttrium
@@ -12,7 +13,191 @@ namespace Yttrium
 
     namespace Concurrent
     {
+        //______________________________________________________________________
+        //
+        //
+        //
+        //! a Job is a concrete Task
+        //
+        //
+        //______________________________________________________________________
+        template <typename TLIST>
+        class Job : public Task
+        {
+        public:
+            //__________________________________________________________________
+            //
+            //
+            // Definitions
+            //
+            //__________________________________________________________________
+            Y_BINDER_ECHO(TLIST); //!< alias
 
+            //__________________________________________________________________
+            //
+            //
+            // C++
+            //
+            //__________________________________________________________________
+
+            //! copy (increase reference)
+            inline Job(const Job &other) noexcept : Task(other) {}
+
+            //! cleanup
+            inline virtual ~Job() noexcept {}
+
+            //__________________________________________________________________
+            //
+            //
+            // Wrappers with no argument
+            //
+            //__________________________________________________________________
+
+            //! create from functioniod : fn(context)
+            template <typename FUNCTION> inline
+            explicit Job(const Functionoid_       &,
+                         const FUNCTION     &fn) :
+            Task(  new Callback<FUNCTION,NullType>(fn) )
+            {
+            }
+
+            //! create from host.meth(context)
+            template <typename OBJECT, typename METHOD> inline
+            explicit Job(const CxxMethodOf_ &,
+                         OBJECT             &host,
+                         METHOD              meth) :
+            Task( new Command<OBJECT,METHOD,NullType>(host,meth) )
+            {
+            }
+
+            //! call into task
+            template <typename FUNCTION> static inline
+            TaskUUID Launch(TaskManager    &tasks,
+                            const FUNCTION &fn)
+            {
+                const Job job(Functionoid,fn);
+                return tasks.load(job);
+            }
+
+            //! invoke into task
+            template <typename OBJECT, typename METHOD> static inline
+            TaskUUID Invoke(TaskManager &tasks,
+                            OBJECT      &host,
+                            METHOD       meth)
+            {
+                const Job job(CxxMethodOf,host,meth);
+                return tasks.load(job);
+            }
+
+            //__________________________________________________________________
+            //
+            //
+            // Wrappers with one argument
+            //
+            //__________________________________________________________________
+
+            //! create from fn(context,p1)
+            template <typename FUNCTION>
+            inline explicit Job(const Functionoid_ &,
+                                const FUNCTION     &fn,
+                                Param1              p1) :
+            Task( new Callback<FUNCTION,TLIST>(fn,p1) )
+            {
+
+            }
+
+
+            //! create from host.meth(context,p1)
+            template <typename OBJECT, typename METHOD> inline
+            explicit Job(const CxxMethodOf_ &,
+                         OBJECT             &host,
+                         METHOD              meth,
+                         Param1              p1) :
+            Task( new Command<OBJECT,METHOD,TLIST>(host,meth,p1) )
+            {
+            }
+
+
+            //! call into task
+            template <typename FUNCTION> static inline
+            TaskUUID Launch(TaskManager    &tasks,
+                            const FUNCTION &fn,
+                            Param1          p1)
+            {
+                const Job job(Functionoid,fn,p1);
+                return tasks.load(job);
+            }
+
+
+            //! invoke into task
+            template <typename OBJECT, typename METHOD> static inline
+            TaskUUID Invoke(TaskManager &tasks,
+                            OBJECT      &host,
+                            METHOD       meth,
+                            Param1       p1)
+            {
+                const Job job(CxxMethodOf,host,meth,p1);
+                return tasks.load(job);
+            }
+
+            //__________________________________________________________________
+            //
+            //
+            // Wrappers with two arguments
+            //
+            //__________________________________________________________________
+
+            //! create from fn(context,p1,p2)
+            template <typename FUNCTION>
+            inline explicit Job(const Functionoid_ &,
+                                const FUNCTION     &fn,
+                                Param1              p1,
+                                Param2              p2) :
+            Task( new Callback<FUNCTION,TLIST>(fn,p1,p2) )
+            {
+
+            }
+
+
+            //! create from host.meth(context,p1)
+            template <typename OBJECT, typename METHOD> inline
+            explicit Job(const CxxMethodOf_ &,
+                         OBJECT             &host,
+                         METHOD              meth,
+                         Param1              p1,
+                         Param2              p2) :
+            Task( new Command<OBJECT,METHOD,TLIST>(host,meth,p1,p2) )
+            {
+            }
+
+
+            //! call into task
+            template <typename FUNCTION> static inline
+            TaskUUID Launch(TaskManager    &tasks,
+                            const FUNCTION &fn,
+                            Param1          p1,
+                            Param2          p2)
+            {
+                const Job job(Functionoid,fn,p1,p2);
+                return tasks.load(job);
+            }
+
+
+            //! invoke into task
+            template <typename OBJECT, typename METHOD> static inline
+            TaskUUID Invoke(TaskManager &tasks,
+                            OBJECT      &host,
+                            METHOD       meth,
+                            Param1       p1,
+                            Param2       p2)
+            {
+                const Job job(CxxMethodOf,host,meth,p1,p2);
+                return tasks.load(job);
+            }
+
+        private:
+            Y_DISABLE_ASSIGN(Job);
+        };
     }
 
 }
