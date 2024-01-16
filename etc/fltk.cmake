604a65fa592c11258e76c3998e3d59a039fe498f
Yann Bouret <yann.bouret@gmail.com>
2024-01-16 13:32:46 +0100

    wip

--- etc/fltk.cmake
+++ etc/fltk.cmake
@@ -3,7 +3,7 @@
 ## Simplified FLTK configuration, based on fltk-config
 ##
 ########################################################################
-MESSAGE( STATUS "<FTLK>" )
+message( STATUS "<FTLK>" )
 
 ########################################################################
 ##
@@ -21,15 +21,15 @@ SET(FLTK_BIN $ENV{FLTK_BIN} CACHE STRING "Override fltk-config location" )
 IF( "" STREQUAL "${FLTK_BIN}" )
 	
 	# automatic detection
-	MESSAGE( STATUS "Looking for default FLTK")
+	message( STATUS "Looking for default FLTK")
 	FIND_PROGRAM( FLTK-CONFIG fltk-config )
 	IF( ${FLTK-CONFIG} STREQUAL FLTK-CONFIG-NOTFOUND )
-		MESSAGE( STATUS "Can't find fltk-config" )
+		message( STATUS "Can't find fltk-config" )
 	ELSE()
 		# got fltk config
 		FIND_PROGRAM( FLUID fluid )
 		IF( ${FLUID} STREQUAL FLUID-NOTFOUND )
-			MESSAGE( STATUS "Can't find fluid" )
+			message( STATUS "Can't find fluid" )
 		ELSE()
 			# got fluid
 			SET(FLTK_FOUND ON)
@@ -47,8 +47,8 @@ ENDIF()
 
 IF(FLTK_FOUND)
 
-MESSAGE( STATUS "@FLTK fltk-config='${FLTK-CONFIG}'" )
-MESSAGE( STATUS "@FLTK fluid='${FLUID}" )
+message( STATUS "@FLTK fltk-config='${FLTK-CONFIG}'" )
+message( STATUS "@FLTK fluid='${FLUID}" )
 
 ########################################################################
 ##
@@ -59,14 +59,14 @@ MESSAGE( STATUS "@FLTK fluid='${FLUID}" )
 #-----------------------------------------------------------------------
 # get cxxflags 
 #-----------------------------------------------------------------------
-#MESSAGE( STATUS "  @FLTK query cxxflags..." )
+#message( STATUS "  @FLTK query cxxflags..." )
 FIND_PROGRAM(BASH bash)
 IF(FLTK_VERBOSE)
-	MESSAGE( STATUS "@FLTK bash='${BASH}'" )
+	message( STATUS "@FLTK bash='${BASH}'" )
 ENDIF()
 EXEC_PROGRAM(${BASH} ARGS "${FLTK-CONFIG}" --cxxflags OUTPUT_VARIABLE FLTK-CXXFLAGS)
 IF(FLTK_VERBOSE)
-	MESSAGE( STATUS "@FLTK-CXXFLAGS='${FLTK-CXXFLAGS}'" )
+	message( STATUS "@FLTK-CXXFLAGS='${FLTK-CXXFLAGS}'" )
 ENDIF()
 
 #-----------------------------------------------------------------------
@@ -76,7 +76,7 @@ STRING( REGEX MATCHALL "[-][I]([^ ;])+" FLTK-INCLUDES ${FLTK-CXXFLAGS} )
 STRING( REPLACE  "-I" "" FLTK-INCLUDES "${FLTK-INCLUDES}")
 LIST(REMOVE_DUPLICATES FLTK-INCLUDES)
 IF(FLTK_VERBOSE)
-	MESSAGE( STATUS "@FLTK-INCLUDES='${FLTK-INCLUDES}'" )
+	message( STATUS "@FLTK-INCLUDES='${FLTK-INCLUDES}'" )
 ENDIF()
 INCLUDE_DIRECTORIES(${FLTK-INCLUDES})
 
@@ -85,7 +85,7 @@ INCLUDE_DIRECTORIES(${FLTK-INCLUDES})
 #-----------------------------------------------------------------------
 STRING( REGEX MATCHALL "[-][D]([^ ;])+" FLTK-DEFINES ${FLTK-CXXFLAGS} )
 IF(FLTK_VERBOSE)
-	MESSAGE( STATUS "@FLTK-DEFINES='${FLTK-DEFINES}'")
+	message( STATUS "@FLTK-DEFINES='${FLTK-DEFINES}'")
 ENDIF()
 ADD_DEFINITIONS(${FLTK-DEFINES})
 
@@ -93,17 +93,17 @@ ADD_DEFINITIONS(${FLTK-DEFINES})
 # extract FLTK link directory
 #-----------------------------------------------------------------------
 #EXEC_PROGRAM( bash ARGS ${FLTK-CONFIG} --ldflags OUTPUT_VARIABLE FLTK-LDFLAGS)
-#MESSAGE( STATUS "  @FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
+#message( STATUS "  @FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
 #STRING( REGEX MATCHALL "[-][L]([^ ;])+" FLTK-LINK-DIR ${FLTK-LDFLAGS} )
 #STRING( REPLACE  "-L" "" FLTK-LINK-DIR "${FLTK-LINK-DIR}")
-#MESSAGE( STATUS "  @FLTK-LINK-DIR='${FLTK-LINK-DIR}'" )
+#message( STATUS "  @FLTK-LINK-DIR='${FLTK-LINK-DIR}'" )
 #LINK_DIRECTORIES( ${FLTK-LINK-DIR} )
 
 ########################################################################
 ## Linking
 ########################################################################
 ## Link with optional FLTK libraries
-MACRO(TARGET_LINK_FLTK THE_TARGET)
+macro(TARGET_LINK_FLTK THE_TARGET)
 
 	#initialize arguments for fltk-config
 	SET(_fltk_ldflags "--ldflags" )
@@ -114,35 +114,35 @@ MACRO(TARGET_LINK_FLTK THE_TARGET)
 	ENDFOREACH( extra ${ARGN} )
 
 	#get all args
-	#MESSAGE( STATUS "@FLTK --> ${THE_TARGET} | 'fltk-config ${_fltk_ldflags}'" )
+	#message( STATUS "@FLTK --> ${THE_TARGET} | 'fltk-config ${_fltk_ldflags}'" )
 	EXEC_PROGRAM( ${BASH} ARGS ${FLTK-CONFIG} ${_fltk_ldflags} OUTPUT_VARIABLE FLTK-LDFLAGS)
 	IF(FLTK_VERBOSE)
-		MESSAGE( STATUS "@FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
+		message( STATUS "@FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
 	ENDIF()
 	
 	#extract libraries, link directories is already set
 	#STRING( REGEX MATCHALL "([-][l]([^ ;])+)|(-framework ([^ ;])+)" FLTK-LIBS ${FLTK-LDFLAGS})
 	#STRING( REPLACE  "-l" "" FLTK-LIBS "${FLTK-LIBS}")
-	#MESSAGE( STATUS "${THE_TARGET} needs '${FLTK-LIBS}'")
+	#message( STATUS "${THE_TARGET} needs '${FLTK-LIBS}'")
 	
 	#and declare libraries to be linked !
 	#TARGET_LINK_LIBRARIES(${THE_TARGET} ${FLTK-LIBS})
 	TARGET_LINK_LIBRARIES(${THE_TARGET} ${FLTK-LDFLAGS})
-ENDMACRO(TARGET_LINK_FLTK)
+endmacro(TARGET_LINK_FLTK)
 
 ########################################################################
 ## fluid compiler API
 ## FLUID_UIC(UI [PATH]) will produce UI_SOURCE UI_HEADER UI_SOURCES
 ########################################################################
-MACRO(FLUID_UIC THE_UI)
+macro(FLUID_UIC THE_UI)
 	SET(_UI_FL     "${CMAKE_CURRENT_SOURCE_DIR}/${THE_UI}.fl" )
 	SET(_UI_SOURCE "${CMAKE_CURRENT_BINARY_DIR}/${THE_UI}.cxx")
 	SET(_UI_HEADER "${CMAKE_CURRENT_BINARY_DIR}/${THE_UI}.h")
-	MESSAGE( STATUS "**** <${THE_UI}> will compile"   )
-	MESSAGE( STATUS "** fluid : ${_UI_FL}"     )
-	MESSAGE( STATUS "** source: ${_UI_SOURCE}" )
-	MESSAGE( STATUS "** header: ${_UI_HEADER}" )
-	MESSAGE( STATUS "@FLTK: GUI from <${THE_UI}.fl>" )
+	message( STATUS "**** <${THE_UI}> will compile"   )
+	message( STATUS "** fluid : ${_UI_FL}"     )
+	message( STATUS "** source: ${_UI_SOURCE}" )
+	message( STATUS "** header: ${_UI_HEADER}" )
+	message( STATUS "@FLTK: GUI from <${THE_UI}.fl>" )
 	
 	#create the command
 	ADD_CUSTOM_COMMAND( OUTPUT  ${_UI_SOURCE} ${_UI_HEADER}
@@ -157,16 +157,16 @@ MACRO(FLUID_UIC THE_UI)
 	SET( ${THE_UI}_SOURCE  ${_UI_SOURCE} )
 	SET( ${THE_UI}_HEADER  ${_UI_HEADER} )
 	SET( ${THE_UI}_SOURCES ${_UI_SOURCE} ${_UI_HEADER} ${_UI_FL} )
-ENDMACRO(FLUID_UIC)
+endmacro(FLUID_UIC)
 
 ENDIF(FLTK_FOUND)
 
-MACRO(Y_FLTK_SILENCE)
-	MESSAGE( STATUS "@FLTK: Removing warnings" )
+macro(Y_FLTK_SILENCE)
+	message( STATUS "@FLTK: Removing warnings" )
 	FOREACH( word IN ITEMS "-Weffc++" "-Wextra" )
 	STRING(REPLACE "${word}" "" CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG}"   )
 	STRING(REPLACE "${word}" "" CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}" )
 ENDFOREACH()
-ENDMACRO(Y_FLTK_SILENCE)
+endmacro(Y_FLTK_SILENCE)
 
 ENDIF(MSVC)
