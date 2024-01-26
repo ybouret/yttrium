70f1e464d19b8fd189cd63a3d5828359c48151c5
Yann Bouret <yann.bouret@unice.fr>
2024-01-26 09:53:15 +0100

    wip

---
+++ src/mkl/y/mkl/fit/sample/abscissae.hpp
@@ -0,0 +1,93 @@
+
+//! \file
+
+#ifndef Y_Fit_Sample_Abscissae_Included
+#define Y_Fit_Sample_Abscissae_Included 1
+
+#include "y/mkl/fit/sample/metrics.hpp"
+#include "y/sort/indexing.hpp"
+
+namespace Yttrium
+{
+    namespace MKL
+    {
+
+        namespace Fit
+        {
+
+            //__________________________________________________________________
+            //
+            //
+            //
+            //! Abscissae access and internal memory for Sample
+            //
+            //
+            //__________________________________________________________________
+            template <typename ABSCISSA>
+            class SampleAbscissae : public SampleMetrics
+            {
+            public:
+                //______________________________________________________________
+                //
+                //
+                // Definitions
+                //
+                //______________________________________________________________
+                typedef ABSCISSA           Abscissa;  //!< alias
+                typedef Readable<Abscissa> Abscissae; //!< alias
+
+                //______________________________________________________________
+                //
+                //
+                // Interface
+                //
+                //______________________________________________________________
+
+                //! access to abscissae
+                virtual const Abscissae & abscissae() const noexcept = 0;
+
+                //______________________________________________________________
+                //
+                //
+                // Method
+                //
+                //______________________________________________________________
+               
+                //! build indices and clean memory
+                inline void prepare()
+                {
+                    assert( dimension() == abscissae().size() );
+                    const size_t n = dimension();
+                    indx.adjust(n,0);
+                    Indexing::Make(indx,Compare,abscissae());
+                }
+
+                //! inline comparator
+                static inline int Compare(const Abscissa &lhs, const Abscissa &rhs) noexcept {
+                    return lhs < rhs ? -1 : ( rhs < lhs ? 1 : 0);
+                }
+
+                //______________________________________________________________
+                //
+                //
+                // C++
+                //
+                //______________________________________________________________
+                inline virtual ~SampleAbscissae() noexcept {}
+            protected:
+                inline explicit SampleAbscissae() noexcept : SampleMetrics() {}
+
+
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(SampleAbscissae);
+
+            };
+
+        }
+
+    }
+}
+
+#endif
+
--- src/mkl/y/mkl/fit/sample/interface.hpp
+++ src/mkl/y/mkl/fit/sample/interface.hpp
@@ -4,7 +4,6 @@
 #define Y_Fit_Sample_Interface_Included 1
 
 #include "y/mkl/fit/sample/abscissae.hpp"
-#include "y/mkl/antelope/add.hpp"
 
 namespace Yttrium
 {
@@ -14,6 +13,55 @@ namespace Yttrium
         namespace Fit
         {
 
+            //__________________________________________________________________
+            //
+            //
+            //
+            //! Sample description
+            //
+            //
+            //__________________________________________________________________
+            template <typename ABSCISSA, typename ORDINATE>
+            class Sample : public SampleAbscissae<ABSCISSA>
+            {
+            public:
+                //______________________________________________________________
+                //
+                //
+                // Definitions
+                //
+                //______________________________________________________________
+                typedef SampleAbscissae<ABSCISSA>       MyAbscissae; //!< alias
+
+                typedef typename MyAbscissae::Abscissa  Abscissa;    //!< alias
+                typedef ORDINATE                        Ordinate;    //!< alias
+
+                typedef typename MyAbscissae::Abscissae Abscissae;   //!< alias
+                typedef Readable<ORDINATE>              Ordinates;   //!< alias
+
+                //______________________________________________________________
+                //
+                //
+                // Interface
+                //
+                //______________________________________________________________
+
+                //! access ordinates
+                virtual const Ordinates & ordinates() const noexcept = 0;
+
+                //______________________________________________________________
+                //
+                //
+                // C++
+                //
+                //______________________________________________________________
+                virtual ~Sample() noexcept {}
+            protected:
+                explicit Sample() noexcept : MyAbscissae() {}
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Sample);
+            };
 
         }
 
