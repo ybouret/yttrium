5bc5ef71050cd6f81f39deb1931149e00de8eabe
ybouret <yann.bouret@gmail.com>
2024-01-30 10:11:09 +0100

    wip

--- src/core/y/field/1d.hpp
+++ src/core/y/field/1d.hpp
@@ -4,7 +4,7 @@
 #define Y_Field1D_Included 1
 
 #include "y/memory/allocator.hpp"
-#include "y/container/operating.hpp"
+#include "y/memory/out-of-reach.hpp"
 #include "y/type/args.hpp"
 
 namespace Yttrium
@@ -55,29 +55,57 @@ namespace Yttrium
         lower(lo),
         upper(up),
         width(lo),
+        built(0),
         items(getItems( &Coerce(width), &Coerce(lower), &Coerce(upper) ) ),
         bytes(0),
         entry( ma.allocate<MutableType>(items,bytes) - lower ),
         alloc( &ma )
         {
-
+            setup();
         }
 
-        inline virtual ~Field1D() noexcept
-        {
-        }
+        inline virtual ~Field1D() noexcept { release(); }
 
         const Coord lower;
         const Coord upper;
         const Coord width;
 
     private:
-        size_t             items;
-        size_t             bytes;
-        MutableType       *entry;
-        Memory::Allocator *alloc;
+        size_t                   built;
+        size_t                   items;
+        size_t                   bytes;
+        MutableType             *entry;
+        Memory::Allocator       *alloc;
 
         Y_DISABLE_COPY_AND_ASSIGN(Field1D);
+        inline void setup() {
+            assert(0==built);
+            try        { for(unit_t i=lower;i<=upper;++i) newAt(i); }
+            catch(...) { release(); throw; }
+        }
+
+        inline void release() noexcept
+        {
+            if(0!=alloc)
+            {
+                entry             += lower;
+                while(built>0)
+                    Memory::OutOfReach::Naught( &entry[--built]);
+                alloc->withdraw(entry, items, bytes);
+                alloc = 0;
+            }
+            clear();
+        }
+
+        inline void newAt(const unit_t i) { new (entry+i) MutableType(); ++built; }
+      
+        inline void clear() noexcept
+        {
+            Y_STATIC_ZVAR(Coerce(lower));
+            Y_STATIC_ZVAR(Coerce(upper));
+            Y_STATIC_ZVAR(Coerce(width));
+        }
+
     };
 
 }
