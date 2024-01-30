c746a05fc6c4ee38377b94eb21ff8439e3fcca95
ybouret <yann.bouret@gmail.com>
2024-01-30 11:39:56 +0100

    wip

--- src/core/y/field/1d.hpp
+++ src/core/y/field/1d.hpp
@@ -22,12 +22,12 @@ namespace Yttrium
             typedef Layout1D               LayoutType;
 
             inline In1D(LayoutType         layout,
-                        Memory::Allocator &mgr) :
+                        Memory::Allocator &memmgr) :
             LayoutType(layout),
             count(items),
             bytes(0),
-            entry( mgr.allocate<MutableType>(count,bytes) - lower ),
-            alloc( &mgr ),
+            entry(  memmgr.allocate<MutableType>(count,bytes) - lower ),
+            alloc( &memmgr ),
             built(0)
             {
                 setup();
@@ -94,76 +94,7 @@ namespace Yttrium
 
 
 
-#if 0
-    template <typename T>
-    class Field1D : public Field
-    {
-    public:
-        static const unsigned DIMS = 1;
-        Y_ARGS_DECL(T,Type);
-        typedef Coord1D Coord;
 
-        inline explicit Field1D(const Coord       lo,
-                                const Coord       up,
-                                Memory::Allocator &ma) :
-        Field(DIMS),
-        lower(lo),
-        upper(up),
-        width(lo),
-        built(0),
-        items(getItems( &Coerce(width), &Coerce(lower), &Coerce(upper) ) ),
-        bytes(0),
-        entry( ma.allocate<MutableType>(items,bytes) - lower ),
-        alloc( &ma )
-        {
-            setup();
-        }
-
-        inline virtual ~Field1D() noexcept { release(); }
-
-        const Coord lower;
-        const Coord upper;
-        const Coord width;
-
-    private:
-        size_t                   built;
-        size_t                   items;
-        size_t                   bytes;
-        MutableType             *entry;
-        Memory::Allocator       *alloc;
-
-        Y_DISABLE_COPY_AND_ASSIGN(Field1D);
-        inline void setup() {
-            assert(0==built);
-            try        { for(unit_t i=lower;i<=upper;++i) newAt(i); }
-            catch(...) { release(); throw; }
-        }
-
-        inline void release() noexcept
-        {
-            if(0!=alloc)
-            {
-                entry             += lower;
-                while(built>0)
-                    Memory::OutOfReach::Naught( &entry[--built]);
-                alloc->withdraw(entry, items, bytes);
-                alloc = 0;
-            }
-            clear();
-        }
-
-        inline void newAt(const unit_t i) { new (entry+i) MutableType(); ++built; }
-      
-        inline void clear() noexcept
-        {
-            Y_STATIC_ZVAR(Coerce(lower));
-            Y_STATIC_ZVAR(Coerce(upper));
-            Y_STATIC_ZVAR(Coerce(width));
-        }
-
-    };
-#endif
-    
 }
 
 #endif
--- src/core/y/field/2d.hpp
+++ src/core/y/field/2d.hpp
@@ -6,6 +6,7 @@
 
 #include "y/field/1d.hpp"
 #include "y/mkl/v2d.hpp"
+#include "y/memory/embed.hpp"
 
 namespace Yttrium
 {
@@ -14,6 +15,31 @@ namespace Yttrium
 
         typedef V2D<unit_t>     Coord2D;
         typedef Layout<Coord2D> Layout2D;
+
+        template <typename T>
+        class In2D : public Layout2D
+        {
+        public:
+            typedef Layout2D LayoutType;
+            typedef Layout1D RowLayout;
+            typedef In1D<T>  RowType;
+
+            explicit In2D(const LayoutType  layout,
+                          Memory::Allocator &memmgr) :
+            LayoutType(layout)
+            {
+            }
+            
+
+            virtual ~In2D() noexcept
+            {
+            }
+
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(In2D);
+        };
+
     }
 
 }
