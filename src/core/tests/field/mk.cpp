a09f16a47abbb53330ad41949cd39142ef5ff1ae
Yann Bouret <yann.bouret@unice.fr>
2024-02-01 08:28:46 +0100

    wip

--- src/core/tests/field/mk.cpp
+++ src/core/tests/field/mk.cpp
@@ -16,9 +16,22 @@ namespace Yttrium
         public:
             virtual ~MetaKey() noexcept;
 
-            virtual size_t maxi() const noexcept = 0;
-            size_t         size() const noexcept { return built; }
+            virtual unsigned maxi() const noexcept = 0;
+            unsigned         size() const noexcept { return built; }
 
+            inline friend std::ostream & operator<<(std::ostream &os, const MetaKey &mk)
+            {
+                String ans;
+                for(unsigned i=0;i<mk.built;++i) ans += mk[i];
+                os << ans;
+                return os;
+            }
+
+            inline const String & operator[](const unsigned dim) const noexcept
+            {
+                assert(dim<built);
+                return *array[dim];
+            }
 
         protected:
             explicit MetaKey(void * const data) noexcept :
@@ -27,15 +40,23 @@ namespace Yttrium
             {
             }
 
+
             inline void push(const SharedString &uuid) noexcept
             {
                 assert( size() < maxi() );
                 new ( & array[built++] ) SharedString(uuid);
             }
 
+            inline void push(const String &s) noexcept
+            {
+                assert(s.quantity()>0);
+                const SharedString uuid( & Coerce(s) );
+                push(uuid);
+            }
+
         private:
             Y_DISABLE_COPY_AND_ASSIGN(MetaKey);
-            size_t               built;
+            unsigned             built;
             SharedString * const array;
 
         };
@@ -75,14 +96,30 @@ namespace Yttrium
             typedef MetaKeyData<NSUB> DataType;
 
             inline virtual ~MetaKeyWith() noexcept {}
-            inline explicit MetaKeyWith(const SharedString &uuid) :
+            inline explicit MetaKeyWith(const SharedString &uuid) noexcept:
             DataType(),
             MetaKey(this->data())
             {
                 push(uuid);
             }
 
-            inline virtual size_t maxi() const noexcept { return this->Maxi; }
+
+
+
+
+            inline explicit MetaKeyWith(const MetaKeyWith<NSUB-1> &root,
+                                        const unit_t               indx) noexcept:
+            DataType(),
+            MetaKey(this->data())
+            {
+                static IndexDataBase &idb = IndexDataBase::Instance();
+                const unsigned        num = root.size();
+                for(unsigned i=0;i<num;++i) push( root[i] );
+                push( idb[indx] );
+            }
+
+
+            inline virtual unsigned maxi() const noexcept { return this->Maxi; }
 
         private:
             Y_DISABLE_COPY_AND_ASSIGN(MetaKeyWith);
@@ -99,8 +136,14 @@ namespace Yttrium
 Y_UTEST(fieldmk)
 {
     const Field::SharedString uuid = new String("field");
-
     Field::MetaKeyWith<0> mk0(uuid);
+    std::cerr << mk0 << std::endl;
+
+    Field::MetaKeyWith<1> mk1(mk0,1);
+    std::cerr << mk1 << std::endl;
+
+    Field::MetaKeyWith<2> mk2(mk1,-3);
+    std::cerr << mk2 << std::endl;
 
 
 }
