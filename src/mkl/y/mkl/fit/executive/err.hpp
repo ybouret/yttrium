57ccf4ca9228a9898c5574c94653ad710fb79c7f
ybouret <yann.bouret@gmail.com>
2024-02-26 16:24:20 +0100

    wip

--- src/mkl/y/mkl/fit/executive/err.hpp
+++ src/mkl/y/mkl/fit/executive/err.hpp
@@ -4,18 +4,20 @@ template <
 typename FUNCTION,
 typename GRADIENT,
 typename SAMPLE>
-ABSCISSA err(FUNCTION           &F,
-             SAMPLE             &S,
-             Writable<ABSCISSA> &aorg,
-             const DomainType   &adom,
-             const Booleans     &used,
-             GRADIENT           &G,
-             XMLog              &xml)
+ABSCISSA err(FUNCTION                 &F,
+             SAMPLE                   &S,
+             const Readable<ABSCISSA> &aorg,
+             const DomainType         &adom,
+             const Booleans           &used,
+             GRADIENT                 &G,
+             XMLog                    &xml)
 {
 
     static const ABSCISSA zero(0);
 
     Y_XML_SECTION(xml,"LeastSquaresErr");
+
+    const ABSCISSA D2org = D2(F,S,aorg,used,G);
 
     return zero;
 
