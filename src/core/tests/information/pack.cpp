6a5932c01ca9e761cc132d7bc7976daeaf6cf00f
Yann Bouret <yann.bouret@unice.fr>
2024-03-15 08:35:52 +0100

    wip

--- src/core/tests/information/pack.cpp
+++ src/core/tests/information/pack.cpp
@@ -12,37 +12,9 @@ Y_UTEST(info_pack)
 {
 
     const char     Ch[] = { 'a', 'e', 'i', 'o', 'u' };
-    const unsigned Nc[] = { 12, 42, 9, 30, 7 };
+    const unsigned Nc[] = { 12,   42,   9,  30,  7  };
 
-    Entropic::Alphabet multiplex(true);
-    Entropic::Alphabet blockwise(false);
-    Entropic::Huffman  huff;
-    StreamBits         io;
-
-    multiplex.display(std::cerr);
-    blockwise.display(std::cerr);
-
-    for(unsigned i=0;i<sizeof(Ch)/sizeof(Ch[0]);++i)
-    {
-        const char c = Ch[i];
-        for(unsigned j=Nc[i];j>0;--j)
-        {
-            multiplex.write_(io,c);
-            blockwise.write_(io,c);
-        }
-    }
-
-    multiplex.display(std::cerr);
-    huff.build(multiplex.used);
-    multiplex.display(std::cerr);
-
-    blockwise.display(std::cerr);
-    huff.build(blockwise.used);
-    blockwise.display(std::cerr);
-
-    Y_SIZEOF(Entropic::Alphabet);
-    Y_SIZEOF(Entropic::Huffman);
-
+    
 
 }
 Y_UDONE()
