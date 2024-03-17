cac2aa7109308114efe9611f0107dfd047adb7b6
ybouret <yann.bouret@gmail.com>
2024-03-17 18:45:41 +0100

    wip

--- src/core/y/fft/xbitrev.hpp
+++ src/core/y/fft/xbitrev.hpp
@@ -65,6 +65,7 @@ namespace Yttrium
         }
 
 
+
         template <typename T> static inline
         size_t Run(T data[], const size_t size) noexcept
         {
@@ -118,6 +119,70 @@ namespace Yttrium
             return n;
         }
 
+        template <typename T> static inline
+        size_t Run(T data1[], T data2[], const size_t size) noexcept
+        {
+            assert(0!=data1);
+            assert(0!=data2);
+            assert(IsPowerOfTwo(size));
+
+            const size_t n = (size<<1);
+            switch(size)
+            {
+                case 0:
+                case 2:
+                    return n;
+
+#if 0
+                    Y_XBITREV(4);
+                    Y_XBITREV(8);
+                    Y_XBITREV(16);
+                    Y_XBITREV(32);
+                    Y_XBITREV(64);
+                    Y_XBITREV(128);
+                    Y_XBITREV(256);
+                    Y_XBITREV(512);
+                    Y_XBITREV(1024);
+                    Y_XBITREV(2048);
+                    Y_XBITREV(4096);
+#endif
+
+                default: {
+                    static const size_t Required = 2 * sizeof(T);
+                    void               *tmp[ Y_WORDS_GEQ(Required) ];
+
+                    for(size_t j=1,i=1;i<n;i+=2)
+                    {
+                        if(j>i)
+                        {
+                            {
+                                T * const lhs = &data1[i];
+                                T * const rhs = &data1[j];
+                                memcpy(tmp,lhs,Required);
+                                memcpy(lhs,rhs,Required);
+                                memcpy(rhs,tmp,Required);
+                            }
+                            {
+                                T * const lhs = &data2[i];
+                                T * const rhs = &data2[j];
+                                memcpy(tmp,lhs,Required);
+                                memcpy(lhs,rhs,Required);
+                                memcpy(rhs,tmp,Required);
+                            }
+                        }
+                        size_t m=size;
+                        while (m >= 2 && j > m)
+                        {
+                            j -= m;
+                            m >>= 1;
+                        }
+                        j += m;
+                    }
+                } break;
+            }
+            return n;
+        }
+
     private:
         struct Info { uint16_t i; uint16_t j; };
 
@@ -134,6 +199,33 @@ namespace Yttrium
                 memcpy(tmp,lhs,Required);
                 memcpy(lhs,rhs,Required);
                 memcpy(rhs,tmp,Required);
+            }
+        }
+
+        template <typename T,const unsigned N>
+        static inline void Call2(T data1[],  T data2[], const Info * const xbr) noexcept
+        {
+            static const size_t Required = 2 * sizeof(T);
+            void               *tmp[ Y_WORDS_GEQ(Required) ];
+            for(unsigned k=0;k<N;++k)
+            {
+                const Info    &_ = xbr[k];
+                const unsigned i = _.i;
+                const unsigned j = _.j;
+                {
+                    T * const lhs = &data1[i];
+                    T * const rhs = &data1[j];
+                    memcpy(tmp,lhs,Required);
+                    memcpy(lhs,rhs,Required);
+                    memcpy(rhs,tmp,Required);
+                }
+                {
+                    T * const lhs = &data2[i];
+                    T * const rhs = &data2[j];
+                    memcpy(tmp,lhs,Required);
+                    memcpy(lhs,rhs,Required);
+                    memcpy(rhs,tmp,Required);
+                }
             }
         }
 
