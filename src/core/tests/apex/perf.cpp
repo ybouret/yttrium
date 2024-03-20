f48ac84e231bdb7ad0f4643b3756076774b8744c
ybouret <yann.bouret@gmail.com>
2024-03-20 09:37:47 +0100

    wip

--- src/core/tests/apex/perf.cpp
+++ src/core/tests/apex/perf.cpp
@@ -47,7 +47,7 @@ namespace
                 Y_ASSERT( PROTO::AreEqual(LongProduct,FFT_Product) );
             }
 
-            std::cerr << "-- Checking for squares equality" << std::endl;
+            std::cerr << "-- Checking for squares  equality" << std::endl;
             for(size_t i=0;i<Loops;++i)
             {
                 const PROTO  lhs(5000,ran);
@@ -65,38 +65,59 @@ namespace
             }
         }
 
-        return;
 
 
-        const String fileName = Formatted::Get("apex%u-%u.dat",PROTO::CoreSize*8,PROTO::WordSize*8 );
-        OutputFile   fp(fileName);
-        for(unsigned lbits=32;lbits<=MaxBits;lbits <<= 1)
         {
-            for(unsigned rbits=32;rbits<=MaxBits;rbits <<= 1)
+            const String fileName = Formatted::Get("mul%u-%u.dat",PROTO::CoreSize*8,PROTO::WordSize*8 );
+            OutputFile   fp(fileName);
+            for(unsigned lbits=32;lbits<=MaxBits;lbits <<= 1)
             {
-                (std::cerr << std::setw(6) << lbits << " x " << std::setw(6) << rbits << " : ").flush();
-                uint64_t l64 = 0;
-                uint64_t f64 = 0;
-                for(size_t i=0;i<Loops;++i)
+                for(unsigned rbits=32;rbits<=MaxBits;rbits <<= 1)
                 {
-                    const PROTO  lhs(lbits,ran);
-                    const PROTO  rhs(rbits,ran);
-                    const hPROTO LNG_Prod    = PROTO::Mul(lhs,rhs,PROTO::LongMul,&l64);
-                    const hPROTO FFT_Prod    = PROTO::Mul(lhs,rhs,PROTO::FFT_Mul,&f64);
-                    const PROTO &LongProduct = *LNG_Prod;
-                    const PROTO &FFT_Product = *FFT_Prod;
-                    Y_ASSERT( PROTO::AreEqual(LongProduct,FFT_Product) );
-                }
-                const double lrate = double(Loops)/double(l64);
-                const double frate = double(Loops)/double(f64);
-                std::cerr << "long: " << std::setw(15) << lrate << " | fft: " << std::setw(15) << frate << std::endl;
-                fp("%u %u %.15g %.15g\n", lbits, rbits, log10(lrate), log10(frate) );
-                if(frate>=lrate)
-                {
-                    //fp("%u %u\n", lbits, rbits);
+                    (std::cerr << "mul " << std::setw(6) << lbits << " x " << std::setw(6) << rbits << " : ").flush();
+                    uint64_t l64 = 0;
+                    uint64_t f64 = 0;
+                    for(size_t i=0;i<Loops;++i)
+                    {
+                        const PROTO  lhs(lbits,ran);
+                        const PROTO  rhs(rbits,ran);
+                        const hPROTO LNG_Prod    = PROTO::Mul(lhs,rhs,PROTO::LongMul,&l64);
+                        const hPROTO FFT_Prod    = PROTO::Mul(lhs,rhs,PROTO::FFT_Mul,&f64);
+                        const PROTO &LongProduct = *LNG_Prod;
+                        const PROTO &FFT_Product = *FFT_Prod;
+                        Y_ASSERT( PROTO::AreEqual(LongProduct,FFT_Product) );
+                    }
+                    const double lrate = double(Loops)/double(l64);
+                    const double frate = double(Loops)/double(f64);
+                    std::cerr << "long: " << std::setw(15) << lrate << " | fft: " << std::setw(15) << frate << std::endl;
+                    fp("%u %u %.15g %.15g\n", lbits, rbits, log10(lrate), log10(frate) );
                 }
             }
         }
+
+        const String fileName = Formatted::Get("sqr%u-%u.dat",PROTO::CoreSize*8,PROTO::WordSize*8 );
+        OutputFile   fp(fileName);
+        for(unsigned lbits=32;lbits<=MaxBits;lbits <<= 1)
+        {
+            (std::cerr << "sqr " << std::setw(6) << lbits << " : ").flush();
+            uint64_t l64 = 0;
+            uint64_t f64 = 0;
+            for(size_t i=0;i<Loops;++i)
+            {
+                const PROTO  lhs(lbits,ran);
+                const hPROTO LNG_Prod    = PROTO::Sqr(lhs,PROTO::LongSqr,&l64);
+                const hPROTO FFT_Prod    = PROTO::Sqr(lhs,PROTO::FFT_Sqr,&f64);
+                const PROTO &LongSquared = *LNG_Prod;
+                const PROTO &FFT_Squared = *FFT_Prod;
+                Y_ASSERT( PROTO::AreEqual(LongSquared,FFT_Squared) );
+            }
+            const double lrate = double(Loops)/double(l64);
+            const double frate = double(Loops)/double(f64);
+            std::cerr << "long: " << std::setw(15) << lrate << " | fft: " << std::setw(15) << frate << std::endl;
+            fp("%u %.15g %.15g\n", lbits, log10(lrate), log10(frate) );
+        }
+
+
 
     }
 
--- src/core/y/apex/n/proto/fmul.hxx
+++ src/core/y/apex/n/proto/fmul.hxx
@@ -1,4 +1,9 @@
-
+//______________________________________________________________________________
+//
+//
+//! bytes to double
+//
+//______________________________________________________________________________
 static inline
 void FillArray(double         * const batch,
                const WordType * const w,
@@ -11,31 +16,85 @@ void FillArray(double         * const batch,
         MakeBytes::ReversePush(top,w[i]);
 }
 
-#if 0
+//______________________________________________________________________________
+//
+//
+//! convert FFT product to array of bytes
+//
+//______________________________________________________________________________
 static inline
-void FillArray_(double         * const batch,
-                const WordType * w,
-                const size_t     nWords,
-                const size_t     nBytes) noexcept
+void fft2bytes(uint8_t              prod[],
+               const size_t         mpn,
+               const double * const b,
+               const size_t         nn,
+               const double         scale)
 {
-    assert(nWords*WordSize==nBytes);
-    double *b = &batch[nBytes];
-    for(size_t i=nWords;i>0;--i)
+    static const double  RX    = 256.0;
+    static const double  IRX   = 0.00390625;
+    double               carry = 0.0;
+    const size_t         ntop  = mpn-1;
+    const double        *addr  = &b[nn];
+
+    for(size_t j=nn-ntop;j>0;--j)
     {
-        WordType              W = *(w++);
-        const uint8_t * const B = MakeBytes::BE(W);
-        for(size_t k=0;k<WordSize;++k)
-            *(b--) = B[k];
+        const double  t =  floor( *(addr--)*scale+carry+0.5 );
+        carry=(unsigned long) (t*IRX);
     }
+
+
+    uint8_t *p = &prod[mpn];
+    for(size_t j=ntop;j>0;--j)
+    {
+        const double  t =  floor( *(addr--)*scale+carry+0.5 );
+        carry=(unsigned long) (t*IRX);
+        *(p--) = static_cast<uint8_t>(t-carry*RX);
+    }
+
+
+    if(carry>=RX)
+        throw Specific::Exception(CallSign,"Invalid carry in FFT_Mul");
+
+    *p = static_cast<uint8_t>(carry);
 }
-#endif
 
-
-static inline Hexadecimal D2H(const double x) noexcept
+//______________________________________________________________________________
+//
+//
+//! convert array of bytes into words
+//
+//______________________________________________________________________________
+static inline
+void bytes2words(Pointer &             proto,
+                 const uint8_t * const prod,
+                 const size_t          mpn) noexcept
 {
-    return Hexadecimal( uint8_t(x) );
+    WordType * const W = proto->block.entry;
+    size_t     I = 0;
+    size_t     k = 0;
+    WordType   w = 0;
+    for(size_t i=mpn;i>0;--i)
+    {
+        static const unsigned kshift[4] = { 0, 8, 16, 24 };
+        w |= ( prod[i] << kshift[k] );
+        if(++k>=WordSize)
+        {
+            W[I++] = w;
+            k      = 0;
+            w      = 0;
+        }
+    }
+    if(k>0)
+        W[I] = w;
+    Coerce(proto->words)  = I;
+    proto->update();
 }
 
+//______________________________________________________________________________
+//
+//
+//! FFT Multiplication
+//
+//______________________________________________________________________________
 static inline
 Proto * FFT_Mul(const WordType * const U, const size_t p,
                 const WordType * const V, const size_t q,
@@ -66,10 +125,10 @@ Proto * FFT_Mul(const WordType * const U, const size_t p,
             const uint64_t  tmx    = chrono ? WallTime::Ticks() : 0;
             uint8_t *       prod   = 0;
 
-            //______________________________________________________________
+            //__________________________________________________________________
             //
             // prepare arrays of reals
-            //______________________________________________________________
+            //__________________________________________________________________
             size_t       nn = 1; //!< size for m+n
             size_t       ln = 0; //!< nn/2 = 2^ln for ScaleTable
             {
@@ -86,7 +145,7 @@ Proto * FFT_Mul(const WordType * const U, const size_t p,
                 double * const b = workspace();          // b[1..nn]
                 {                                        //
                     double * const a = b+nn;             // a[1..nn]
-                    Coerce(prod) = ((uint8_t *)&a[1])-1; // prod[1..mpn]
+                    prod = ((uint8_t *)&a[1])-1;         // prod[1..mpn]
                     FillArray(a,U,p,n);
                     FillArray(b,V,q,m);
 
@@ -114,67 +173,9 @@ Proto * FFT_Mul(const WordType * const U, const size_t p,
                     }
                 }
                 FFT::ReverseReal(b,nn);
-
-                //______________________________________________________________
-                //
-                // in place product as array of bytes
-                //______________________________________________________________
-                static const double  RX    = 256.0;
-                static const double  IRX   = 0.00390625;
-                double               carry = 0.0;
-                const double         scale = ScaleTable[ln];
-                const size_t         ntop  = mpn-1;
-                const double        *addr  = &b[nn];
-
-                for(size_t j=nn-ntop;j>0;--j)
-                {
-                    const double  t =  floor( *(addr--)*scale+carry+0.5 );
-                    carry=(unsigned long) (t*IRX);
-                }
-
-
-                uint8_t *p = &prod[mpn];
-                for(size_t j=ntop;j>0;--j)
-                {
-                    const double  t =  floor( *(addr--)*scale+carry+0.5 );
-                    carry=(unsigned long) (t*IRX);
-                    *(p--) = static_cast<uint8_t>(t-carry*RX);
-                }
-
-
-                if(carry>=RX)
-                    throw Specific::Exception(CallSign,"Invalid carry in FFT_Mul");
-
-                *p = static_cast<uint8_t>(carry);
+                fft2bytes(prod,mpn,b,nn,ScaleTable[ln]);
             }
-
-
-
-            //__________________________________________________________________
-            //
-            // transform array of bytes into array of words
-            //__________________________________________________________________
-            {
-                WordType *W = proto->block.entry;
-                size_t    I = 0;
-                size_t    k = 0;
-                WordType  w = 0;
-                for(size_t i=mpn;i>0;--i)
-                {
-                    static const unsigned kshift[4] = { 0, 8, 16, 24 };
-                    w |= ( prod[i] << kshift[k] );
-                    if(++k>=WordSize)
-                    {
-                        W[I++] = w;
-                        k      = 0;
-                        w      = 0;
-                    }
-                }
-                if(k>0)
-                    W[I] = w;
-                Coerce(proto->words)  = I;
-                proto->update();
-            }
+            bytes2words(proto,prod,mpn);
 
             //__________________________________________________________________
             //
@@ -186,6 +187,12 @@ Proto * FFT_Mul(const WordType * const U, const size_t p,
     }
 }
 
+//______________________________________________________________________________
+//
+//
+//! FFT Square
+//
+//______________________________________________________________________________
 static inline
 Proto * FFT_Sqr(const WordType * const U, const size_t p, uint64_t *ell)
 {
@@ -199,17 +206,17 @@ Proto * FFT_Sqr(const WordType * const U, const size_t p, uint64_t *ell)
     {
 
         assert(p>0);
-        const size_t    n     = p * WordSize; // U bytes
-        const size_t    mpn   = n+n;          // product bytes
-        Pointer         proto = new Proto(mpn,AsCapacity);
+        const size_t    n      = p * WordSize; // U bytes
+        const size_t    mpn    = n<<1;         // product bytes
+        Pointer         proto  = new Proto(mpn,AsCapacity);
         const bool      chrono = 0!=ell;
         const uint64_t  tmx    = chrono ? WallTime::Ticks() : 0;
         uint8_t *       prod   = 0;
 
-        //______________________________________________________________
+        //__________________________________________________________________
         //
         // prepare arrays of reals
-        //______________________________________________________________
+        //__________________________________________________________________
         size_t       nn = 1; //!< size for m+n
         size_t       ln = 0; //!< nn/2 = 2^ln for ScaleTable
         {
@@ -225,16 +232,16 @@ Proto * FFT_Sqr(const WordType * const U, const size_t p, uint64_t *ell)
             double * const b = workspace();          // b[1..nn]
             {                                        //
                 double * const a = b+nn;             // a[1..nn]
-                Coerce(prod) = ((uint8_t *)&a[1])-1; // prod[1..mpn]
+                prod = ((uint8_t *)&a[1])-1;         // prod[1..mpn]
                 FillArray(b,U,p,n);
 
                 //__________________________________________________________
                 //
                 // forward real FFT
                 //__________________________________________________________
-                //FFT::ForwardReal(a,b,nn);
                 FFT::ForwardReal(b,nn);
-                
+
+
                 //__________________________________________________________
                 //
                 // in-place product in b
@@ -243,75 +250,17 @@ Proto * FFT_Sqr(const WordType * const U, const size_t p, uint64_t *ell)
                 b[2] *= b[2];
                 for(size_t j=3;j<=nn;j+=2)
                 {
-                    const size_t j1 = j+1;
+                    const size_t j1  = j+1;
                     const double re  = b[j];
                     const double im  = b[j1];
                     b[j]  = re*re - im*im;
-                    b[j1] = re*im + im*re;
+                    b[j1] = Twice(re*im);
                 }
             }
             FFT::ReverseReal(b,nn);
-
-            //______________________________________________________________
-            //
-            // in place product as array of bytes
-            //______________________________________________________________
-            static const double  RX    = 256.0;
-            static const double  IRX   = 0.00390625;
-            double               carry = 0.0;
-            const double         scale = ScaleTable[ln];
-            const size_t         ntop  = mpn-1;
-            const double        *addr  = &b[nn];
-
-            for(size_t j=nn-ntop;j>0;--j)
-            {
-                const double  t =  floor( *(addr--)*scale+carry+0.5 );
-                carry=(unsigned long) (t*IRX);
-            }
-
-
-            uint8_t *p = &prod[mpn];
-            for(size_t j=ntop;j>0;--j)
-            {
-                const double  t =  floor( *(addr--)*scale+carry+0.5 );
-                carry=(unsigned long) (t*IRX);
-                *(p--) = static_cast<uint8_t>(t-carry*RX);
-            }
-
-
-            if(carry>=RX)
-                throw Specific::Exception(CallSign,"Invalid carry in FFT_Mul");
-
-            *p = static_cast<uint8_t>(carry);
+            fft2bytes(prod,mpn,b,nn,ScaleTable[ln]);
         }
-
-
-
-        //__________________________________________________________________
-        //
-        // transform array of bytes into array of words
-        //__________________________________________________________________
-        {
-            WordType *W = proto->block.entry;
-            size_t    I = 0;
-            size_t    k = 0;
-            WordType  w = 0;
-            for(size_t i=mpn;i>0;--i)
-            {
-                static const unsigned kshift[4] = { 0, 8, 16, 24 };
-                w |= ( prod[i] << kshift[k] );
-                if(++k>=WordSize)
-                {
-                    W[I++] = w;
-                    k      = 0;
-                    w      = 0;
-                }
-            }
-            if(k>0)
-                W[I] = w;
-            Coerce(proto->words)  = I;
-            proto->update();
-        }
+        bytes2words(proto,prod,mpn);
 
         //__________________________________________________________________
         //
