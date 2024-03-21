2e62b80c41ce3d80ed7a0fedddfce4c8a26d5f73
Yann Bouret <yann.bouret@unice.fr>
2024-03-21 15:37:39 +0100

    wip

--- src/core/tests/calculus/prime32.cpp
+++ src/core/tests/calculus/prime32.cpp
@@ -18,9 +18,13 @@ Y_UTEST(calculus_prime32)
     uint64_t next = 0;
     OutputFile::Overwrite("zprime.dat");
 
+    size_t freq[256];
+    memset(freq,0,sizeof(freq));
+
     while( true )
     {
         const uint64_t q = Prime64::Next(p+1);
+        if(BitsFor(q)>24) break;
         const uint64_t d = q-p;  Y_ASSERT(0==(d&1));
         const uint64_t h = d>>1; Y_ASSERT(h>0);
         const uint64_t z = h-1;
@@ -38,6 +42,14 @@ Y_UTEST(calculus_prime32)
             break;
         }
 
+        const uint8_t b(z);
+        ++freq[b];
+
+        {
+            AppendFile fp("zprime.dat");
+            fp.write( uint8_t(z) );
+        }
+        if(false)
         {
             AppendFile fp("zprime.dat");
             fp("%g %g %g %g\n", double(q), double(z), double (zmax), double(BitsFor(zmax)) );
@@ -48,6 +60,15 @@ Y_UTEST(calculus_prime32)
     }
     std::cerr << "zmax=" << zmax << " between " << prev << " and " << next << std::endl;
     std::cerr << "count = " << count << std::endl;
+
+    {
+        OutputFile fp("hist.dat");
+        for(unsigned i=0;i<256;++i)
+        {
+            fp("%g %g\n", double(i), double(freq[i]));
+        }
+    }
+
 }
 Y_UDONE()
 
