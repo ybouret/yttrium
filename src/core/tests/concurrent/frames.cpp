b2facde6f2bcf222af60db2306df7bd089d5089a
ybouret <yann.bouret@gmail.com>
2024-03-25 16:31:02 +0100

    wip

--- src/core/tests/concurrent/frames.cpp
+++ src/core/tests/concurrent/frames.cpp
@@ -46,7 +46,6 @@ namespace Yttrium
             class  Punctual
             {
             public:
-                typedef int Type;
                 explicit Punctual() noexcept {}
                 virtual ~Punctual() noexcept {}
 
@@ -82,7 +81,7 @@ namespace Yttrium
                 if(this->workspace.isValid())
                 {
                     this->shutdown();
-                    this->workspace.dismiss();
+                    this->workspace.erase();
                 }
             }
 
@@ -102,6 +101,7 @@ namespace Yttrium
         class Frame0D : public Frame<Nucleus::Punctual>
         {
         public:
+            typedef int               Type;
             typedef Nucleus::Punctual Mapping;
             typedef Frame<Mapping>    FrameType;
 
@@ -128,12 +128,13 @@ namespace Yttrium
         class Frame1D : public Frame< ForLoop<T> >
         {
         public:
+            typedef T              Type;
             typedef ForLoop<T>     Mapping;
             typedef Frame<Mapping> FrameType;
 
             virtual ~Frame1D() noexcept {}
 
-            void init(const T head, const T tail, const T step)
+            void init(const T &head, const T &tail, const T &step)
             {
                 assert(this->workspace.isEmpty());
                 const Mapping mapping = Split::For(*this, head, tail, step);
@@ -157,11 +158,22 @@ namespace Yttrium
         class Frame2D : public Frame< AutoPtr< typename Tiling<T>::Tile > >
         {
         public:
+            typedef T                        Type;
             typedef typename Tiling<T>::Tile Tile;
-            typedef AutoPtr<Tile>  Mapping;
-            typedef Frame<Mapping> FrameType;
+            typedef AutoPtr<Tile>            Mapping;
+            typedef Frame<Mapping>           FrameType;
 
             virtual ~Frame2D() noexcept {}
+
+            void init(const V2D<T> &lower, const V2D<T> &upper)
+            {
+                assert(this->workspace.isEmpty());
+                const Mapping mapping = Tiling<T>::Tiles::For(*this,lower,upper);
+                this->workspace.build(mapping);
+                this->activate();
+                assert(mapping.isEmpty());
+                assert(this->workspace.isValid());
+            }
 
 
         protected:
@@ -249,14 +261,24 @@ namespace Yttrium
                 catch(...) { quit(); throw; }
             }
 
-            inline void init(const typename Mapping::Type head,
-                             const typename Mapping::Type tail,
-                             const typename Mapping::Type step)
+            inline void init(const typename FrameType::Type head,
+                             const typename FrameType::Type tail,
+                             const typename FrameType::Type step)
             {
                 quit();
                 Writable<FRAME> &self = *this;
                 const size_t     n    = self.size();
                 try { for(size_t i=1;i<=n;++i) self[i].init(head,tail,step); }
+                catch(...) { quit(); throw; }
+            }
+
+            inline void init(const V2D<typename FrameType::Type> lower,
+                             const V2D<typename FrameType::Type> upper)
+            {
+                quit();
+                Writable<FRAME> &self = *this;
+                const size_t     n    = self.size();
+                try { for(size_t i=1;i<=n;++i) self[i].init(lower,upper); }
                 catch(...) { quit(); throw; }
             }
 
@@ -293,12 +315,12 @@ namespace
 
         virtual void activate()
         {
-            std::cerr << "Demo0D activate@" << name << std::endl;
+            std::cerr << "+0D@" << name << std::endl;
         }
 
         virtual void shutdown() noexcept
         {
-            std::cerr << "Demo0D shutdown@" << name << std::endl;
+            std::cerr << "-0D@" << name << std::endl;
         }
 
     };
@@ -318,12 +340,12 @@ namespace
         Y_DISABLE_COPY_AND_ASSIGN(Demo1D);
         virtual void activate()
         {
-            std::cerr << "Demo1D activate" << std::endl;
+            std::cerr << "+1D@" << name << std::endl;
         }
 
         virtual void shutdown() noexcept
         {
-            std::cerr << "Demo1D shutdown" << std::endl;
+            std::cerr << "-1D@" << name << std::endl;
         }
     };
 
@@ -343,12 +365,12 @@ namespace
         Y_DISABLE_COPY_AND_ASSIGN(Demo2D);
         virtual void activate()
         {
-            std::cerr << "Demo2D activate" << std::endl;
+            std::cerr << "+2D@" << name << std::endl;
         }
 
         virtual void shutdown() noexcept
         {
-            std::cerr << "Demo2D shutdown" << std::endl;
+            std::cerr << "-2D@" << name << std::endl;
         }
     };
 
@@ -368,39 +390,54 @@ Y_UTEST(concurrent_frames)
     Concurrent::SharedPipeline seqPipe = new Concurrent::Alone();
     Concurrent::SharedPipeline parPipe = new Concurrent::Queue(topo);
 
+    {
 
-    Concurrent::Frames<Demo0D> f0seq(seqPipe);
-    Concurrent::Frames<Demo0D> f0par(parPipe);
+        Concurrent::Frames<Demo0D> f0seq(seqPipe);
+        Concurrent::Frames<Demo0D> f0par(parPipe);
 
-    std::cerr << "f0seq=" << f0seq << std::endl;
-    std::cerr << "f0par=" << f0par << std::endl;
-
-
-    f0seq.init();
-    f0par.init();
-
-    std::cerr << "f0seq=" << f0seq << std::endl;
-    std::cerr << "f0par=" << f0par << std::endl;
+        std::cerr << "f0seq=" << f0seq << std::endl;
+        std::cerr << "f0par=" << f0par << std::endl;
 
 
+        f0seq.init();
+        f0par.init();
 
-    Concurrent::Frames<Demo1D> f1seq(seqLoop);
-    Concurrent::Frames<Demo1D> f1par(parLoop);
+        std::cerr << "f0seq=" << f0seq << std::endl;
+        std::cerr << "f0par=" << f0par << std::endl;
 
-    std::cerr << "f1seq=" << f1seq << std::endl;
-    std::cerr << "f1par=" << f1par << std::endl;
+    }
+
+    {
+        Concurrent::Frames<Demo1D> f1seq(seqLoop);
+        Concurrent::Frames<Demo1D> f1par(parLoop);
+
+        std::cerr << "f1seq=" << f1seq << std::endl;
+        std::cerr << "f1par=" << f1par << std::endl;
 
 
-    f1seq.init(1,10,2);
-    f1par.init(1,10,2);
-    
-    std::cerr << "f1seq=" << f1seq << std::endl;
-    std::cerr << "f1par=" << f1par << std::endl;
+        f1seq.init(1,10,2);
+        f1par.init(1,10,2);
 
+        std::cerr << "f1seq=" << f1seq << std::endl;
+        std::cerr << "f1par=" << f1par << std::endl;
 
-    Concurrent::Frames<Demo2D> f2seq(seqLoop);
-    Concurrent::Frames<Demo2D> f2par(parLoop);
+    }
 
+    {
+        Concurrent::Frames<Demo2D> f2seq(seqLoop);
+        Concurrent::Frames<Demo2D> f2par(parLoop);
+        std::cerr << "f2seq=" << f2seq << std::endl;
+        std::cerr << "f2par=" << f2par << std::endl;
+
+        const V2D<unit_t> lower(1,1);
+        const V2D<unit_t> upper(10,10);
+
+        f2seq.init( lower, upper );
+        f2par.init( lower, upper );
+
+        std::cerr << "f2seq=" << f2seq << std::endl;
+        std::cerr << "f2par=" << f2par << std::endl;
+    }
 
 }
 Y_UDONE()
