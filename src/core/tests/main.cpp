db04ee2fc6058bc5c5eefb4136c30230eb85d774
ybouret <yann.bouret@gmail.com>
2024-03-26 09:09:24 +0100

    wip

--- src/core/tests/main.cpp
+++ src/core/tests/main.cpp
@@ -109,7 +109,6 @@ Y_UTEST_DECL(256)
     Y_UTEST(concurrent_pipeline);
     Y_UTEST(concurrent_resource);
     Y_UTEST(concurrent_dispatcher);
-    Y_UTEST(concurrent_frames);
     Y_UTEST(concurrent_frame0d);
     Y_UTEST(concurrent_frame1d);
     Y_UTEST(concurrent_frame2d);
