6a9ce0cdd489ff0a11b81a68f075261a488ecc5a
ybouret <yann.bouret@gmail.com>
2024-04-15 10:27:44 +0200

    wip

--- src/core/y/container/operating.hpp
+++ src/core/y/container/operating.hpp
@@ -201,6 +201,7 @@ namespace Yttrium
             assert(0!=user);
             SEQUENCE &source = *static_cast<SEQUENCE *>(user);
             std::cerr << "source = " << source << std::endl;
+            (std::cerr << "source[" << indx << "]=").flush() << source[indx] << std::endl;
             assert(indx>=1);
             assert(indx<=source.size());
             new (addr) MutableType( source[indx] );
