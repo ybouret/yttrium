eb5fb90a2b53748d8ff27b7444cda5bc29a43e42
ybouret <yann.bouret@gmail.com>
2024-04-15 09:58:13 +0200

    wip

--- src/core/y/container/operating.hpp
+++ src/core/y/container/operating.hpp
@@ -198,6 +198,7 @@ namespace Yttrium
         {
             assert(0!=addr);
             assert(0!=user);
+            std::cerr << "XProcAny@" << addr << ", Sequence@" << user << ", indx=" << indx << std::endl;
             SEQUENCE &source = *static_cast<SEQUENCE *>(user);
             assert(indx>=1);
             assert(indx<=source.size());
