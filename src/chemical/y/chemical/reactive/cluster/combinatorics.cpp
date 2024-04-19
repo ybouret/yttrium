b0f224f9e725cbffb633ef39fe0d3f57d2e74c22
ybouret <yann.bouret@gmail.com>
2024-04-19 22:46:34 +0200

    wip

--- src/chemical/y/chemical/reactive/cluster/combinatorics.cpp
+++ src/chemical/y/chemical/reactive/cluster/combinatorics.cpp
@@ -8,6 +8,33 @@ namespace Yttrium
 {
     namespace Chemical
     {
+
+        class Coefficients
+        {
+        public:
+
+            explicit Coefficients(const Readable<int> &w,
+                                  const Readable<int> &s,
+                                  const size_t         d) :
+            weight(CopyOf,w),
+            stoich(CopyOf,s),
+            snorm1(0),
+            order(d)
+            {
+
+            }
+
+            virtual ~Coefficients() noexcept {}
+
+            const CxxArray<int> weight;
+            const CxxArray<int> stoich;
+            const apz           snorm1;
+            const size_t        order;
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Coefficients);
+        };
+
         void Cluster:: buildCombinatorics(Equilibria &eqs, XMLog &xml)
         {
             WOVEn::IntegerSurvey survey(xml);
@@ -20,10 +47,27 @@ namespace Yttrium
                 Y_XMLOG(xml,"NuT  = "   << NuT);
                 Y_XMLOG(xml,"NuTx = "  << NuTx);
                 WOVEn::Explore(NuTx, survey, false, xml);
+                survey.sort();
             }
 
             // convert to stoichiometry
-            
+            const size_t  n = Nu.rows;
+            const size_t  m = Nu.cols;
+            CxxArray<int> weight(n,0);
+            CxxArray<apz> apzSto(m,0);
+
+            for(const WOVEn::IntegerArray *arr=survey.head;arr;arr=arr->next)
+            {
+                assert(arr->size()==n);
+                const Readable<const apz> &w = *arr;
+                for(size_t i=n;i>0;--i) weight[i] = w[i].cast<int>("weight");
+                for(size_t j=m;j>0;--j)
+                {
+                    apzSto[j] = 0;
+                    for(size_t i=n;i>0;--i)
+                        apzSto[j] += w[j] * Nu[i][j];
+                }
+            }
 
         }
     }
