089d765ce097ad8191e92bcc6dc33b972f75d8e8
Yann Bouret <yann.bouret@unice.fr>
2024-05-27 13:13:02 +0200

    wip

--- src/core/tests/apk/component.cpp
+++ src/core/tests/apk/component.cpp
@@ -57,7 +57,7 @@ namespace Yttrium
             void load(const Assembly<U> &source)
             {
                 // TARGET = T, SOURCE = U
-                const size_t cycles = TOW::API<T,U>::Cycles(space,source.space);
+                const size_t cycles = TOW::API<T,U>::Cycles(count,source.count);
                 TOW::Transmute(entry,source.entry,cycles);
             }
 
@@ -92,11 +92,10 @@ num64(entry,num32.space>>1)
             entry( EntryFor(usrBytes,shift) ),
             Y_APK_Component_Ctor()
             {
-                std::cerr << "bytes : " << bytes << std::endl;
-                std::cerr << "num16 : " << num16 << std::endl;
-                std::cerr << "num32 : " << num32 << std::endl;
-                std::cerr << "num64 : " << num64 << std::endl;
-
+                //std::cerr << "bytes : " << bytes << std::endl;
+                //std::cerr << "num16 : " << num16 << std::endl;
+                //std::cerr << "num32 : " << num32 << std::endl;
+                //std::cerr << "num64 : " << num64 << std::endl;
             }
 
             explicit Component(const size_t numBits, Random::Bits &ran) :
@@ -109,16 +108,88 @@ num64(entry,num32.space>>1)
             {
                 if(bits>0)
                 {
-                    Coerce(bytes.count) = Y_ALIGN8(bits)/8; assert(bytes.count>0);
-                    
+
+                    const size_t   msi = (Coerce(bytes.count) = Y_ALIGN8(bits)/8)-1; assert(bytes.count>0);
+                    const unsigned msn = static_cast<unsigned>(bits - (msi<<3));     assert(msn>0);
+
+                    for(size_t i=0;i<msi;++i) Coerce(bytes.entry[i]) = ran.to<uint8_t>();
+                   
+                    Coerce(bytes.entry[msi]) = ran.to<uint8_t>(msn);
+                    Coerce(num16.count) = Y_ALIGN16(bits)/16;
+                    Coerce(num32.count) = Y_ALIGN32(bits)/32;
+                    Coerce(num64.count) = Y_ALIGN64(bits)/64;
                 }
             }
 
+            inline friend std::ostream & operator<<(std::ostream &os, const Component &self)
+            {
+                switch(self.state)
+                {
+                    case AsBytes: os << self.bytes; break;;
+                    case AsNum16: os << self.num16; break;;
+                    case AsNum32: os << self.num32; break;;
+                    case AsNum64: os << self.num64; break;;
+                }
+                return os;
+            }
 
             virtual ~Component() noexcept 
             {
                 Memory::Archon::Release(entry,shift);
             }
+
+
+            Component & set(const InnerState st) noexcept
+            {
+                switch(st)
+                {
+                    case AsBytes:
+                        switch(state)
+                        {
+                            case AsBytes: return *this;
+                            case AsNum16: Coerce(bytes).load(num16); break;
+                            case AsNum32: Coerce(bytes).load(num32); break;
+                            case AsNum64: Coerce(bytes).load(num64); break;
+                        }
+                        Coerce(state) = st;
+                        break;
+
+                    case AsNum16:
+                        switch(state)
+                        {
+                            case AsBytes: Coerce(num16).load(bytes); break;
+                            case AsNum16: return *this;
+                            case AsNum32: Coerce(num16).load(num32); break;
+                            case AsNum64: Coerce(num16).load(num64); break;
+                        }
+                        Coerce(state) = AsNum16;
+                        break;
+
+                    case AsNum32:
+                        switch(state)
+                        {
+                            case AsBytes: Coerce(num32).load(bytes); break;
+                            case AsNum16: Coerce(num32).load(num16); break;
+                            case AsNum32: return *this;
+                            case AsNum64: Coerce(num32).load(num64); break;
+                        }
+                        Coerce(state) = AsNum32;
+                        break;
+
+                    case AsNum64:
+                        switch(state)
+                        {
+                            case AsBytes: Coerce(num64).load(bytes); break;
+                            case AsNum16: Coerce(num64).load(num16); break;
+                            case AsNum32: Coerce(num64).load(num32); break;
+                            case AsNum64: return *this;
+                        }
+                        Coerce(state) = AsNum64;
+                        break;
+                }
+                return *this;
+            }
+
 
             const size_t            bits;
             const InnerState        state;
@@ -170,7 +241,26 @@ using namespace Yttrium;
 Y_UTEST(apk_component)
 {
 
-    APK::Component cm(0);
+    Random::ParkMiller ran;
+
+    for(size_t i=0;i<=80;++i)
+    {
+        APK::Component cm(i);
+        Y_ASSERT(cm.bytes.space>=i);
+        //std::cerr << cm << std::endl;
+    }
+
+    for(size_t i=0;i<=64;++i)
+    {
+        APK::Component cm(i,ran); Y_ASSERT(APK::AsBytes==cm.state);
+        std::cerr << cm << std::endl;
+        std::cerr << cm.set(APK::AsNum16) << std::endl;
+        std::cerr << cm.set(APK::AsNum32) << std::endl;
+        std::cerr << cm.set(APK::AsNum64) << std::endl;
+
+        std::cerr << std::endl;
+    }
+
 
     Y_SIZEOF(APK::Component);
 }
