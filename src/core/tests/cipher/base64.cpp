8110778afaeef5674c74c6e1e59359cefaca2f09
Yann Bouret <yann.bouret@unice.fr>
2024-06-24 13:45:04 +0200

    wip

--- src/core/tests/cipher/base64.cpp
+++ src/core/tests/cipher/base64.cpp
@@ -1,5 +1,4 @@
-#include "y/cipher/stream/buffered.hpp"
-#include "y/text/base64/encode.hpp"
+#include "y/cipher/stream/base64/encoder.hpp"
 
 #include "y/utest/run.hpp"
 #include <cstring>
@@ -7,105 +6,7 @@
 
 using namespace Yttrium;
 
-namespace Yttrium
-{
 
-    namespace Base64
-    {
-        class Encoder : public BufferedStreamCipher
-        {
-        public:
-            static const char * const CallSign;
-
-            enum State
-            {
-                WaitFor1,
-                WaitFor2,
-                WaitFor3
-            };
-
-            explicit Encoder(bool padding) noexcept :
-            BufferedStreamCipher(),
-            doPad(padding),
-            state(WaitFor1),
-            input()
-            {}
-
-            virtual ~Encoder() noexcept {}
-
-            virtual const char * callSign() const noexcept { return CallSign; }
-
-
-            virtual void write(const char C)
-            {
-                switch(state)
-                {
-                    case WaitFor1: input[0] = C; state = WaitFor2; break;
-                    case WaitFor2: input[1] = C; state = WaitFor3; break;
-                    case WaitFor3: input[2] = C; flush3(); break;
-                }
-            }
-
-            virtual void flush() noexcept
-            {
-                switch(state)
-                {
-                    case WaitFor1: break;
-                    case WaitFor2: flush1(); break;
-                    case WaitFor3: flush2(); break;
-                }
-                assert(WaitFor1==state);
-            }
-
-
-            virtual void reset() noexcept
-            {
-                buffer.release();
-                waitFor1();
-            }
-
-            bool   doPad;
-        private:
-            Y_DISABLE_COPY_AND_ASSIGN(Encoder);
-            State  state;
-            char   input[4];
-
-
-            void   waitFor1() noexcept
-            {
-                state = WaitFor1;
-                memset(input,0,sizeof(input));
-            }
-
-            void flush1() {
-                char output[4] = {0,0,0,0};
-                send(output,Encode::_1(output, input[0], doPad));
-            }
-
-            void flush2() {
-                char output[4] = {0,0,0,0};
-                send(output,Encode::_2(output, input[0], input[1], doPad));
-            }
-
-            void   flush3() {
-                char output[4] = {0,0,0,0};
-                send(output,Encode::_3(output, input[0], input[1], input[2]));
-            }
-
-            void send(const char *p, size_t n)
-            {
-                IO::Chars out;
-                while(n-- > 0 ) out << *(p++);
-                buffer.mergeTail(out);
-                waitFor1();
-            }
-
-        };
-
-        const char * const Encoder:: CallSign = "Base64-Encoder";
-    }
-
-}
 
 Y_UTEST(cipher_base64)
 {
---
+++ src/core/y/cipher/stream/base64/encoder.hpp
@@ -0,0 +1,104 @@
+
+//! \file
+
+#ifndef Y_Base64_Encoder_Included
+#define Y_Base64_Encoder_Included 1
+
+#include "y/cipher/stream/buffered.hpp"
+
+
+namespace Yttrium
+{
+
+    namespace Base64
+    {
+        //______________________________________________________________________
+        //
+        //
+        //
+        //! Base64 Encoder
+        //
+        //
+        //______________________________________________________________________
+        class Encoder : public BufferedStreamCipher
+        {
+        public:
+            //__________________________________________________________________
+            //
+            //
+            // Definitions
+            //
+            //__________________________________________________________________
+            static const char * const CallSign; //!< "Base64-Encoder"
+
+            //! internal state
+            enum State
+            {
+                WaitFor1, //!< wait for 1st char in input[0]
+                WaitFor2, //!< wait for 2nd char in input[1]
+                WaitFor3  //!< wait for 3rd char in input[2]
+            };
+
+            //__________________________________________________________________
+            //
+            //
+            // C++
+            //
+            //__________________________________________________________________
+            explicit Encoder(bool padding) noexcept; //!< setup
+            virtual ~Encoder()             noexcept; //!< cleanup
+
+            //__________________________________________________________________
+            //
+            //
+            // [Identifiable]
+            //
+            //__________________________________________________________________
+            virtual const char * callSign() const noexcept;
+
+            //__________________________________________________________________
+            //
+            //
+            // [OutputStream]
+            //
+            //__________________________________________________________________
+            virtual void write(const char C);
+            virtual void flush();
+
+            //__________________________________________________________________
+            //
+            //
+            // [Cipher]
+            //
+            //__________________________________________________________________
+            virtual void reset() noexcept;
+
+
+            //__________________________________________________________________
+            //
+            //
+            // Members
+            //
+            //__________________________________________________________________
+            bool   doPad; //!< padding option
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Encoder);
+            State  state;
+            char   input[4];
+
+
+            void waitFor1() noexcept;
+            void flush1();
+            void flush2();
+            void flush3();
+            void send(const char *p, size_t n);
+
+        };
+
+    }
+
+}
+
+#endif
+
