091a2be4443b940647ddf783b62ab815865d11f0
Yann Bouret <yann.bouret@unice.fr>
2024-07-18 15:02:49 +0200

    wip

--- src/core/tests/counting/nested.cpp
+++ src/core/tests/counting/nested.cpp
@@ -1,6 +1,7 @@
 #include "y/container/cxx/array.hpp"
 #include "y/utest/run.hpp"
 
+#include "y/kemp/natural.hpp"
 
 using namespace Yttrium;
 
@@ -12,11 +13,31 @@ namespace Yttrium
     {
     public:
 
-        explicit NestedLoop(const size_t dims, 
+        static apn apCount(const size_t d,
+                           const size_t n)
+        {
+            assert(d>=1);
+            assert(n>=d);
+            apn res = 1;
+            for(size_t i=0;i<d;++i) {
+                res *= n-i;
+            }
+            return res / apn::Factorial(d);
+        }
+
+        explicit NestedLoop(const size_t dims,
                             const size_t nmax) :
         LoopArray(dims,0),
-        maxi(nmax)
+        imin(dims,0),
+        imax(dims,0)
         {
+            assert(nmax>=dims);
+            const size_t delta = nmax-dims;
+            for(size_t i=1;i<=dims;++i)
+            {
+                Coerce(imin[i]) = i;
+                Coerce(imax[i]) = i+delta;
+            }
             init();
         }
 
@@ -26,46 +47,109 @@ namespace Yttrium
 
         bool next()
         {
-            const size_t      n = size();
-            Writable<size_t> &u = *this;
+            return move( size() );
+        }
 
-            for(size_t i=n;i>0;--i)
+        bool move(const size_t k)
+        {
+            size_t &i = (*this)[k];
+            if(i<imax[k])
             {
-                size_t &j = u[i];
-                if(j<maxi)
-                {
-                    // update local index
-                    ++j;
-                    return true;
-                }
-                else
+                ++i;
+                return true;
+            }
+            else
+            {
+                if(k<=1)
                 {
                     return false;
                 }
+                else
+                {
+                    if(move(k-1))
+                    {
+                        for(size_t j=k;j<=size();++j)
+                        {
+                            (*this)[j] = (*this)[j-1] + 1;
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
             }
-
-            return false;
         }
 
-        const size_t maxi;
 
-
+        const LoopArray imin;
+        const LoopArray imax;
     private:
         Y_DISABLE_COPY_AND_ASSIGN(NestedLoop);
-        void init() noexcept { for(size_t i=size();i>0;--i) (*this)[i] = i; }
+        void init() noexcept { for(size_t i=size();i>0;--i) (*this)[i] = imin[i]; }
+
     };
 }
+
+namespace {
+
+    static inline
+    size_t test3d(const size_t nmax)
+    {
+        size_t n3d = 0;
+        NestedLoop       loop(3,nmax);
+        CxxArray<size_t> v(3,0);
+        bool             res = true;
+        for(size_t i=1;i<=nmax;++i)
+        {
+            for(size_t j=i+1;j<=nmax;++j)
+            {
+                for(size_t k=j+1;k<=nmax;++k)
+                {
+                    v[1] = i; v[2] = j; v[3] = k;
+                    std::cerr << v << "/" << v << std::endl;
+                    Y_ASSERT(loop==v);
+                    ++n3d;
+                    res = loop.next();
+                }
+            }
+        }
+        Y_ASSERT(!res);
+        std::cerr << "n(3," << nmax << ")=" << n3d << std::endl;
+        return n3d;
+    }
+}
+
+#include "y/text/ascii/convert.hpp"
+#include "y/stream/libc/output.hpp"
 
 Y_UTEST(counting_nested)
 {
 
-    NestedLoop loop(3,4);
-
-    std::cerr << loop << std::endl;
-    while(loop.next())
+    size_t nmax = 4;
+    if(argc>1) nmax = ASCII::Convert::To<size_t>(argv[1],"nmax");
     {
-        std::cerr << loop << std::endl;
+        OutputFile fp("n3d.dat");
+        for(size_t n=3;n<=nmax;++n)
+        {
+            const size_t n3d = test3d(n);
+            fp("%u %u\n", unsigned(n), unsigned(n3d)).flush();
+        }
     }
+
+    for(size_t d=1;d<=5;++d)
+    {
+        std::cerr << "d=" << d << std::endl;
+        for(size_t n=d;n<=10;++n)
+        {
+            std::cerr << "\tn=" << n << " => " << NestedLoop::apCount(d,n) << std::endl;
+        }
+    }
+
+    return 0;
+
+
 
 }
 Y_UDONE()
