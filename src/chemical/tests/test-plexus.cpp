07b1390989014481300862ce4af121efb3c7d506
ybouret <yann.bouret@gmail.com>
2024-08-29 09:59:32 +0200

    wip

--- src/chemical/tests/test-plexus.cpp
+++ src/chemical/tests/test-plexus.cpp
@@ -1068,6 +1068,10 @@ namespace Yttrium
                     assert(0==wobbly.size);
                     if(lawz.size>0)
                         enforceZeroLaws(C,L,xml); //!< will set positive concentrations to zero
+                    else
+                    {
+                        Y_XMLOG(xml, "(#) no z-law to enforce");
+                    }
                     return;
                 }
 
@@ -1109,7 +1113,15 @@ namespace Yttrium
                 for(const LNode *ln=lawz.head;ln;ln=ln->next)
                 {
                     const Conservation::Law &law = **ln;
-                    Y_XMLOG(xml, "check " << law);
+                    Y_XML_SECTION(xml,law.name);
+                    for(const Actor *a = law->head;a;a=a->next)
+                    {
+                        const Species &      sp = a->sp;
+                        const size_t * const id = sp.indx;
+                        const size_t         J  = id[L];
+                        const xreal_t        cc = C[J];
+                        Y_XMLOG(xml, std::setw(15) << real_t(cc) << " = [" << sp << "]");
+                    }
                 }
             }
 
