30bbf64dcc5a1c59fe228cc688110f244180aa4f
Yann Bouret <yann.bouret@unice.fr>
2024-09-05 09:49:59 +0200

    wip

--- src/core/tests/associative/leak.cpp
+++ src/core/tests/associative/leak.cpp
@@ -1,5 +1,7 @@
 
 #include "y/associative/little-endian-addressed.hpp"
+#include "y/object.hpp"
+#include "y/ptr/auto.hpp"
 #include "y/utest/run.hpp"
 
 using namespace Yttrium;
@@ -8,14 +10,28 @@ namespace {
 
     typedef LittleEndianAddressed LEAK;
 
+    class Derived : public Object, public LEAK
+    {
+    public:
+        explicit Derived()
+        {
+        }
+
+        virtual ~Derived() noexcept
+        {
+        }
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(Derived);
+    };
+
+
 }
 
 Y_UTEST(associative_leak)
 {
-    {
-        LEAK  obj;
-        LEAK &ref = obj.as<LEAK>();
-        Y_CHECK(&ref == &obj);
-    }
+    
+    AutoPtr<LEAK> ptr = new Derived();
+    std::cerr << "ptr.leak@" << ptr->leak << std::endl;
 }
 Y_UDONE()
--- src/core/y/associative/little-endian-addressed.hpp
+++ src/core/y/associative/little-endian-addressed.hpp
@@ -5,28 +5,37 @@
 #define Y_Associative_LE_Addressed_Included 1
 
 #include "y/associative/little-endian-address.hpp"
-#include "y/type/conversion.hpp"
-#include "y/check/static.hpp"
 
 namespace Yttrium
 {
 
+    //__________________________________________________________________________
+    //
+    //
+    //
+    //! Base class to use unique key based on object address
+    //
+    //
+    //__________________________________________________________________________
     class LittleEndianAddressed
     {
     public:
-        explicit LittleEndianAddressed() noexcept;
-        virtual ~LittleEndianAddressed() noexcept;
+        //______________________________________________________________________
+        //
+        //
+        // C++
+        //
+        //______________________________________________________________________
+        explicit LittleEndianAddressed() noexcept; //!< setup with *this
+        virtual ~LittleEndianAddressed() noexcept; //!< cleanup
 
-        const LittleEndianAddress leak;
-
-        template <typename T> inline
-        T & as() noexcept
-        {
-            static const bool Inherited = Y_Is_SuperSubClass(LittleEndianAddressed,T);
-            Y_STATIC_CHECK(Inherited,BadCast);
-            return *static_cast<T*>( leak.fetch() );
-        }
-
+        //______________________________________________________________________
+        //
+        //
+        // Members
+        //
+        //______________________________________________________________________
+        const LittleEndianAddress leak; //!< unique key based on address
 
     private:
         Y_DISABLE_COPY_AND_ASSIGN(LittleEndianAddressed);
