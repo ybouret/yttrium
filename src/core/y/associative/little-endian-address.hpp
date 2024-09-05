22f9c42f6ab15412ca794b215faee944725bd7eb
Yann Bouret <yann.bouret@unice.fr>
2024-09-05 09:02:11 +0200

    wip

--- src/core/y/associative/little-endian-address.hpp
+++ src/core/y/associative/little-endian-address.hpp
@@ -13,19 +13,32 @@ namespace Yttrium
     //__________________________________________________________________________
     //
     //
+    //
     //! Encode an address to a Big Endian R/O buffer
+    //
     //
     //__________________________________________________________________________
     class LittleEndianAddress : public LittleEndianKey
     {
     public:
-
+        //______________________________________________________________________
+        //
+        //
+        // C++
+        //
+        //______________________________________________________________________
         template <typename T> inline
-        explicit LittleEndianAddress(const T &args)        noexcept : LittleEndianKey( &args )  {}  //!< setup
-        explicit LittleEndianAddress(const CopyOf_ &, const void *addr) noexcept;                   //!< setup
-        virtual ~LittleEndianAddress()                     noexcept;                                //!< cleanup
-        LittleEndianAddress(const LittleEndianAddress &)   noexcept;                                //!< copy
+        LittleEndianAddress(const T &args)        noexcept : LittleEndianKey( &args )  {}  //!< setup
+        LittleEndianAddress(const CopyOf_ &, const void *addr) noexcept;                   //!< setup
+        virtual ~LittleEndianAddress()                     noexcept;                       //!< cleanup
+        LittleEndianAddress(const LittleEndianAddress &)   noexcept;                       //!< copy
 
+        //______________________________________________________________________
+        //
+        //
+        // Methods
+        //
+        //______________________________________________________________________
         void *fetch() const noexcept; //!< retrieve stored address
 
 
