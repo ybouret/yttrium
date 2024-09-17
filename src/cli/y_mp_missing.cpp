bd268793edab64923ce22569eeb669f595a53333
ybouret <yann.bouret@gmail.com>
2024-09-17 08:58:56 +0200

    wip

--- src/cli/y_mp_missing.cpp
+++ src/cli/y_mp_missing.cpp
@@ -1,10 +1,33 @@
 #include "y/stream/proc/input.hpp"
 #include "y/program.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/string/tokenizer.hpp"
 
 using namespace Yttrium;
 
 Y_Program()
 {
+    if(argc<=1)
+    {
+        std::cerr << "usage: " << program << " portname" << std::endl;
+        return 0;
+    }
+
+    const String   port = argv[1];
+    Vector<String> fields(32,AsCapacity);;
+    {
+        const String cmd = "port deps " + port;
+        ProcInput    fp(cmd);
+        String line;
+        while( fp.gets(line) )
+        {
+            fields.free();
+            if(line.size()<=0) continue;
+            //Tokenizer::AppendTo(fields, line, ':');
+            
+        }
+    }
+
 
 }
 Y_End()
