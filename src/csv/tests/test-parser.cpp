6b9e706131a2b7f7f5e0b540fbc9c4511018a8c7
Yann Bouret <yann.bouret@unice.fr>
2024-10-16 09:37:51 +0200

    wip

--- src/csv/tests/test-parser.cpp
+++ src/csv/tests/test-parser.cpp
@@ -1,13 +1,89 @@
 #include "y/csv/parser.hpp"
 #include "y/utest/run.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/memory/allocator/dyadic.hpp"
+
+namespace Yttrium
+{
+
+    namespace CSV
+    {
+        class Field
+        {
+        public:
+            enum Kind
+            {
+                Generic, //!< whatever
+                DQMarks, //!< double quotation marks
+                SQMarks  //!< single quotation marks
+            };
+            Field();
+            ~Field() noexcept;
+            Field(const Field &);
+
+            template <typename DATA> inline
+            Field(const DATA &d, const Kind k) : text(d), kind(k) {}
+
+
+            String text;
+            Kind   kind;
+        private:
+            Y_DISABLE_ASSIGN(Field);
+        };
+
+        Field:: Field() : text(), kind(Generic)
+        {
+        }
+
+        Field:: ~Field() noexcept
+        {
+        }
+
+        Field:: Field(const Field &other) : text(other.text), kind(other.kind) {}
+
+
+        typedef Vector<Field> Fields;
+
+        class Line :  public Fields
+        {
+        public:
+            explicit Line() noexcept;
+            explicit Line(const size_t capa);
+            virtual ~Line() noexcept;
+            Line(const Line &);
+
+            virtual const char * callSign() const noexcept;
+
+        private:
+            Y_DISABLE_ASSIGN(Line);
+        };
+
+
+        Line:: ~Line() noexcept {}
+        Line::  Line() noexcept : Fields() {}
+        Line::  Line(const size_t capa) : Fields(capa,AsCapacity) {}
+        Line::  Line(const Line &other) :
+        Identifiable(), Collection(), Fields(other) {}
+
+        const char * Line:: callSign() const noexcept
+        {
+            return "CSV::Line";
+        }
+
+
+    }
+}
 
 using namespace Yttrium;
+
+
 
 Y_UTEST(parser)
 {
     CSV::Parser parser;
 
-    
+    Y_SIZEOF(CSV::Field);
+    Y_SIZEOF(CSV::Line);
 
     if(argc>1)
     {
