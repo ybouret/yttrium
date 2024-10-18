59fc95f5ffedc368747a6a79d8233b2471a991bc
ybouret <yann.bouret@gmail.com>
2024-10-18 11:39:57 +0200

    wip

---
+++ src/lingo/y/lingo/char.cpp
@@ -0,0 +1,45 @@
+
+#include "y/lingo/char.hpp"
+#include "y/memory/exclusive/impl.hpp"
+
+Y_Studio(Yttrium::Lingo::Char,AtExit::MaximumLongevity-7,128);
+
+namespace Yttrium
+{
+    namespace Lingo
+    {
+
+        Y_EXCLUSIVE_IMPL(Char);
+
+        Char:: ~Char() noexcept {}
+
+        Char:: Char(const Context &ctx, const uint8_t chr) noexcept :
+        Context(ctx),
+        next(0),
+        prev(0),
+        data(chr)
+        {
+        }
+
+        Char:: Char(const Char &other) noexcept :
+        Context(other),
+        next(0),
+        prev(0),
+        data(other.data)
+        {
+        }
+
+        uint8_t & Char:: operator*() noexcept
+        {
+            return data;
+        }
+
+        const uint8_t & Char:: operator*() const noexcept
+        {
+            return data;
+        }
+
+
+    }
+
+}
---
+++ src/lingo/y/lingo/char.hpp
@@ -0,0 +1,75 @@
+//! \file
+
+#ifndef Y_Lingo_Char_Included
+#define Y_Lingo_Char_Included 1
+
+#include "y/lingo/context.hpp"
+#include "y/memory/exclusive/decl.hpp"
+#include "y/data/list/cxx.hpp"
+
+namespace Yttrium
+{
+    namespace Lingo
+    {
+        //______________________________________________________________________
+        //
+        //
+        //
+        //! Char with full Context
+        //
+        //
+        //______________________________________________________________________
+        class Char : public Context
+        {
+        public:
+            //__________________________________________________________________
+            //
+            //
+            // Definitions
+            //
+            //__________________________________________________________________
+            typedef CxxListOf<Char> List; //!< alias
+
+            //__________________________________________________________________
+            //
+            //
+            // C++
+            //
+            //__________________________________________________________________
+            explicit Char(const Context &, const uint8_t) noexcept; //!< setup
+            virtual ~Char()                               noexcept; //!< cleanup
+            Char(const Char &)                            noexcept; //!< copy
+
+            //__________________________________________________________________
+            //
+            //
+            // Methods
+            //
+            //__________________________________________________________________
+            uint8_t &       operator*()       noexcept; //!< r/w access
+            const uint8_t & operator*() const noexcept; //!< r/o access
+
+            //__________________________________________________________________
+            //
+            //
+            //  Members
+            //
+            //__________________________________________________________________
+            Char *  next; //!< for token
+            Char *  prev; //!< for token
+
+            //__________________________________________________________________
+            //
+            //! managed by a Memory::Studio
+            //__________________________________________________________________
+            Y_EXCLUSIVE_DECL();
+
+        private:
+            uint8_t data;
+            Y_DISABLE_ASSIGN(Char);
+        };
+    }
+}
+
+#endif
+
