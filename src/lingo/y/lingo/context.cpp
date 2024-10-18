ad0b8618c62171c3947d73ccd43e3cc5b301e29c
ybouret <yann.bouret@gmail.com>
2024-10-18 11:02:55 +0200

    wip

---
+++ src/lingo/y/lingo/context.cpp
@@ -0,0 +1,21 @@
+
+#include "y/lingo/context.hpp"
+#include "y/exception.hpp"
+
+namespace Yttrium
+{
+    namespace Lingo
+    {
+        Context:: ~Context() noexcept { Coerce(column)=0; Coerce(line)=0; }
+
+
+        Exception & Context:: stamp(Exception &excp) const noexcept
+        {
+            return excp.pre("%s:%lu:%lu: ", caption->c_str(), line, column);
+        }
+
+        void Context:: newChar() noexcept { ++Coerce(column); }
+        void Context:: newLine() noexcept { ++Coerce(line); Coerce(column)=1; }
+    }
+
+}
---
+++ src/lingo/y/lingo/context.hpp
@@ -0,0 +1,52 @@
+//! \file
+
+#ifndef Y_Lingo_Context_Included
+#define Y_Lingo_Context_Included 1
+
+#include "y/lingo/caption.hpp"
+
+namespace Yttrium
+{
+    class Exception;
+    
+    namespace Lingo
+    {
+
+        class Context
+        {
+        public:
+
+            // C++
+
+            //! generic constructor
+            template <typename CAPTION>
+            inline Context(const CAPTION &_, const AsCaption_ &) :
+            caption(_), line(1), column(1)
+            {
+            }
+
+            explicit Context(const Caption &) noexcept; //!< copy
+            virtual ~Context()                noexcept; //!< cleanup
+
+            // Methods
+
+            //! stamp exception with 'caption:line:colum: '
+            Exception & stamp(Exception &) const noexcept;
+
+            void newChar() noexcept; //!< increase column
+            void newLine() noexcept; //!< incrase  line, column=1
+
+            // Members
+            const Caption       caption;
+            const unsigned long line;
+            const unsigned long column;
+        private:
+            Y_DISABLE_ASSIGN(Context);
+        };
+
+    }
+
+}
+
+#endif
+
