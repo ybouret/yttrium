1d5dad7323eff2e65d2b678c77a306ab8b2a5f73
Yann Bouret <yann.bouret@unice.fr>
2024-10-21 09:30:23 +0200

    wip

--- src/lingo/y/lingo/pattern/logic/or.cpp
+++ src/lingo/y/lingo/pattern/logic/or.cpp
@@ -17,6 +17,36 @@ namespace Yttrium
 
         Pattern * Or:: clone() const { return new Or(*this); }
 
+        bool Or:: strong() const noexcept
+        {
+            return false;
+        }
+
+        bool Or:: univocal() const noexcept
+        {
+            return false;
+        }
+
+        void Or:: query(CharDB &fc) const
+        {
+
+        }
+
+        String Or:: regularExpression() const
+        {
+            String ans;
+
+            return ans;
+        }
+
+        bool Or:: takes(Token &token, Source &source) const
+        {
+            assert(0==token.size);
+
+            return false;
+        }
+
+
 
     }
 
