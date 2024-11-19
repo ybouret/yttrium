47cab1bcc790cad3871051ad3257854a475a3e06
ybouret <yann.bouret@gmail.com>
2024-11-19 14:25:31 +0100

    wip

--- src/core/tests/io/input-data.cpp
+++ src/core/tests/io/input-data.cpp
@@ -5,30 +5,53 @@
 #include "y/ptr/auto.hpp"
 #include "y/stream/gzip/input.hpp"
 #include "y/stream/bzip2/input.hpp"
-
 #include <cstring>
+#include "y/type/proxy.hpp"
 
 using namespace Yttrium;
 
 namespace Yttrium
 {
 
-    class InputStreamProxy : public Object, public Counted, public Identifiable
+    class InputStreamFormat : public Object,  public Identifiable
     {
+    public:
+        typedef CxxListOf<InputStreamFormat> List;
+
     protected:
-        explicit InputStreamProxy() noexcept {}
+        explicit InputStreamFormat() noexcept : next(0), prev(0) {}
 
     public:
-        virtual ~InputStreamProxy() noexcept {}
+        virtual ~InputStreamFormat() noexcept {}
 
         virtual InputStream * open(const String &) const = 0;
 
+        InputStreamFormat *next;
+        InputStreamFormat *prev;
+
     private:
-        Y_DISABLE_COPY_AND_ASSIGN(InputStreamProxy);
+        Y_DISABLE_COPY_AND_ASSIGN(InputStreamFormat);
+    };
+
+    class FallbackInputStream : public InputStreamFormat
+    {
+    public:
+
+        explicit FallbackInputStream() noexcept {}
+        virtual ~FallbackInputStream() noexcept {}
+
+        virtual const char * callSign() const noexcept { return "InputFile"; }
+        virtual InputStream * open(const String &fileName) const
+        {
+            return new InputFile(fileName);
+        }
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(FallbackInputStream);
     };
 
     template <typename ISTREAM, const size_t N>
-    class MagicInputStream : public InputStreamProxy
+    class MagicInputStream : public InputStreamFormat
     {
     public:
         static const char * const CallSign;
@@ -72,26 +95,65 @@ namespace Yttrium
     };
 
     typedef MagicInputStream<GZip::InputFile,2> MagicGZipFile;
-    template<> const uint8_t      MagicGZipFile:: Bytes[] = { 0x1f, 0x8b };
+    template<> const uint8_t      MagicGZipFile:: Bytes[]  = { 0x1f, 0x8b };
     template<> const char * const MagicGZipFile:: CallSign = "GZip::InputStream";
 
     typedef MagicInputStream<Bzip2::InputFile,2> MagicBzip2File;
-    template<> const uint8_t      MagicBzip2File:: Bytes[] = { 'B', 'Z' };
+    template<> const uint8_t      MagicBzip2File:: Bytes[]  = { 'B', 'Z' };
     template<> const char * const MagicBzip2File:: CallSign = "Bzip2::InputStream";
+
+
+    class MagicInputFactory : public Proxy<const InputStreamFormat::List>
+    {
+    public:
+        explicit MagicInputFactory() : crew() {
+
+        }
+
+        virtual ~MagicInputFactory() noexcept   {}
+
+        template <typename MAGIC_FILE> inline
+        void add()
+        {
+            crew.pushHead( new MAGIC_FILE() );
+        }
+
+        InputStream * create(const String &fileName) const noexcept
+        {
+            for(const InputStreamFormat *fmt=crew.head;fmt;fmt=fmt->next)
+            {
+                InputStream * const fp = fmt->open(fileName);
+                if(0!=fp) return fp;
+            }
+
+            return fallback.open(fileName);
+        }
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(MagicInputFactory);
+        InputStreamFormat::List crew;
+        FallbackInputStream     fallback;
+
+        virtual ConstInterface & surrogate() const noexcept { return crew; }
+    };
+
+
 
 
 }
 
 Y_UTEST(io_input_data)
 {
+    MagicInputFactory mif;
+    mif.add<MagicGZipFile>();
+    mif.add<MagicBzip2File>();
+
     if(argc>1)
     {
 
-        MagicGZipFile        mgz;
-        MagicBzip2File       mbz2;
 
-        AutoPtr<InputStream> fp = mgz.open(argv[1]);  if(fp.isValid()) std::cerr << "success!" << std::endl;
-        fp = mbz2.open(argv[1]); if(fp.isValid()) std::cerr << "success!" << std::endl;
+
+
 
     }
 }
