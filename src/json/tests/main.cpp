d699a13a33d5c6a7d3f22ac9af8b1438371cdf04
ybouret <yann.bouret@gmail.com>
2024-11-19 09:00:40 +0100

    wip

--- src/json/tests/main.cpp
+++ src/json/tests/main.cpp
@@ -1,4 +1,3 @@
-#include "y/json/parser.hpp"
 #include "y/json/compiler.hpp"
 #include "y/program.hpp"
 
@@ -6,24 +5,13 @@ using namespace Yttrium;
 
 Y_Program()
 {
-#if 0
-    JSON::Parser &json = JSON::Parser::Instance();
-    std::cerr << "LifeTime: " << json.LifeTime << std::endl;
 
-    json.renderGraphViz();
-    JSON::Value jv;
-    if(argc>1)
-    {
-        json.load(jv,Jive::Module::OpenFile(argv[1]) );
-    }
-#endif
-
-    JSON::Compiler jcmp;
+    JSON::Compiler json;
     JSON::Value    jv;
 
     if(argc>1)
     {
-        jcmp.load(jv, Lingo::Module::OpenFile(argv[1]));
+        json.load(jv, Lingo::Module::OpenFile(argv[1]));
     }
 }
 Y_End()
