d83cdcc1c55c8937d22c4532ac8f7d1de32c2c93
Yann Bouret <yann.bouret@unice.fr>
2024-12-16 15:59:27 +0100

    wip

--- src/chem/y/chemical/plexus/solver/ode.cpp
+++ src/chem/y/chemical/plexus/solver/ode.cpp
@@ -1,75 +1,11 @@
 #include "y/chemical/plexus/solver.hpp"
-#include "y/stream/libc/output.hpp"
-#include "y/mkl/opt/minimize.hpp"
 
-#include "y/stream/libc/output.hpp"
 
 namespace Yttrium
 {
     namespace Chemical
     {
 
-        xReal Solver:: makeStep(XMLog &            xml,
-                               const xReal        f0,
-                               const char * const sid)
-        {
-            assert(0!=sid);
-            Y_XML_SECTION_OPT(xml, "makeStep", "name='" << sid << "'");
-            bool  hasCut = false;
-            xReal factor = one;
-            for(const SNode *sn=mix->species.head;sn;sn=sn->next)
-            {
-                const Species &sp = **sn;
-                const xReal    cc = sp(Cini,SubLevel); assert(cc>=0.0);
-                const xReal    dc = sp(step,SubLevel);
-                if(dc<0.0)
-                {
-                    const xReal scale = cc/-dc;
-                    factor = Min(factor,scale);
-                    hasCut = true;
-                }
-            }
-            Y_XMLOG(xml, "hasCut=" << hasCut);
-            Y_XMLOG(xml, "factor=" << real_t(factor) );
-
-            if(hasCut)
-            {
-                factor *= 0.9;
-
-            }
-
-            for(const SNode *sn=mix->species.head;sn;sn=sn->next)
-            {
-                const Species &sp = **sn;
-                sp(Cend,SubLevel) = sp(Cini,SubLevel) + factor * sp(step,SubLevel);
-                assert(sp(Cend,SubLevel) >= 0.0 );
-            }
-
-            {
-                String     fn = sid; fn += ".dat";
-                OutputFile fp(fn);
-                const size_t np=100;
-                for(size_t i=0;i<=np;++i)
-                {
-                    const double u = double(i)/np;
-                    const double f = double(objectiveFunction(u));
-                    fp("%.15g %.15g\n",u,f);
-                }
-            }
-
-            XTriplet x = { zero, zero, one };
-            XTriplet f = { f0,   f0,   objectiveFunction(Cend,SubLevel) };
-
-            std::cerr << "ff=" << real_t(f.c) << std::endl;
-            Solver &    F     = *this;
-            const xReal x_opt = MKL::Minimize<xReal>::Locate(MKL::Minimizing::Inside, F, x, f);
-            const xReal f_opt = F(x_opt);
-
-            std::cerr << "ff=" << real_t(f_opt) << " @" << real_t(x_opt) << std::endl;
-
-
-            return f_opt;
-        }
 
         void Solver:: buildODE(XMLog &xml, const xReal f0)
         {
@@ -92,7 +28,12 @@ namespace Yttrium
                 for(const Actor *a=cm.prod->head;a;a=a->next) a->sp(Cadd,SubLevel) << (a->xn*pxi);
                 for(const Actor *a=cm.reac->head;a;a=a->next) a->sp(Cadd,SubLevel) << (a->xn*rxi);
             }
-            
+
+            //------------------------------------------------------------------
+            //
+            // compose step
+            //
+            //------------------------------------------------------------------
             step.ld(zero);
             for(const SNode *sn=mix->species.head;sn;sn=sn->next)
             {
