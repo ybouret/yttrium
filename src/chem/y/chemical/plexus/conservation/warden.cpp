7068acacc640b12b0bb6effdf7d58c4ce4a990c5
Yann Bouret <yann.bouret@unice.fr>
2025-01-03 10:07:11 +0100

    wip

--- src/chem/y/chemical/plexus/conservation/warden.cpp
+++ src/chem/y/chemical/plexus/conservation/warden.cpp
@@ -58,33 +58,58 @@ namespace Yttrium
 
 
                 act.transfer(Cini,AuxLevel,C,L);
-                if(xml.verbose) act( xml() << "Cini=", "  [", Cini, "]", xReal::ToString) << std::endl;
+                if(xml.verbose) act( xml() << "Cini=", "\t[", Cini, "]", xReal::ToString) << std::endl;
 
-                Y_XML_COMMENT(xml, "collecting broken laws");
-                for(const LNode *ln=act->head;ln;ln=ln->next)
                 {
-                    const Law & law = **ln;
-                    const xReal xs  = law.excess(xadd,C,L);
-                    if(xml.verbose) print( xml(),  "d_(",*law, ")", Justify::Right) << " = " << xs.str() << std::endl;
-                    if(xs<=0.0) continue;
+                    Y_XML_SECTION(xml,"Inquiry");
+                    for(const LNode *ln=act->head;ln;ln=ln->next)
+                    {
+                        const Law & law = **ln;
+                        const xReal xs  = law.excess(xadd,C,L);
+                        if(xml.verbose) print( xml(),  "d_(",*law, ")", Justify::Right) << " = " << xs.str() << std::endl;
+                        if(xs<=0.0) continue;
 
-                    XWritable &cc = Corr[blist.size+1];
-                    law.project(xadd,cc,Cini,AuxLevel);
-                    //if(xml.verbose) act( xml() << "Corr=","  [", cc, "]", xReal::ToString) << std::endl;
-                    const Broken broken(law,xs,cc);
-                    blist << broken;
+                        XWritable &cc = Corr[blist.size+1];
+                        law.project(xadd,cc,Cini,AuxLevel);
+                        //if(xml.verbose) act( xml() << "Corr=","  [", cc, "]", xReal::ToString) << std::endl;
+                        const Broken broken(law,xs,cc);
+                        blist << broken;
+                    }
                 }
 
                 while(blist.size>0)
                 {
-                    std::cerr << "#broken=" << blist.size << std::endl;
+                    Y_XML_SECTION_OPT(xml,"Relieve","broken=" << blist.size);
+
                     MergeSort::Call(blist,CompareBroken);
+                    Y_XML_COMMENT(xml, "increasing excess");
                     for(const BNode *bn=blist.head;bn;bn=bn->next)
                     {
                         const Broken &broken = **bn;
                         if(xml.verbose)
+                        {
                             print( xml(),  "d_(", *broken.law, ")", Justify::Right) << " = " << broken.bad.str() << std::endl;
+                        }
                     }
+
+                    const Broken    &broken = **(blist.head);
+                    const XReadable &Cnew   = broken.cok;
+                    Y_XML_COMMENT(xml, "using " << broken.law);
+                    if(xml.verbose)
+                    {
+                        act( xml() << broken.law << " => ","  [", Cnew, "]", xReal::ToString) << std::endl;
+                    }
+                    for(const SNode *sn=act->species.head;sn;sn=sn->next)
+                    {
+                        const Species &sp    = **sn;
+                        const xReal    c_old = sp(Cini,AuxLevel);
+                        const xReal    c_new = sp(Cnew,AuxLevel); assert(c_new>=c_old);
+                        sp(Cadd,AuxLevel) << (c_new-c_old);
+                        sp(Cini,AuxLevel) = c_new;
+                    }
+                    std::cerr << "Cadd=" << Cadd << std::endl;
+
+
                     break;
                 }
 
