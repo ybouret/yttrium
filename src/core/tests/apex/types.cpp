b3b0ccbc5f85745fc5e3bd77a027bbf3561fe8fa
ybouret <yann.bouret@gmail.com>
2025-01-07 10:38:33 +0100

    wip

--- src/core/tests/apex/types.cpp
+++ src/core/tests/apex/types.cpp
@@ -2,6 +2,7 @@
 #include "y/utest/run.hpp"
 #include "y/system/rtti.hpp"
 #include "y/calculus/ilog2.hpp"
+#include "y/calculus/base2.hpp"
 
 
 namespace Yttrium
@@ -11,10 +12,16 @@ namespace Yttrium
 
         struct API
         {
-            static const size_t MinSize = 2*sizeof(uint64_t);
-            static const size_t MinLog2 = iLog2<MinSize>::Value;
-            
+            static const size_t MinCapa = 2*sizeof(uint64_t);
+            static const size_t MinLog2 = iLog2<MinCapa>::Value;
 
+            template <typename T> static inline
+            size_t CapacityTo(const size_t capa) noexcept
+            {
+                assert(IsPowerOfTwo(capa));
+                assert(capa>=MinCapa);
+                return capa >> ( iLog2Of<T>::Value );
+            }
 
 
         };
@@ -28,8 +35,13 @@ using namespace Apex;
 Y_UTEST(apex_types)
 {
 
-    Y_USHOW(Apex::API::MinSize);
+    Y_USHOW(Apex::API::MinCapa);
     Y_USHOW(Apex::API::MinLog2);
+
+    Y_USHOW(Apex::API::CapacityTo<uint8_t>(32));
+    Y_USHOW(Apex::API::CapacityTo<uint16_t>(32));
+    Y_USHOW(Apex::API::CapacityTo<uint32_t>(32));
+    Y_USHOW(Apex::API::CapacityTo<uint64_t>(32));
 
 }
 Y_UDONE()
