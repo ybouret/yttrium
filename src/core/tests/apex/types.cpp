3ef396d6b5d0e6ab3c892a7de3976dbf609d8b8b
ybouret <yann.bouret@gmail.com>
2025-01-08 10:51:31 +0100

    wip

--- src/core/tests/apex/types.cpp
+++ src/core/tests/apex/types.cpp
@@ -4,8 +4,11 @@
 #include "y/calculus/align.hpp"
 #include "y/memory/allocator/archon.hpp"
 #include "y/system/exception.hpp"
-#include "y/object.hpp"
-
+#include "y/quantized.hpp"
+#include "y/data/list/cxx.hpp"
+#include "y/data/pool/cxx.hpp"
+#include "y/type/proxy.hpp"
+#include "y/memory/out-of-reach.hpp"
 #include <cstring>
 #include <cerrno>
 
@@ -60,7 +63,8 @@ namespace Yttrium
             inline  View(void * const addr, const size_t capa) noexcept :
             word( static_cast<Word *>(addr ) ),
             count(0),
-            space( capa >> WordShift )
+            space( capa >> WordShift ),
+            priv(0)
             {
             }
 
@@ -69,7 +73,7 @@ namespace Yttrium
             Word * const word;
             size_t       count;
             const size_t space;
-
+            void        *priv;
         private:
             Y_DISABLE_COPY_AND_ASSIGN(View);
         };
@@ -120,11 +124,17 @@ capa(API::StaticBytes),    \
 size(0),                   \
 bits(0),                   \
 mode(StaticMemory),        \
-addr( (void*) wksp )
+addr( (void*) wksp ),      \
+next(0), \
+prev(0)
 
-        class Block : public Object
+
+        class Block : public Quantized
         {
         public:
+            typedef CxxListOf<Block> List;
+            typedef CxxPoolOf<Block> Pool;
+
             explicit Block() noexcept :
             Y_Apex_Block()
             {
@@ -172,12 +182,20 @@ addr( (void*) wksp )
                 bits = 0;
             }
 
+            void zero() noexcept
+            {
+                size = 0;
+                bits = 0;
+                Coerce(plan) = Plan1;
+                memset(addr,0,capa);
+            }
+
 
         private:
             void *           wksp[API::StaticWords+Y_WORDS_FOR(Views)];
             Bulk &           bulk;
-            Views &          view;
         public:
+            Views &          view;
             const Plan       plan;
             const size_t     capa; //!< in bytes, a power of two
             size_t           size; //!< in bytes <= capa
@@ -185,7 +203,10 @@ addr( (void*) wksp )
             const MemoryMode mode;
         private:
             void * const     addr;
-
+        public:
+            Block * next;
+            Block * prev;
+        private:
             Y_DISABLE_COPY_AND_ASSIGN(Block);
             void zclear() noexcept {
                 memset(wksp,0,sizeof(wksp));
@@ -205,9 +226,86 @@ addr( (void*) wksp )
         };
 
 
+        class Blocks : public Proxy<const  Block::Pool>
+        {
+        public:
+            static const size_t One = 1;
+
+            explicit Blocks(const unsigned _shift) noexcept :
+            shift(_shift),
+            bytes(One<<shift),
+            my()
+            {
+                assert(shift>=API::StaticShift);
+                assert(shift<=Base2<size_t>::MaxShift);
+            }
+
+            virtual ~Blocks() noexcept {}
+
+            void store(Block * const block) noexcept
+            {
+                assert(0!=block);
+                assert(block->capa == bytes);
+                my.store(block)->zero();
+            }
+
+            Block *query()
+            {
+                return (my.size>0) ?  my.query() : new Block(bytes);
+            }
 
 
 
+            const unsigned shift;
+            const size_t   bytes;
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Blocks);
+            Y_PROXY_DECL();
+            Block::Pool my;
+        };
+
+        Y_PROXY_IMPL(Blocks,my);
+
+
+        class Factory
+        {
+        public:
+            static const unsigned MaxShift  = Base2<size_t>::MaxShift;
+            static const unsigned MinShift  = API::StaticShift;
+            static const unsigned NumBlocks = MaxShift-MinShift+1;
+            static const unsigned Requested = NumBlocks * sizeof(Blocks);
+            explicit Factory() noexcept : blocks(0), wksp()
+            {
+                std::cerr << "sizeof(wksp)=" << sizeof(wksp) << std::endl;
+                memset(wksp,0,sizeof(wksp));
+                Coerce(blocks) = static_cast<Blocks *>(Memory::OutOfReach::Addr( &wksp[0] ))-MinShift;
+                for(unsigned i=MinShift;i<=MaxShift;++i)
+                {
+                    new (&blocks[i]) Blocks(i);
+                }
+            }
+
+            Block * queryShift(unsigned shift)
+            {
+                if(shift>=MaxShift) throw Specific::Exception("here", "shift overflow");
+                if(shift<MinShift)  shift = MinShift;
+                return blocks[shift].query();
+            }
+
+            virtual ~Factory() noexcept
+            {
+                for(unsigned i=MaxShift;i>=MinShift;--i)
+                    Destruct( &blocks[i] );
+                Coerce(blocks) = 0;
+                memset(wksp,0,sizeof(wksp));
+            }
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Factory);
+            Blocks * const blocks;
+            void   *       wksp[Y_WORDS_GEQ(Requested)];
+        };
 
     }
 }
@@ -230,13 +328,23 @@ Y_UTEST(apex_types)
     Y_USHOW(Memory::Archon::MinBytes);
     Y_USHOW(Memory::Archon::MinShift);
 
+    Y_SIZEOF(Apex::View1);
+    Y_SIZEOF(Apex::View2);
+    Y_SIZEOF(Apex::View4);
+    Y_SIZEOF(Apex::View8);
 
     Y_SIZEOF(Apex::Block);
+    Y_SIZEOF(Apex::Blocks);
 
+    Y_USHOW(Apex::Factory::NumBlocks);
 
     Apex:: Block b0;
     Apex:: Block b1(100);
 
+    Apex:: Factory f;
+
+    Block::List blist;
+    blist.pushTail( f.queryShift(0) );
 
 
 }
