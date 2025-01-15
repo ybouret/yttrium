f12b668636c2f796b8151601e27098edb8d5b9bc
Yann Bouret <yann.bouret@unice.fr>
2025-01-15 11:37:38 +0100

    wip

--- src/core/y/apex/natural.cpp
+++ src/core/y/apex/natural.cpp
@@ -15,30 +15,36 @@ namespace Yttrium
 
         static const unsigned    NaturalShift = iLog2Of<natural_t>::Value;
         static const Plan        NaturalPlan  = Plan(NaturalShift);
-       // typedef Jig<NaturalPlan> NaturalJig;
 
         Natural:: Natural(const natural_t value) :
         Number(),
-        block( _Factory().query(NaturalShift) )
+        block( _Factory().query(NaturalShift) ),
+        mutex( _Factory().query() )
         {
             block->make<NaturalPlan>().word[0] = value;
             block->sync();
         }
 
-        Natural:: ~Natural() noexcept
-        {
+        Natural:: ~Natural() noexcept {
 
         }
 
         Natural:: Natural(const Natural &other) :
         Number(),
-        block( _Factory().duplicate( * other.block) )
+        block( _Factory().duplicate(*other.block) ),
+        mutex( _Factory().query() )
         {
             
         }
 
+        Lockable & Natural:: operator*() const noexcept
+        {
+            return *mutex;
+        }
+
 
         std::ostream & operator<<(std::ostream &os, const Natural &n) {
+            Y_LOCK(*n);
             const Block::Briefly change(Plan1,*n.block);
             os << *n.block;
             return os;
@@ -46,8 +52,8 @@ namespace Yttrium
 
         Natural & Natural:: operator=(const Natural &other)
         {
-            Natural temp(other);
-            block.swp(temp.block);
+            BlockPtr temp( _Factory().duplicate( *other.block) );
+            block.swp(temp);
             return *this;
         }
 
--- src/core/y/apex/natural.hpp
+++ src/core/y/apex/natural.hpp
@@ -6,6 +6,7 @@
 
 #include "y/apex/number.hpp"
 #include "y/apex/block/ptr.hpp"
+#include "y/apex/block/mtx.hpp"
 #include "y/ostream-proto.hpp"
 
 namespace Yttrium
@@ -22,9 +23,12 @@ namespace Yttrium
             Natural(const Natural &);
             Natural & operator=(const Natural &);
             Y_OSTREAM_PROTO(Natural);
-            
+
+            Lockable & operator*() const noexcept;
+
         private:
             mutable BlockPtr block;
+            mutable MutexPtr mutex;
         };
 
     }
