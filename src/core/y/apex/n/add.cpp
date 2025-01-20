9e1469720313a25fbb0ce12cb187ce09782d723e
Yann Bouret <yann.bouret@unice.fr>
2025-01-20 14:23:26 +0100

    wip

--- src/core/y/apex/n/add.cpp
+++ src/core/y/apex/n/add.cpp
@@ -1,7 +1,7 @@
 #include "y/apex/natural.hpp"
 
 #include "y/apex/block/factory.hpp"
-
+#include "y/check/static.hpp"
 
 namespace Yttrium
 {
@@ -9,9 +9,38 @@ namespace Yttrium
     {
 
         template <
-        Plan CPU,
-        Plan USR
+        Plan CORE,
+        Plan PLAN
         > struct JigAdd {
+            typedef typename Jig<CORE>::Word CoreType;
+            typedef          Jig<PLAN>       JigType;
+            typedef typename JigType::Word   WordType;
+
+            static inline
+            Block * Get(const WordType * const lw,
+                        const size_t           ln,
+                        const WordType * const rw,
+                        const size_t           rn) {
+                Y_STATIC_CHECK(sizeof(WordType)<sizeof(CoreType),InvalidPlan);
+                return (ln>=rn) ? Impl(lw,ln,rw,rn) : Impl(rw,rn,lw,ln);
+             }
+
+        private:
+            static inline
+            Block * Impl(const WordType * const bigWord,
+                         const size_t           bigSize,
+                         const WordType * const litWord,
+                         const size_t           litSize)
+            {
+                static Factory &factory = Factory::Instance();
+                const size_t  resSize   = bigSize+1;
+                Block * const block     = factory.queryBytes( resSize * sizeof(WordType) );
+                {
+                    JigType &jig = block->make<PLAN>();
+
+                }
+                return block;
+            }
 
         };
 
