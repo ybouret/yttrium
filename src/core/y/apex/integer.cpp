69069cc7650503bc4fc5f6ef761db311ba7752c0
Yann Bouret <yann.bouret@unice.fr>
2025-01-29 09:08:59 +0100

    wip

--- src/core/y/apex/integer.cpp
+++ src/core/y/apex/integer.cpp
@@ -6,6 +6,30 @@ namespace Yttrium
 
     namespace Apex
     {
+        static inline
+        natural_t z2n(const SignType  s,
+                      const integer_t z) noexcept
+        {
+            switch(s)
+            {
+                case Negative: return -z;
+                case Positive: return  z;
+                case __Zero__: break;
+            }
+            assert(0==z);
+            return 0;
+        }
+
+        Integer:: Integer(const integer_t z) :
+        s( Sign::Of(z) ),
+        n( z2n(s,z) )
+        {
+        }
+
+        Integer:: ~Integer() noexcept
+        {
+        }
+        
 
     }
 
