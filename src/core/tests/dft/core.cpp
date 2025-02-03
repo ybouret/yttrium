fc85ab2407e4f4d1ad51cc7f71209371672b61be
Yann Bouret <yann.bouret@unice.fr>
2025-02-03 09:17:57 +0100

    wip

--- src/core/tests/dft/core.cpp
+++ src/core/tests/dft/core.cpp
@@ -84,26 +84,25 @@ namespace Yttrium
          - data is a complex array of length nn or, equivalently, a real array of length 2*nn
          - nn MUSTbe an integer power of 2
          */
-        template <typename T>
+        template <typename T> static inline
         void Transform(T            data[],
                        const size_t size,
                        const int    isign)
         {
             typedef typename DFT_Real<T>::Type long_T;
-            size_t n,m,j,istep,i;
+            size_t istep;
             long_T wtemp,wr,wpr,wpi,wi,theta;
             T tempr,tempi;
 
-            n = size << 1;
-            j = 1;
-            for (i=1;i<n;i+=2)
+            const size_t n = size << 1;
+            for(size_t i=1,j=1;i<n;i+=2)
             {
-                if (j > i)
+                if(j>i)
                 {
                     Swap(data[j],data[i]);
                     Swap(data[j+1],data[i+1]);
                 }
-                m=size;
+                size_t m=size;
                 while (m >= 2 && j > m)
                 {
                     j -= m;
@@ -122,16 +121,16 @@ namespace Yttrium
                 wpi   = sin(theta);
                 wr    = 1.0;
                 wi    = 0.0;
-                for (m=1;m<mmax;m+=2)
+                for(size_t m=1;m<mmax;m+=2)
                 {
-                    for (i=m;i<=n;i+=istep)
+                    for(size_t i=m;i<=n;i+=istep)
                     {
-                        j=i+mmax;
+                        const size_t j=i+mmax;
                         tempr=wr*data[j]-wi*data[j+1];
                         tempi=wr*data[j+1]+wi*data[j];
-                        data[j]=data[i]-tempr;
-                        data[j+1]=data[i+1]-tempi;
-                        data[i] += tempr;
+                        data[j]    = data[i]-tempr;
+                        data[j+1]  = data[i+1]-tempi;
+                        data[i]   += tempr;
                         data[i+1] += tempi;
                     }
                     wr=(wtemp=wr)*wpr-wi*wpi+wr;
@@ -156,9 +155,8 @@ Y_UTEST(dft_core)
         const size_t   n = 1<<p;
         Vector<float> data(n*2,0);
         for(size_t i=1;i<=data.size();++i)
-        {
             data[i] = i;
-        }
+
         std::cerr << data << std::endl;
         DFT::Transform_(data()-1,n,1);
         //std::cerr << data << std::endl;
@@ -170,6 +168,26 @@ Y_UTEST(dft_core)
         std::cerr << data << std::endl;
         std::cerr << std::endl;
     }
+
+    for(unsigned p=0;p<6;++p)
+    {
+        const size_t   n = 1<<p;
+        Vector<float> data(n*2,0);
+        for(size_t i=1;i<=data.size();++i)
+            data[i] = i;
+
+        std::cerr << data << std::endl;
+        DFT::Transform(data()-1,n,1);
+        //std::cerr << data << std::endl;
+        DFT::Transform(data()-1,n,-1);
+        for(size_t i=1;i<=data.size();++i)
+        {
+            data[i] = floor( data[i]/n + 0.5);
+        }
+        std::cerr << data << std::endl;
+        std::cerr << std::endl;
+    }
+
 }
 Y_UDONE()
 
