afa2a51fc0f53cb5d737c85658090250271a6369
ybouret <yann.bouret@gmail.com>
2025-02-04 14:28:10 +0100

    wip

--- src/core/y/dft/dft.hpp
+++ src/core/y/dft/dft.hpp
@@ -335,17 +335,20 @@ namespace Yttrium
         }
 
         template <typename T>
-        static void RealTransform(T data[], const size_t n, const int isign)
+        static void RealTransform(T            data[],
+                                  const size_t n,
+                                  const int    isign)
         {
             typedef typename DFT_Real<T>::Type long_T;
             size_t i,i1,i2,i3,i4,np3;
             T c1=0.5,c2,h1r,h1i,h2r,h2i;
             long_T wr,wi,wpr,wpi,wtemp,theta;
 
-            theta=3.141592653589793/(long_T) (n>>1);
+            const size_t nh = n>>1;
+            theta=3.141592653589793/(long_T) (nh);
             if (isign == 1) {
                 c2 = -0.5;
-                Forward(data,n>>1);
+                Forward(data,nh);
             }
             else
             {
@@ -353,16 +356,18 @@ namespace Yttrium
                 theta = -theta;
             }
 
-            wtemp= sin(0.5*theta);
-            wpr  = -2.0*wtemp*wtemp;
-            wpi  = sin(theta);
-            wr=1.0+wpr;
-            wi=wpi;
+            const size_t nq = n>>2;
+            wtemp = sin(0.5*theta);
+            wpr   = -2.0*wtemp*wtemp;
+            wpi   = sin(theta);
+            wr    = 1.0+wpr;
+            wi    = wpi;
             np3=n+3;
-            for (i=2;i<=(n>>2);i++)
+            for(i=2;i<=nq;++i)
             {
                 i4=1+(i3=np3-(i2=1+(i1=i+i-1)));
-                h1r=c1*(data[i1]+data[i3]); h1i=c1*(data[i2]-data[i4]);
+                h1r=c1*(data[i1]+data[i3]);
+                h1i=c1*(data[i2]-data[i4]);
                 h2r = -c2*(data[i2]+data[i4]);
                 h2i=c2*(data[i1]-data[i3]);
                 data[i1] = h1r+wr*h2r-wi*h2i;
@@ -379,7 +384,7 @@ namespace Yttrium
             } else {
                 data[1]=c1*((h1r=data[1])+data[2]);
                 data[2]=c1*(h1r-data[2]);
-                Reverse(data,n>>1);
+                Reverse(data,nh);
             }
         }
 
