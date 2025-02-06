7a824f9f2756001bf8c04e31b18516804cec051f
ybouret <yann.bouret@gmail.com>
2025-02-06 18:57:31 +0100

    wip

--- src/core/y/apex/rational.hpp
+++ src/core/y/apex/rational.hpp
@@ -11,6 +11,26 @@ namespace Yttrium
     namespace Apex
     {
 
+        //! helper to implement comparisons
+#define Y_Apex_Rational_Op(OP,LHS,RHS,MATCHES,RESULT)             \
+/**/ inline friend                                                \
+/**/ bool  operator OP (const LHS lhs, const RHS rhs)   {         \
+/**/   return RESULT MATCHES Rational::Compare(lhs,rhs);          \
+/**/ }
+
+        //! helper to implement comparisons
+#define Y_Apex_Rational_Ops(OP,MATCHES,RESULT)               \
+Y_Apex_Rational_Op(OP, Rational &, Rational &, MATCHES, RESULT) \
+Y_Apex_Rational_Op(OP, Rational &, integer_t , MATCHES, RESULT) \
+Y_Apex_Rational_Op(OP, integer_t , Rational &, MATCHES, RESULT) \
+Y_Apex_Rational_Op(OP, Rational &, Integer & , MATCHES, RESULT) \
+Y_Apex_Rational_Op(OP, Integer  &, Rational &, MATCHES, RESULT)
+
+#if 0
+Y_Apex_Rational_Op(OP, Rational &, Natural & , MATCHES, RESULT) \
+Y_Apex_Rational_Op(OP, Natural  &, Rational &, MATCHES, RESULT)
+#endif
+
         //______________________________________________________________________
         //
         //
@@ -94,6 +114,13 @@ namespace Yttrium
             static SignType Compare(const Integer  &, const Rational &); //!< comparison
             static SignType Compare(const Rational &, const integer_t ); //!< comparison
             static SignType Compare(const integer_t,  const Rational &); //!< comparison
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+            Y_Apex_Rational_Ops(<, ==,Negative)
+            Y_Apex_Rational_Ops(>, ==,Positive)
+            Y_Apex_Rational_Ops(<=,!=,Positive)
+            Y_Apex_Rational_Ops(>=,!=,Negative)
+#endif
 
             //__________________________________________________________________
             //
