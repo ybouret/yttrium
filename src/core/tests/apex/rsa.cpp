59a8ee9dc6e36924c59345afd6bcbed21a85becf
Yann Bouret <yann.bouret@unice.fr>
2025-02-10 14:50:00 +0100

    wip

--- src/core/tests/apex/rsa.cpp
+++ src/core/tests/apex/rsa.cpp
@@ -20,12 +20,12 @@ Y_UTEST(apex_rsa)
     const Natural      e   = 7;
     const Natural      d   = Modular::Inv(e,lam);
 
-    std::cerr << "p   = " << p << std::endl;
-    std::cerr << "q   = " << q << std::endl;
-    std::cerr << "n   = " << n << std::endl;
-    std::cerr << "lam = " << lam << std::endl;
-    std::cerr << "e   = " << e << std::endl;
-    std::cerr << "d   = " << d << std::endl;
+    std::cerr << "p   = " << std::setw(20) << p << std::endl;
+    std::cerr << "q   = " << std::setw(20) << q << std::endl;
+    std::cerr << "n   = " << std::setw(20) << n << std::endl;
+    std::cerr << "lam = " << std::setw(20) << lam << std::endl;
+    std::cerr << "e   = " << std::setw(20) << e << std::endl;
+    std::cerr << "d   = " << std::setw(20) << d << std::endl;
 
     std::cerr << '[';
     for(Natural m=0;m<n;++m)
