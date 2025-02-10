c635eea6952726cf83739437e79665178fbabf11
Yann Bouret <yann.bouret@unice.fr>
2025-02-10 09:30:07 +0100

    wip

--- src/core/y/apex/q/mul.cpp
+++ src/core/y/apex/q/mul.cpp
@@ -7,7 +7,25 @@ namespace Yttrium
 
         Rational Rational::Mul(const Rational &a, const Rational &b)
         {
-            
+            switch( Sign::MakePair( a.numer.s, b.numer.s) )
+            {
+                case ZN_Signs:
+                case ZZ_Signs:
+                case ZP_Signs:
+                case NZ_Signs:
+                case PZ_Signs:
+                    return Rational(0);
+
+                case NN_Signs:
+                case PP_Signs:
+                case NP_Signs:
+                case PN_Signs:
+                    break;
+            }
+            const Integer num = a.numer * b.numer; assert(0!=num);
+            const Natural den = a.denom * b.denom;
+            return Rational(num,den);
+
         }
 
     }
