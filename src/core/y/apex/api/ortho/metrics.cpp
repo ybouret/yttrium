9741d5870b64685e3405c250cca5470b9d7ea948
ybouret <yann.bouret@gmail.com>
2025-02-11 16:45:14 +0100

    wip

---
+++ src/core/y/apex/api/ortho/metrics.cpp
@@ -0,0 +1,59 @@
+
+#include "y/apex/api/ortho/metrics.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+        namespace Ortho
+        {
+
+            const char * Metrics:: QualityText(const Quality quality) noexcept
+            {
+                switch(quality)
+                {
+                    case Generating: return "Generating";
+                    case Hyperplane: return "Hyperplane";
+                    default: break;
+                }
+                assert(Fragmental==quality);
+                return "Fragmental";
+            }
+
+            Metrics:: Metrics(const size_t n) noexcept :
+            dimensions(n),
+            concluding(dimensions-1)
+            {
+                assert(dimensions>0);
+            }
+
+            Metrics:: ~Metrics() noexcept
+            {
+            }
+
+            Metrics:: Metrics(const Metrics &_) noexcept :
+            dimensions(_.dimensions),
+            concluding(_.concluding)
+            {
+            }
+
+            Quality Metrics:: getQuality(const size_t n) const noexcept
+            {
+                assert(n>0);
+                assert(n<=dimensions);
+                if(n>=dimensions)
+                    return Generating;
+
+
+                if(n>=concluding)
+                    return Hyperplane;
+
+                return Fragmental;
+
+            }
+
+        }
+
+    }
+}
+
---
+++ src/core/y/apex/api/ortho/metrics.hpp
@@ -0,0 +1,72 @@
+//! \file
+
+
+#ifndef Y_Apex_Ortho_Metrics_Included
+#define Y_Apex_Ortho_Metrics_Included 1
+
+#include "y/config/starting.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+        namespace Ortho
+        {
+            //__________________________________________________________________
+            //
+            //! quality of a family w.r.t space dimensions
+            //__________________________________________________________________
+            enum Quality
+            {
+                Fragmental, // size < dimensions-1
+                Hyperplane, // size == dimensions-1
+                Generating  // size == dimensions
+            };
+
+            //__________________________________________________________________
+            //
+            //
+            //
+            //! Metrics to keep track of working dimensions
+            //
+            //
+            //__________________________________________________________________
+            class Metrics
+            {
+            public:
+                //______________________________________________________________
+                //
+                //
+                // C++
+                //
+                //______________________________________________________________
+                explicit Metrics(const size_t) noexcept;         //!< setup, dims>0
+                Metrics(const Metrics &)       noexcept;         //!< copy
+                virtual ~Metrics() noexcept;                     //!< cleanup
+
+                //______________________________________________________________
+                //
+                //
+                // Methods
+                //
+                //______________________________________________________________
+                Quality             getQuality(const size_t)     const noexcept; //!< compute quality
+                static const char * QualityText(const Quality quality) noexcept; //!< human readable quality
+                //______________________________________________________________
+                //
+                //
+                // Members
+                //
+                //______________________________________________________________
+                const size_t dimensions;                         //!< dimensions>0 of working space
+                const size_t concluding;                         //!< dimensions-1 = hyperplane dimensions
+
+            private:
+                Y_DISABLE_ASSIGN(Metrics);
+            };
+        }
+    }
+}
+
+#endif
+
