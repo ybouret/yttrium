bbe3c59e6f12c9233f555bbf65b1030590a44882
ybouret <yann.bouret@gmail.com>
2025-02-11 16:55:28 +0100

    wip

---
+++ src/core/y/apex/api/ortho/vector.hpp
@@ -0,0 +1,22 @@
+//! \file
+
+#ifndef Y_Apex_Ortho_Vector_Included
+#define Y_Apex_Ortho_Vector_Included 1
+
+#include "y/apex/api/ortho/metrics.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+        namespace Ortho
+        {
+
+        }
+
+    }
+
+}
+
+#endif
+
