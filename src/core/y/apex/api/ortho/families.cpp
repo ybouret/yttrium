44814cdc044f364e90534fea9c98257ade4aac63
Yann Bouret <yann.bouret@unice.fr>
2025-02-12 14:56:41 +0100

    wip

---
+++ src/core/y/apex/api/ortho/families.cpp
@@ -0,0 +1,17 @@
+
+#include "y/apex/api/ortho/families.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+
+        namespace Ortho
+        {
+
+        }
+
+    }
+
+}
+
---
+++ src/core/y/apex/api/ortho/families.hpp
@@ -0,0 +1,32 @@
+
+//! \file
+
+#ifndef Y_Apex_Ortho_Families_Included
+#define Y_Apex_Ortho_Families_Included 1
+
+#include "y/apex/api/ortho/family.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+
+        namespace Ortho
+        {
+
+            class Families  
+            {
+            public:
+
+            private:
+
+            };
+
+        }
+
+    }
+
+}
+
+#endif
+
