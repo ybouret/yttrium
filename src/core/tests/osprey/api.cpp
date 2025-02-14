7dc0cc62786d7d83e6fd17c05ca055a28f3d1ec5
ybouret <yann.bouret@gmail.com>
2025-02-14 09:42:51 +0100

    wip

--- src/core/tests/osprey/api.cpp
+++ src/core/tests/osprey/api.cpp
@@ -5,7 +5,8 @@
 #include "y/data/small/heavy/list/coop.hpp"
 #include "y/data/small/light/list/coop.hpp"
 
-
+#include "y/random/shuffle.hpp"
+#include "y/random/park-miller.hpp"
 
 namespace Yttrium
 {
@@ -22,6 +23,7 @@ namespace Yttrium
         {
         public:
             explicit ISet(const IndexBank &ibank) noexcept :
+            Proxy<const IndexList>(),
             my(ibank)
             {
             }
@@ -34,6 +36,11 @@ namespace Yttrium
                 for(size_t i=1;i<=dims;++i) my << i;
                 for(size_t i=nrot;i>0;--i)  my.rotl();
             }
+
+            ISet(const ISet &_) :
+            Proxy<const IndexList>(),
+            my(_.my)
+            {}
 
             bool search(const size_t i) const noexcept
             {
@@ -50,6 +57,21 @@ namespace Yttrium
                 my << i;      return true;
             }
 
+            void shuffle(Random::Bits &ran) noexcept
+            {
+                Random::Shuffle::List(my,ran);
+            }
+
+            static bool AreEqual(const ISet &lhs, const ISet &rhs) noexcept
+            {
+                if(lhs->size!=rhs->size) return false;
+                for(const IndexNode *node=rhs->head;node;node=node->next)
+                {
+                    if( !lhs.search(**node) ) return false;
+                }
+                return true;
+            }
+
             virtual ~ISet() noexcept
             {
             }
@@ -63,6 +85,43 @@ namespace Yttrium
         };
 
         Y_PROXY_IMPL(ISet,my)
+
+        typedef Apex::Ortho::Metrics QMetrics;
+
+        class Tribe : public Object
+        {
+        public:
+            template <typename MATRIX>
+            explicit Tribe(const MATRIX    &data,
+                           const size_t     rank,
+                           const IndexBank &bank) :
+            content(bank,data.rows,rank),
+            residue(bank),
+            next(0),
+            prev(0)
+            {
+            }
+
+            Tribe(const Tribe &_) :
+            content(_.content),
+            residue(_.residue),
+            next(0),
+            prev(0)
+            {
+            }
+
+
+
+
+            const ISet content;
+            const ISet residue;
+
+            Tribe *next;
+            Tribe *prev;
+        private:
+            Y_DISABLE_ASSIGN(Tribe);
+        };
+
     }
 }
 
@@ -70,7 +129,8 @@ using namespace Yttrium;
 
 Y_UTEST(osprey)
 {
-    Osprey::IndexBank ibank;
+    Random::ParkMiller ran;
+    Osprey::IndexBank  ibank;
 
     { Osprey::ISet _(ibank); }
 
@@ -79,7 +139,9 @@ Y_UTEST(osprey)
         for(size_t nrot=0;nrot<dims;++nrot)
         {
             Osprey::ISet iset(ibank,dims,nrot);
-            std::cerr << iset << std::endl;
+            Osprey::ISet copy(iset);
+            Osprey::ISet rset(iset); rset.shuffle(ran);
+            std::cerr << iset << " / " << copy << " / " << rset << std::endl;
         }
     }
 
