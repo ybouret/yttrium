3e797426b1d419c1ff3ab64c59fa95e6ad020941
Yann Bouret <yann.bouret@unice.fr>
2025-02-27 14:48:55 +0100

    wip

--- src/core/y/apex/api/ortho/coven/tribes.cpp
+++ src/core/y/apex/api/ortho/coven/tribes.cpp
@@ -25,6 +25,186 @@ namespace Yttrium
                     os << '}';
                     return os;
                 }
+
+
+                Natural Tribes:: MaxCount(const size_t n)
+                {
+                    Natural res = 0;
+                    for(size_t k=1;k<=n;++k) res += Natural::Arrange(n,k);
+                    return res;
+                }
+
+                void Tribes:: Display(const Vector &v)
+                {
+                    std::cerr << "\t(+) " << v << std::endl;
+                }
+
+                Tribes:: ~Tribes() noexcept
+                {
+                    while(db.size) vc->store( Coerce(db).popTail() );
+                }
+
+
+                const Vector * Tribes:: tryInsertNew(const Vector &vec)
+                {
+                    for(const Vector *v=db.head;v;v=v->next)
+                    {
+                        switch( Vector::Compare(*v,vec) )
+                        {
+                            case __Zero__: return 0;
+                            case Negative:
+                            case Positive:
+                                continue;
+                        }
+                    }
+                    return Coerce(db).pushTail( vc->query(vec) );
+                }
+
+                SignType Tribes:: CompareVectors(const Vector * const lhs,
+                                                 const Vector * const rhs) noexcept
+                {
+                    return Vector::Compare(*lhs,*rhs);
+                }
+
+                Digest Tribes:: signature(Hashing::Function &H) const
+                {
+                    H.set();
+                    {
+                        HashingStream fp(H);
+                        for(const Vector *v=db.head;v;v=v->next) v->serialize(fp);
+                    }
+                    return Hashing::MD::Of(H);
+                }
+
+                void Tribes:: makeReplacement(XMLog &xml)
+                {
+                    assert( isSortedAccordingTo(Tribe::Compare) );
+                    size_t replacement = 0;
+                    for(Tribe *curr=head;curr;curr=curr->next)
+                    {
+                        for(Tribe *prev=curr->prev;prev;prev=prev->prev)
+                        {
+                            if( (curr->qfamily!=prev->qfamily) && (curr->content==prev->content) )
+                            {
+                                curr->adoptedBy(*prev);
+                                ++replacement;
+                            }
+                        }
+                    }
+
+
+                    Y_XML_COMMENT(xml,"#replacement = " << replacement);
+                    if(replacement<=0) { assert( isSortedAccordingTo(Tribe::Compare) ); return; }
+                    MergeSort::Call(*this,Tribe::Compare);
+                }
+
+
+                void Tribes:: assembleLast(Callback &proc)
+                {
+                    size_t & count = Coerce(collected);
+                    count = 0;
+                    for(const Tribe *tribe=head;tribe;tribe=tribe->next)
+                    {
+                        const Vector *pVector = tribe->lastVec;        if(0==pVector) continue;
+                        const Vector *pSingle = tryInsertNew(*pVector); if(0==pSingle) continue;
+                        proc(*pSingle);
+                        ++count;
+                    }
+                }
+
+
+                void Tribes:: RemoveFrom(Tribe::List &tribes, const size_t zid)
+                {
+                    for(Tribe *tribe=tribes.head;tribe;tribe=tribe->next)
+                    {
+                        if(!tribe->removed(zid)) throw Specific::Exception(CallSign,"missing index=%u in Tribe", unsigned(zid));
+                    }
+                }
+
+                const Tribe * Tribes:: FoundDuplicateOf(const Vector      &vec,
+                                                        const Tribe::List &ok) noexcept
+                {
+                    for(const Tribe *guess=ok.head;guess;guess=guess->next)
+                    {
+                        assert(0!=guess->lastVec);
+                        if( vec == *(guess->lastVec) ) return guess;
+                    }
+                    return 0;
+                }
+
+                void Tribes:: noNullVector(XMLog &xml)
+                {
+                    Tribe::List ok;
+                    while(size>0)
+                    {
+                        Tribe * const tribe = popHead();
+                        if(0==tribe->lastVec)
+                        {
+                            const size_t zid = tribe->lastIdx;
+                            Y_XML_COMMENT(xml, "zero vector #" << zid);
+                            delete tribe;
+                            RemoveFrom(ok,   zid);
+                            RemoveFrom(*this,zid);
+                        }
+                        else
+                        {
+                            ok.pushTail(tribe);
+                        }
+                    }
+                    swapWith(ok);
+                }
+
+
+                void Tribes:: noDuplicates(XMLog &xml)
+                {
+                    Tribe::List ok;
+                    while(size>0)
+                    {
+                        Tribe * const        tribe = popHead();assert(0!=tribe->lastVec);
+                        const Tribe *  const guess = FoundDuplicateOf(*(tribe->lastVec),ok);
+
+                        if(0!=guess)
+                        {
+                            const size_t rid = tribe->lastIdx;
+                            Y_XML_COMMENT(xml, "duplicates #" << rid << " and #" << guess->lastIdx);
+                            RemoveFrom(*this,rid);
+                            for(Tribe *tr=ok.head;tr;tr=tr->next)
+                            {
+                                if(!tr->residue.removed(rid)) throw Specific:: Exception(CallSign, "missing index=%u in Tribe residue", unsigned(rid));
+                            }
+                            delete tribe;
+                        }
+                        else
+                            ok.pushTail(tribe);
+                    }
+                    swapWith(ok);
+                }
+
+                void Tribes:: doInitialize(XMLog &xml, Callback &proc)
+                {
+                    noNullVector(xml);
+                    noDuplicates(xml);
+                    MergeSort::Call(*this,Tribe::Compare);
+
+                    // collect first
+                    for(const Tribe *tribe=head;tribe;tribe=tribe->next)
+                    {
+                        assert(0!=tribe->lastVec);
+                        const Vector *vec = tryInsertNew(*(tribe->lastVec));
+                        if(!vec) throw Exception("unexpected multiple initial vector");
+                        proc(*vec);
+                    }
+
+                    assert(db.size==size);
+                    Coerce(collected) = db.size;
+                }
+
+
+                void Tribes:: makeCompression(XMLog &xml)
+                {
+                    makeReplacement(xml);
+                }
+
             }
         }
 
--- src/core/y/apex/api/ortho/coven/tribes.hpp
+++ src/core/y/apex/api/ortho/coven/tribes.hpp
@@ -28,17 +28,8 @@ namespace Yttrium
                 public:
                     static const char * const CallSign;
 
-                    static Natural MaxCount(const size_t n)
-                    {
-                        Natural res = 0;
-                        for(size_t k=1;k<=n;++k) res += Natural::Arrange(n,k);
-                        return res;
-                    }
-
-                    static void Display(const Vector &v)
-                    {
-                        std::cerr << "\t(+) " << v << std::endl;
-                    }
+                    static Natural MaxCount(const size_t n);
+                    static void Display(const Vector &v);
 
                     template <typename MATRIX> inline
                     explicit Tribes(XMLog &        xml,
@@ -58,24 +49,16 @@ namespace Yttrium
                             for(size_t indx=1;indx<=n;++indx)
                                 (void) pushTail( new Tribe(data,bank,indx,qfcc) );
                         }
-                        initialize(xml,proc);
-                        assert(db.size==size);
-                        Coerce(collected) = db.size;
+                        doInitialize(xml,proc);
                     }
 
-                    virtual ~Tribes() noexcept
-                    {
-                        while(db.size) vc->store( Coerce(db).popTail() );
-                    }
-
+                    virtual ~Tribes() noexcept;
                     Y_OSTREAM_PROTO(Tribes);
 
+                    Digest          signature(Hashing::Function &H) const;
+                    static SignType CompareVectors(const Vector * const lhs,
+                                                   const Vector * const rhs) noexcept;
 
-                    static inline SignType CompareVectors(const Vector * const lhs,
-                                                          const Vector * const rhs) noexcept
-                    {
-                        return Vector::Compare(*lhs,*rhs);
-                    }
 
                     template <typename MATRIX> inline
                     void generate(XMLog        & xml,
@@ -96,7 +79,7 @@ namespace Yttrium
 
 
                         Y_XML_COMMENT(xml, "#generated   = " << size);
-                        collect(proc);
+                        assembleLast(proc);
                         Y_XML_COMMENT(xml, "#collected   = " << collected);
 
                         if(size<=0)
@@ -111,17 +94,10 @@ namespace Yttrium
 
                     }
 
-                    Digest signature(Hashing::Function &H) const
-                    {
-                        H.set();
-                        {
-                            HashingStream fp(H);
-                            for(const Vector *v=db.head;v;v=v->next) v->serialize(fp);
-                        }
-                        return Hashing::MD::Of(H);
-                    }
 
-                    const size_t iteration;;
+
+
+                    const size_t iteration;
                     const size_t collected;
 
                 private:
@@ -130,143 +106,21 @@ namespace Yttrium
                 public:
                     const Vector::List db;
 
-                    void makeReplacement(XMLog &xml)
-                    {
-                        assert( isSortedAccordingTo(Tribe::Compare) );
-                        size_t replacement = 0;
-                        for(Tribe *curr=head;curr;curr=curr->next)
-                        {
-                            for(Tribe *prev=curr->prev;prev;prev=prev->prev)
-                            {
-                                if( (curr->qfamily!=prev->qfamily) && (curr->content==prev->content) )
-                                {
-                                    curr->adoptedBy(*prev);
-                                    ++replacement;
-                                }
-                            }
-                        }
+
+                    void           doInitialize(XMLog &xml, Callback &proc);
+                    void           noNullVector(XMLog &xml);
+                    void           noDuplicates(XMLog &xml);
+                    const Vector * tryInsertNew(const Vector &vec);
+                    void           assembleLast(Callback &);
+
+                    void makeReplacement(XMLog &xml);
+                    void makeCompression(XMLog &xml);
+
+                    static void          RemoveFrom(Tribe::List &tribes, const size_t zid);
+                    static const Tribe * FoundDuplicateOf(const Vector      &vec, const Tribe::List &ok) noexcept;
 
 
-                        Y_XML_COMMENT(xml,"#replacement = " << replacement);
-                        if(replacement<=0) { assert( isSortedAccordingTo(Tribe::Compare) ); return; }
-                        MergeSort::Call(*this,Tribe::Compare);
-                    }
 
-                    void makeCompression(XMLog &xml)
-                    {
-                        makeReplacement(xml);
-                    }
-
-                    void collect(Callback &proc)
-                    {
-                        size_t & count = Coerce(collected);
-                        count = 0;
-                        for(const Tribe *tribe=head;tribe;tribe=tribe->next)
-                        {
-                            const Vector *pVector = tribe->lastVec;      if(0==pVector) continue;
-                            const Vector *pSingle = tryInsert(*pVector); if(0==pSingle) continue;
-                            proc(*pSingle);
-                            ++count;
-                        }
-                    }
-
-                    const Vector * tryInsert(const Vector &vec)
-                    {
-                        for(const Vector *v=db.head;v;v=v->next)
-                        {
-                            switch( Vector::Compare(*v,vec) )
-                            {
-                                case __Zero__: return 0;
-                                case Negative:
-                                case Positive:
-                                    continue;
-                            }
-                        }
-                        return Coerce(db).pushTail( vc->query(vec) );
-                    }
-
-                    static void RemoveFrom(Tribe::List &tribes, const size_t zid)
-                    {
-                        for(Tribe *tribe=tribes.head;tribe;tribe=tribe->next)
-                        {
-                            if(!tribe->removed(zid)) throw Specific::Exception(CallSign,"missing index=%u in Tribe", unsigned(zid));
-                        }
-                    }
-
-                    void noNullVector(XMLog &xml)
-                    {
-                        Tribe::List ok;
-                        while(size>0)
-                        {
-                            Tribe * const tribe = popHead();
-                            if(0==tribe->lastVec)
-                            {
-                                const size_t zid = tribe->lastIdx;
-                                Y_XML_COMMENT(xml, "zero vector #" << zid);
-                                delete tribe;
-                                RemoveFrom(ok,   zid);
-                                RemoveFrom(*this,zid);
-                            }
-                            else
-                            {
-                                ok.pushTail(tribe);
-                            }
-                        }
-                        swapWith(ok);
-                    }
-
-                    static const Tribe * FoundDuplicateOf(const Vector      &vec,
-                                                          const Tribe::List &ok) noexcept
-                    {
-                        for(const Tribe *guess=ok.head;guess;guess=guess->next)
-                        {
-                            assert(0!=guess->lastVec);
-                            if( vec == *(guess->lastVec) ) return guess;
-                        }
-                        return 0;
-                    }
-
-                    void noDuplicates(XMLog &xml)
-                    {
-                        Tribe::List ok;
-                        while(size>0)
-                        {
-                            Tribe * const        tribe = popHead();assert(0!=tribe->lastVec);
-                            const Tribe *  const guess = FoundDuplicateOf(*(tribe->lastVec),ok);
-
-                            if(0!=guess)
-                            {
-                                const size_t rid = tribe->lastIdx;
-                                Y_XML_COMMENT(xml, "duplicates #" << rid << " and #" << guess->lastIdx);
-                                RemoveFrom(*this,rid);
-                                for(Tribe *tr=ok.head;tr;tr=tr->next)
-                                {
-                                    if(!tr->residue.removed(rid)) throw Specific:: Exception(CallSign, "missing index=%u in Tribe residue", unsigned(rid));
-                                }
-                                delete tribe;
-                            }
-                            else
-                                ok.pushTail(tribe);
-                        }
-                        swapWith(ok);
-                    }
-
-                    void initialize(XMLog &xml, Callback &proc)
-                    {
-                        noNullVector(xml);
-                        noDuplicates(xml);
-                        MergeSort::Call(*this,Tribe::Compare);
-                        assert(isSortedAccordingTo(Tribe::Compare));
-
-                        // collect first
-                        for(const Tribe *tribe=head;tribe;tribe=tribe->next)
-                        {
-                            assert(0!=tribe->lastVec);
-                            const Vector *vec = tryInsert(*(tribe->lastVec));
-                            if(!vec) throw Exception("unexpected multiple initial vector");
-                            proc(*vec);
-                        }
-                    }
 
 
                 };
