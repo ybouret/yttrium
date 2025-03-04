3f732d789e3084dbd6d24057a51d3fbe788e9391
ybouret <yann.bouret@gmail.com>
2025-03-04 18:58:11 +0100

    wip

--- src/chemical/y/chemical/weasel/parser.cpp
+++ src/chemical/y/chemical/weasel/parser.cpp
@@ -1,5 +1,7 @@
 
 #include "y/chemical/weasel/parser.hpp"
+#include "y/lingo/lexical/add-on/single-line-comment.hpp"
+#include "y/lingo/lexical/add-on/multi-lines-comment.hpp"
 
 namespace Yttrium
 {
@@ -15,8 +17,11 @@ namespace Yttrium
 
             Parser:: Parser() : Lingo::Parser("Weasel")
             {
+                
+                (void) lexer.plug<Lingo::Lexical::CPlusPlusComment>("Comment++");
+                (void) lexer.plug<Lingo::Lexical::C_Comment>("Comment");
             }
-            
+
 
         }
 
