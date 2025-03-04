8e87cb5e6ef489ce6929cbd0f8637d9dd773715d
ybouret <yann.bouret@gmail.com>
2025-03-04 18:35:10 +0100

    wip

---
+++ src/chemical/tests/test-chemtran.cpp
@@ -0,0 +1,9 @@
+#include "y/chemical/formula.hpp"
+#include "y/utest/run.hpp"
+
+Y_UTEST(chemtran)
+{
+
+}
+Y_UDONE()
+
---
+++ src/chemical/y/chemical/formula.hpp
@@ -0,0 +1,10 @@
+//! \file
+
+
+#ifndef Y_Chemical_Formula_Included
+#define Y_Chemical_Formula_Included 1
+
+
+
+#endif
+
