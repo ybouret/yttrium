46a95e62a64b4f87ff72be0c75c2bbe8755d8125
ybouret <yann.bouret@gmail.com>
2025-03-05 10:27:10 +0100

    wip

--- src/core/tests/dft/nd.cpp
+++ src/core/tests/dft/nd.cpp
@@ -8,43 +8,45 @@ using namespace Yttrium;
 
 Y_UTEST(dft_nd)
 {
-    //Field::In2D<float,Memory::> field;
-    Field::Format2D  L = new Field::Layout2D( Field::Coord2D(1,1), Field::Coord2D(4,8) );
 
-    typedef Field::In2D< Complex<float>, Memory::Dyadic > F2D;
-    F2D F("F",L);
-
-    std::cerr << F << std::endl;
-    for(unit_t j=F->lower.y;j<=F->upper.y;++j)
+    for(unit_t nx=1;nx<=8;nx*=2)
     {
-        //std::cerr << "j=" << j << std::endl;
-        for(unit_t i=F->lower.x;i<=F->upper.x;++i)
+        for(unit_t ny=1;ny<=8;ny*=2)
         {
-            F[j][i].re = i;
-            F[j][i].im = j;
+            Field::Format2D  L = new Field::Layout2D( Field::Coord2D(1,1), Field::Coord2D(nx,ny) );
+
+            typedef Field::In2D< Complex<float>, Memory::Dyadic > F2D;
+            F2D F("F",L);
+
+            std::cerr << F << std::endl;
+            for(unit_t j=F->lower.y;j<=F->upper.y;++j)
+            {
+                //std::cerr << "j=" << j << std::endl;
+                for(unit_t i=F->lower.x;i<=F->upper.x;++i)
+                {
+                    F[j][i].re = i;
+                    F[j][i].im = j;
+                }
+                Core::Display( std::cerr, &F[j][1].re, 2*F->width.x ) << std::endl;
+            }
+
+            const size_t  nn[] = { 0, F->width.y, F->width.x };
+            float * const data = (&F[1][1].re) - 1;
+            DFTN::Transform(data, nn, 2, 1);
+            DFTN::Transform(data, nn, 2, -1);
+
+            std::cerr << std::endl;
+            for(unit_t j=F->lower.y;j<=F->upper.y;++j)
+            {
+                //std::cerr << "j=" << j << std::endl;
+                for(unit_t i=F->lower.x;i<=F->upper.x;++i)
+                {
+                    F[j][i] /= F->shift.y;
+                }
+                Core::Display( std::cerr, &F[j][1].re, F->width.x*2 ) << std::endl;
+            }
         }
-        Core::Display( std::cerr, &F[j][1].re, 2*F->width.x ) << std::endl;
     }
-
-    const size_t  nn[] = { 0, F->width.y, F->width.x };
-    float * const data = (&F[1][1].re) - 1;
-    DFTN::fourn(data, nn, 2, 1);
-    DFTN::fourn( (&F[1][1].re) - 1, nn, 2, -1);
-
-    std::cerr << std::endl;
-    for(unit_t j=F->lower.y;j<=F->upper.y;++j)
-    {
-        //std::cerr << "j=" << j << std::endl;
-        for(unit_t i=F->lower.x;i<=F->upper.x;++i)
-        {
-            F[j][i] /= F->shift.y;
-        }
-        Core::Display( std::cerr, &F[j][1].re, F->width.x*2 ) << std::endl;
-    }
-
-    std::cerr << "width=" << F->width << std::endl;
-    std::cerr << "shift=" << F->shift << std::endl;
-
 
 }
 Y_UDONE()
--- src/core/y/dft/dftn.hpp
+++ src/core/y/dft/dftn.hpp
@@ -12,6 +12,15 @@ namespace Yttrium
 
     struct DFTN
     {
+        static inline size_t Prod(const size_t   nn[],
+                                  const unsigned ndim) noexcept
+        {
+            size_t prod=1;
+            for(unsigned idim=1;idim<=ndim;idim++)
+                prod *= nn[idim];
+            return prod;
+        }
+
         /**
          Replaces data by its ndim-dimensional discrete Fourier transform, if isign is input as 1.
          nn[1..ndim] is an integer array containing the lengths of each dimension (number of complex
@@ -22,28 +31,20 @@ namespace Yttrium
          equivalent to storing the array by rows. If isign is input as−1, data is replaced by its inverse
          */
         static inline
-        void fourn(float          data[],
-                   const size_t   nn[],
-                   const unsigned ndim,
-                   const int      isign)
+        void Transform(float * const  data,
+                       const size_t   nn[],
+                       const unsigned ndim,
+                       const int      isign)
         {
-            size_t  k1,k2;
-            float tempi,tempr;
-            double theta,wi,wpi,wpr,wr,wtemp;
+            double theta,wtemp;
 
-            size_t ntot=1;
-            for(unsigned idim=1;idim<=ndim;idim++)
-            {
-                std::cerr << "nn[" << idim << "]=" << nn[idim] << std::endl;
-                ntot *= nn[idim];
-            }
-            std::cerr << "ntot=" << ntot << std::endl;
-            size_t nprev=1;
+            const size_t ntot = Prod(nn,ndim);
+            size_t       nprv = 1;
             for(unsigned idim=ndim;idim>0;--idim)
             {
                 const size_t n     = nn[idim];
-                const size_t nrem  = ntot/(n*nprev);
-                const size_t ip1   = nprev << 1;
+                const size_t nrem  = ntot/(n*nprv);
+                const size_t ip1   = nprv << 1;
                 const size_t ip2   = ip1*n;
                 const size_t ip3   = ip2*nrem;
                 {
@@ -57,13 +58,7 @@ namespace Yttrium
                                 for (size_t i3=i1;i3<=ip3;i3+=ip2)
                                 {
                                     const size_t i3rev=i2rev+i3-i2;
-                                    assert(i3>=1);
-                                    assert(i3rev>=1);
-                                    assert(i3+1<=ntot*2);
-                                    assert(i3rev+1<=ntot*2);
-
-                                    Swap(data[i3],data[i3rev]);
-                                    Swap(data[i3+1],data[i3rev+1]);
+                                    DFT::Swap2(data+i3,data+i3rev);
                                 }
                             }
                         }
@@ -82,24 +77,27 @@ namespace Yttrium
                     const size_t ifp2  = ifp1 << 1;
                     theta = isign*6.28318530717959/(ifp2/ip1);
                     wtemp = sin(0.5*theta);
-                    wpr   = -2.0*wtemp*wtemp;
-                    wpi   = sin(theta);
-                    wr=1.0;
-                    wi=0.0;
+                    double wpr = -2.0*wtemp*wtemp;
+                    double wpi = sin(theta);
+                    double wr  = 1.0;
+                    double wi  = 0.0;
                     for(size_t i3=1;i3<=ifp1;i3+=ip1)
                     {
                         for (size_t i1=i3;i1<=i3+ip1-2;i1+=2)
                         {
                             for(size_t i2=i1;i2<=ip3;i2+=ifp2)
                             {
-                                k1=i2;        assert(k1>=1); assert(k1+1<=2*ntot);
-                                k2=k1+ifp1;   assert(k2>=1); assert(k2+1<=2*ntot);
-                                tempr=(float)wr*data[k2]-(float)wi*data[k2+1];
-                                tempi=(float)wr*data[k2+1]+(float)wi*data[k2];
-                                data[k2]=data[k1]-tempr;
-                                data[k2+1]=data[k1+1]-tempi;
-                                data[k1] += tempr;
-                                data[k1+1] += tempi;
+                                const size_t k1re = i2;
+                                const size_t k2re = k1re+ifp1;
+                                const size_t k1im = k1re+1;
+                                const size_t k2im = k2re+1;
+
+                                const float tempr=(float)wr*data[k2re]-(float)wi*data[k2im];
+                                const float tempi=(float)wr*data[k2im]+(float)wi*data[k2re];
+                                data[k2re]  = data[k1re]-tempr;
+                                data[k2im]  = data[k1im]-tempi;
+                                data[k1re] += tempr;
+                                data[k1im] += tempi;
                             }
                         }
                         wr=(wtemp=wr)*wpr-wi*wpi+wr;
@@ -107,7 +105,7 @@ namespace Yttrium
                     }
                     ifp1=ifp2;
                 }
-                nprev *= n;
+                nprv *= n;
             }
         }
     };
