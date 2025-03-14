10ebff79c01c72c68b62cecae6e2a2f053d37a42
Yann Bouret <yann.bouret@unice.fr>
2025-03-14 09:45:59 +0100

    wip

--- src/chemical/y/chemical/plexus/cluster/conservations.cpp
+++ src/chemical/y/chemical/plexus/cluster/conservations.cpp
@@ -1,13 +1,24 @@
 
 #include "y/chemical/plexus/clusters.hpp"
+#include "y/mkl/algebra/ortho-space.hpp"
+#include "y/system/exception.hpp"
 
 namespace Yttrium
 {
     namespace Chemical
     {
+        using namespace MKL;
+
         void Cluster:: conservations(XMLog &xml)
         {
-            
+            Y_XML_SECTION(xml, "conservations");
+
+            Matrix<apz> Q;
+            if(!OrthoSpace::Make(Q,topology))
+            {
+                throw Specific::Exception(CallSign, "no orthogonal space");
+            }
+
         }
     }
 
