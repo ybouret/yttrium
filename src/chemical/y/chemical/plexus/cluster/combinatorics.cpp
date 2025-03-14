20ed2f1dbe2e3c55e2bf04eb9fbad0cb5a06e7cd
ybouret <yann.bouret@gmail.com>
2025-03-14 12:15:01 +0100

    wip

--- src/chemical/y/chemical/plexus/cluster/combinatorics.cpp
+++ src/chemical/y/chemical/plexus/cluster/combinatorics.cpp
@@ -6,6 +6,8 @@
 #include "y/apex/api/ortho/coven/survey/integer.hpp"
 #include "y/apex/api/count-non-zero.hpp"
 
+#include "y/associative/address-book.hpp"
+
 namespace Yttrium
 {
     namespace Chemical
@@ -14,6 +16,62 @@ namespace Yttrium
         using namespace Apex;
         using namespace Ortho;
         using namespace Coven;
+
+        class MixTab : public Quantized, public CxxArray<int,Memory::Dyadic>
+        {
+        public:
+            typedef CxxArray<int,Memory::Dyadic> ArrayType;
+
+            explicit MixTab(const Readable<apz> &arr,
+                            const iMatrix       &topo) :
+            ArrayType(arr.size()),
+            stoi(topo.cols)
+            {
+                const size_t n = arr.size();
+                for(size_t i=n;i>0;--i)
+                {
+                    if( !arr[i].tryCast( (*this)[i]) )
+                        throw Specific::Exception(Cluster::CallSign,"coefficient overflow");
+                }
+
+                const size_t m = stoi.size();
+                for(size_t j=m;j>0;--j)
+                {
+                    apz sum = 0;
+                    for(size_t i=n;i>0;--i)
+                        sum += arr[i] * topo[i][j];
+                    if( !sum.tryCast(stoi[j]))
+                        throw Specific::Exception(Cluster::CallSign,"stoichiometry overflow");
+                }
+
+            }
+
+            virtual ~MixTab() noexcept
+            {
+            }
+
+
+            bool isEfficientFor(const ClusterType &cl) const
+            {
+                AddressBook target;
+                for(const SNode *sn=cl.species->head;sn;sn=sn->next)
+                {
+                    const Species &sp = **sn;
+                    if( 0 != sp(stoi,SubLevel) ) target |= sp;
+                }
+                
+
+
+            }
+
+            ArrayType stoi;
+
+            MixTab *next;
+            MixTab *prev;
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(MixTab);
+        };
 
         void Cluster:: combinatorics(XMLog &xml)
         {
@@ -25,6 +83,18 @@ namespace Yttrium
                 Y_XML_COMMENT(xml,"no combinatorics");
                 return;
             }
+
+            for(const IntegerSurvey::ArrayType *arr=survey->head;arr;arr=arr->next)
+            {
+                MixTab mix(*arr,topology);
+                std::cerr << mix << " => " << mix.stoi << std::endl;
+                if(mix.isEfficientFor(**this))
+                {
+
+                }
+            }
+
+            std::cerr << "sizeof(MixTab)=" << sizeof(MixTab) << std::endl;
         }
 
     }
