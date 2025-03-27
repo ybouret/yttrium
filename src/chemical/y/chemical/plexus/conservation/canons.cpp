f590616b128f966ec6856f74587dfbf403c00977
ybouret <yann.bouret@gmail.com>
2025-03-27 10:49:59 +0100

    wip

--- src/chemical/y/chemical/plexus/conservation/canons.cpp
+++ src/chemical/y/chemical/plexus/conservation/canons.cpp
@@ -14,10 +14,41 @@ namespace Yttrium
             Canons:: Canons(const ListOf<Law> &laws) :
             CxxListOf<Canon>()
             {
+                std::cerr << "\t---- Building Canons ----" << std::endl;
                 for(const Law *law=laws.head;law;law=law->next)
                 {
-                    
+
+                    std::cerr << "using " << law->name << std::endl;
+                    for(Canon *canon=head;canon;canon=canon->next)
+                    {
+                        assert(!canon->has(*law));
+
+                        if(canon->accepts(*law))
+                        {
+                            *canon << *law;
+                            goto FUSION;
+                        }
+                    }
+
+                    // not found => new canon
+                    pushTail( new Canon(*law) );
+                    continue;
+
+                FUSION:
+                    {
+                        CxxListOf<Canon> store;
+                        while(size>0)
+                        {
+                            AutoPtr<Canon> lhs = popHead();
+
+                            store.pushTail(lhs.yield());
+                        }
+                        swapWith(store);
+                    }
                 }
+
+                std::cerr << "|canons|=" << size << std::endl;
+
             }
 
         }
