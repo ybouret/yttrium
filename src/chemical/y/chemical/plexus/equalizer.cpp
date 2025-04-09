3249c409f85758cb269178a839c94d760453df2e
Yann Bouret <yann.bouret@unice.fr>
2025-04-09 14:13:44 +0200

    wip

--- src/chemical/y/chemical/plexus/equalizer.cpp
+++ src/chemical/y/chemical/plexus/equalizer.cpp
@@ -1,8 +1,4 @@
-
 #include "y/chemical/plexus/equalizer.hpp"
-
-
-
 
 namespace Yttrium
 {
@@ -10,50 +6,6 @@ namespace Yttrium
     {
         namespace Equalizer
         {
-
-            Proceeding:: ~Proceeding() noexcept
-            {
-            }
-
-
-            Proceeding:: Proceeding(const Cluster & cluster,
-                                    const Banks   & banks) :
-            watchers(),
-            twoSided(),
-            oneSided(cluster,banks),
-            vanishing(),
-            next(0),
-            prev(0)
-            {
-                for(const Conservation::Canon *pc=cluster.canons.head;pc;pc=pc->next)
-                {
-                    const Conservation::Canon &canon = *pc;
-                    watchers.pushTail( new Watcher(cluster,canon)        );
-                    twoSided.pushTail( new TwoSided(cluster,canon,banks) );
-                }
-                assert(watchers.size==twoSided.size);
-            }
-
-            void Proceeding:: session(XMLog     &  xml,
-                                      XWritable &  C0,
-                                      Summator  &  I0)
-            {
-                I0.forEach( & XAdd::free );
-                {
-                    Watcher  *cw = watchers.head;
-                    TwoSided *ts = twoSided.head;
-                    for(size_t i=watchers.size;i>0;--i,cw=cw->next,ts=ts->next)
-                    {
-                        assert(0!=cw);
-                        assert(0!=ts);
-                        vanishing.free();
-                        cw->fix(xml, C0, I0, TopLevel, vanishing);
-                        ts->fix(xml, C0, I0, TopLevel, vanishing);
-                    }
-                }
-                oneSided.fix(xml,C0,TopLevel);
-
-            }
 
 
         }
--- src/chemical/y/chemical/plexus/equalizer.hpp
+++ src/chemical/y/chemical/plexus/equalizer.hpp
@@ -5,10 +5,7 @@
 #ifndef Y_Chemical_Equalizer_Included
 #define Y_Chemical_Equalizer_Included 1
 
-#include "y/chemical/plexus/conservation/warden.hpp"
-#include "y/chemical/plexus/equalizer/two-sided.hpp"
-#include "y/chemical/plexus/equalizer/one-sided.hpp"
-
+#include "y/chemical/plexus/equalizer/proceeding.hpp"
 
 
 
@@ -18,30 +15,7 @@ namespace Yttrium
     {
         namespace Equalizer
         {
-
-            class Proceeding : public Quantized
-            {
-            public:
-                typedef Conservation::Warden Watcher;;
-
-                explicit Proceeding(const Cluster &, const Banks &);
-                virtual ~Proceeding() noexcept;
-
-                //! equalize cluster
-                void session(XMLog     &  xml,
-                             XWritable &  C0,
-                             Summator  &  I0);
-
-                Watcher::List     watchers;
-                TwoSided::List    twoSided;
-                OneSided          oneSided;
-                AddressBook       vanishing;
-                Proceeding *      next;
-                Proceeding *      prev;
-                
-            private:
-                Y_DISABLE_COPY_AND_ASSIGN(Proceeding);
-            };
+            
 
         }
 
