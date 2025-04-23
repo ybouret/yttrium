eb7ced11cb622c7e31804f6a0a3b65f8b5b61c83
ybouret <yann.bouret@gmail.com>
2025-04-23 15:16:46 +0200

    wip

--- algo/co2.py
+++ algo/co2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
+from scipy.optimize import root_scalar
 
 # water
 K_w = 10**(-14)
@@ -13,42 +13,57 @@ K_H = 29.41  # atm.L.mol^(-1), Henry's constant
 # buffer pKa, here for HEPES
 K_Y = 10**(-7.5)
 
-# global data for computation
-C_Y   = 0         # buffer concentration
-C_Na  = C_Y/2     # added NaOH to produce buffer
-P_CO2 = 0.000355  # atmospheric pressure in atm
 
 
 # matter + charge conservation
-def electroneutrality(h):
-    return h + C_Na          \
+def electroneutrality(h,CO2,Y,Na):
+    return h + Na          \
         - (K_w/h)            \
-        - C_Y * K_Y/(K_Y+h)  \
-        - (P_CO2*K_1)/(K_H*h) * (1.0 + 2.0 * K_2/h)
+        - Y * K_Y/(K_Y+h)  \
+        - (CO2*K_1)/(K_H*h) * (1.0 + 2.0 * K_2/h)
 
 
-# use SciPy to solve equation from pH=7
-def compute_pH(pressure, buffer):
-    global P_CO2, C_Y, C_Na
-    P_CO2 = pressure
-    C_Y   = buffer
-    C_Na  = buffer * 0.5
-    return -np.log10(fsolve(electroneutrality, 10**(-7))[0])
+def compute_initial_Na(initial_pH,initial_buffer):
+    h0 = 10.0**(-initial_pH)
+    return - electroneutrality(h0,0.0,initial_buffer,0.0)
 
+def compute_protons(initial_pH,P_CO2,C_Y):
+    C_Na = compute_initial_Na(initial_pH,C_Y)
+    res  = root_scalar(electroneutrality,
+                       args=(P_CO2,C_Y,C_Na),
+                       xtol=1e-12,
+                       method='bisect',
+                       bracket=[1e-14,1]);
+    #print(res)
+    return res.root;
+
+compute_protons(8,0,0)
 
 # build and array of buffer concentration
-buffer_conc = np.arange(0,0.01,0.00001)
+buffer_conc = np.arange(0,0.1,0.0001)
+n = len(buffer_conc)
+
+pH0=[7.5,8,8.5,9]
+m  = len(pH0)
 
 # prepare array of pH
-pH          = np.arange(len(buffer_conc), dtype=float)
+pH     = np.zeros([m,n], dtype=float)
 
-# fill pH
-for i in range(len(pH)):
-    pH[i] = compute_pH(0.000355,buffer_conc[i])
+
+CO2=5.0/100
+#CO2=0.0
+for i in range(n):
+    for j in range(m):
+        pH[j][i] =  -np.log10(compute_protons(pH0[j],CO2,buffer_conc[i]))
 
 fig, ax = plt.subplots()
-plt.plot(buffer_conc,pH)
+description = []
+for j in range(m):
+    plt.plot(buffer_conc,pH[j])
+    description.append( f"pH0={pH0[j]:.1f}" )
+
+plt.legend(description)
+plt.xlabel('[buffer] in mol/L')
+plt.ylabel('pH')
 plt.grid()
 plt.show()
-
-
