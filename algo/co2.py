2fabe961a4f3a07e172ef28d4b7225145502621f
Yann Bouret <yann.bouret@unice.fr>
2025-04-24 09:07:13 +0200

    wip

--- algo/co2.py
+++ algo/co2.py
@@ -14,53 +14,54 @@ K_H = 29.41  # atm.L.mol^(-1), Henry's constant
 K_Y = 10**(-7.5)
 
 
-
 # matter + charge conservation
-def electroneutrality(h,CO2,Y,Na):
+def electroneutrality(h, CO2, Y, Na):
     return h + Na          \
-        - (K_w/h)            \
+        - (K_w/h)          \
         - Y * K_Y/(K_Y+h)  \
         - (CO2*K_1)/(K_H*h) * (1.0 + 2.0 * K_2/h)
 
 
-def compute_initial_Na(initial_pH,initial_buffer):
+def compute_initial_Na(initial_pH, initial_buffer):
     h0 = 10.0**(-initial_pH)
-    return - electroneutrality(h0,0.0,initial_buffer,0.0)
+    return - electroneutrality(h0, 0.0, initial_buffer, 0.0)
 
-def compute_protons(initial_pH,P_CO2,C_Y):
-    C_Na = compute_initial_Na(initial_pH,C_Y)
-    res  = root_scalar(electroneutrality,
-                       args=(P_CO2,C_Y,C_Na),
-                       xtol=1e-12,
-                       method='bisect',
-                       bracket=[1e-14,1]);
-    #print(res)
-    return res.root;
 
-compute_protons(8,0,0)
+def compute_protons(initial_pH, P_CO2, C_Y):
+    C_Na = compute_initial_Na(initial_pH, C_Y)
+    res = root_scalar(electroneutrality,
+                      args=(P_CO2, C_Y, C_Na),
+                      xtol=1e-12,
+                      method='bisect',
+                      bracket=[1e-14, 1])
+    # print(res)
+    return res.root
+
+
+compute_protons(8, 0, 0)
 
 # build and array of buffer concentration
-buffer_conc = np.arange(0,0.1,0.0001)
+buffer_conc = np.arange(0, 0.1, 0.0001)
 n = len(buffer_conc)
 
-pH0=[7,7.5,8,8.5,9]
-m  = len(pH0)
+pH0 = [7, 7.5, 8, 8.5, 9]
+m = len(pH0)
 
 # prepare array of pH
-pH     = np.zeros([m,n], dtype=float)
+pH = np.zeros([m, n], dtype=float)
 
 
-CO2=5.0/100
-#CO2=0.0
+CO2 = 5.0/100
+# CO2=0.0
 for i in range(n):
     for j in range(m):
-        pH[j][i] =  -np.log10(compute_protons(pH0[j],CO2,buffer_conc[i]))
+        pH[j][i] = -np.log10(compute_protons(pH0[j], CO2, buffer_conc[i]))
 
 fig, ax = plt.subplots()
 description = []
 for j in range(m):
-    plt.plot(buffer_conc,pH[j])
-    description.append( f"pH0={pH0[j]:.1f}" )
+    plt.plot(buffer_conc, pH[j])
+    description.append(f"pH0={pH0[j]:.1f}")
 
 plt.legend(description)
 plt.xlabel('[buffer] in mol/L')
